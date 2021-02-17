248191d63f0c89856eabcb7a1f3114546aff3ae9
David Durst <davidbdurst@gmail.com>
2021-02-17 15:58:34 -0800

    tracking intial actions

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/David-Durst/csknow
+
+go 1.16
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
+	github.com/markus-wa/demoinfocs-golang/v2 v2.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	dem "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
+	events "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
+)
+
+func main() {
+	f, err := os.Open("./test_2_17_21.dem")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	p := dem.NewParser(f)
+	defer p.Close()
+
+	// Register handler on kill events
+	p.RegisterEventHandler(func(e events.Kill) {
+		var hs string
+		if e.IsHeadshot {
+			hs = " (HS)"
+		}
+		var wallBang string
+		if e.PenetratedObjects > 0 {
+			wallBang = " (WB)"
+		}
+		fmt.Printf("%s <%v%s%s> %s\n", e.Killer, e.Weapon, hs, wallBang, e.Victim)
+	})
+
+	// Parse to end
+	err = p.ParseToEnd()
+	if err != nil {
+		panic(err)
+	}
+}
