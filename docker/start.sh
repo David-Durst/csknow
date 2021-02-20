703978bb9f14c4594a7960e7c4a56383af21622f
David Durst <davidbdurst@gmail.com>
2021-02-20 00:22:31 +0000

    start loading demos folder, ignoring folder

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,5 @@
-docker run --name durst_csgo -d -p 27015:27015 -p 27015:27015/udp durst/csgo:0.1 -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_de_dust2 +map de_dust2 +sv_lan 1
+mkdir -p ../demos
+docker run --name durst_csgo \
+    --mount type=bind,source="$(pwd)"/../demos,target=/home/csgo/demos \
+    --rm -d -p 27015:27015 -p 27015:27015/udp durst/csgo:0.1 \
+    -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_de_dust2 +map de_dust2 +sv_lan 1
