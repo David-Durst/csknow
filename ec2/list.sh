e27f336392a6738f83318c3714bec475e30297c1
David Durst <davidbdurst@gmail.com>
2021-02-21 09:31:44 -0800

    adding ec2 scripts, only list confirmed to work

---
+++ ec2/list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+id=$(cat ${script_dir}/../private/.ec2)
+aws ec2 describe-instances --instance-ids $id \
+    --query 'Reservations[0].Instances[0].{InstanceId:InstanceId,IP:PublicIpAddress,State:State}'
+
