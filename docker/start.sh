7b7a50a47d5337b3ef43fcaa80d4625bff5fcafb
David Durst <davidbdurst@gmail.com>
2021-02-21 03:05:09 +0000

    updating to nonvolume directory works

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 mkdir -p ../demos
 docker run --name durst_csgo \
-    --rm -d --net=host \
+    --rm --net=host \
     -e SRCDS_MAPGROUP=mg_de_durst2 -e SRCDS_STARTMAP=de_dust2 \
     --mount type=bind,source="$(pwd)"/../demos,target=/home/steam/demos \
     durst/csgo:0.1
--- docker/update.sh
+++ docker/update.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+mkdir -p "${NONVOLUMESTEAMAPPDIR}" || true  
+
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
-				+force_install_dir "${STEAMAPPDIR}" \
+				+force_install_dir "${NONVOLUMESTEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
-				+quit
+				+quit			+quit
