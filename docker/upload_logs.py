2e3b85b7fbf6a67e68bbc62add64009ed80856f9
David Durst <davidbdurst@gmail.com>
2021-02-21 23:01:30 +0000

    adding uuid per machine

--- docker/upload_logs.py
+++ docker/upload_logs.py
@@ -2,8 +2,13 @@
 import glob
 import os
 import time
+import uuid
+from pathlib import Path
 
 cur_path = os.path.dirname(os.path.realpath(__file__))
+
+machine_id = uuid.uuid1()
+print(f"machine uuid: {machine_id}")
 print("starting while loop of uploading")
 while True:
     files = glob.glob(cur_path + '/csgo-dedicated-non-volumne/csgo/*.dem') # * means all if need specific format then *.csv
@@ -11,7 +16,9 @@ while True:
     # leave the most recently touched demo, as cs is still writing to it
     for f in files[:-1]:
         print("moving file:" + str(f))
-        os.system(f"aws s3 cp {f} s3://csknow/demos/")
+        p = Path(f)
+        aws_name = p.stem + str(machine_id) + p.suffix 
+        os.system(f"aws s3 cp {f} s3://csknow/demos/{aws_name}")
         os.remove(f)
     time.sleep(60)
 print("done while loop of uploading")
