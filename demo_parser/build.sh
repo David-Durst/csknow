a71bc6e7c7c50f029d74fe038427884d3375ad5d
David Durst <davidbdurst@gmail.com>
2021-02-22 12:47:33 -0800

    added python wrapper for loading and running

---
+++ demo_parser/build.sh
@@ -0,0 +1,1 @@
+docker build -t durst/csgo-demo-parser:0.1 .
---
+++ demo_parser/start.sh
@@ -0,0 +1,26 @@
+mkdir -p ../demos
+
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+
+iam_role=$(cat ${script_dir}/../private/.aws_csgo_server_role)
+
+docker run --name durst_csgo_demo_parser \
+    --rm \
+    -e RUNNING_IN_EC2=1 -e ROLE=${iam_role} \
+    durst/csgo-demo-parser:0.1
+
