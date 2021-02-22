e720e817d66f58a6834ad3f6f571e59bc955ad6d
David Durst <davidbdurst@gmail.com>
2021-02-21 19:59:16 -0800

    parsing csvs and moving demos to logged location

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/David-Durst/csknow
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.37.15
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
-	github.com/markus-wa/demoinfocs-golang/v2 v2.5.0 // indirect
+	github.com/markus-wa/demoinfocs-golang/v2 v2.5.0
 )
--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	dem "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
 	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/common"
 	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
 	"os"
+	"path"
+	"path/filepath"
 	"sort"
 )
+
+const localDemName = "local.dem"
+const localCSVName = "local.csv"
+const unprocessedPrefix = "demos/unprocessed/"
+const processedPrefix = "demos/processed/"
+const csvPrefiix = "demos/csvs/"
+const bucketName = "csknow"
 
 func teamToNum(team common.Team) int {
 	switch team {
@@ -19,30 +32,78 @@ func teamToNum(team common.Team) int {
 	return -1
 }
 
-func main() {
-	f, err := os.Open("./tv_demo1.dem")
+
+func downloadFile(downloader *s3manager.Downloader, fileKey string) {
+	// Create a file to write the S3 Object contents to.
+	awsF, err := os.Create(localDemName)
+	if err != nil {
+		fmt.Errorf("failed to create file %q, %v", localDemName, err)
+		os.Exit(1)
+	}
+	defer awsF.Close()
+
+	// Write the contents of S3 Object to the file
+	_, err = downloader.Download(awsF, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key: aws.String(fileKey),
+	})
+	if err != nil {
+		fmt.Errorf("failed to download file, %v", err)
+		os.Exit(1)
+	}
+
+}
+
+func uploadFile(uploader *s3manager.Uploader, fileKey string) {
+	csvFile, err := os.Open(localCSVName)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key: aws.String(csvPrefiix + path.Base(fileKey) + ".csv"),
+		Body: csvFile,
+	})
+	if err != nil {
+		fmt.Errorf("Couldn't upload file" + fileKey)
+		os.Exit(1)
+	}
+}
+
+func processFile(unprocessedKey string) {
+	demFilePath := path.Base(unprocessedKey)
+	f, err := os.Open(localDemName)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
+	csvFile, err := os.Create(localCSVName)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
 	p := dem.NewParser(f)
 	defer p.Close()
 
 	// Register handler on kill events
-	fmt.Printf("tick number,match started,game phase,rounds played,round start,round end,round end reason,freeze time ended,t score,ct score,num players")
+	csvFile.WriteString("tick number,match started,game phase,rounds played,is warmup,round start,round end,round end reason,freeze time ended,t score,ct score,num players")
 	for i := 0; i < 10; i++ {
-		fmt.Printf(",player %d name,player %d team,player %d x postion,player %d y position,player %d z position,player %d x view direction,player %d y view direction",i,i,i,i,i,i,i)
+		csvFile.WriteString(fmt.Sprintf(",player %d name,player %d team,player %d x postion,player %d y position,player %d z position,player %d x view direction,player %d y view direction",i,i,i,i,i,i,i))
 	}
-	fmt.Printf("\n")
+	csvFile.WriteString(fmt.Sprintf(",demo file key\n"))
 	// flags from other event handlers to frame done handler
 	roundStart := 0
-	roundEndReason := 0
+	roundEndReason := -1
 	roundEnd := 0
-	freezeTimeEnded := 0
+	freezeTime := 0
 
 	p.RegisterEventHandler(func(e events.RoundStart) {
 		roundStart = 1
+		freezeTime = 1
 	})
 
 	p.RegisterEventHandler(func(e events.RoundEnd) {
@@ -70,53 +131,95 @@ func main() {
 	})
 
 	p.RegisterEventHandler(func(e events.RoundFreezetimeEnd) {
-		freezeTimeEnded = 1
+		freezeTime = 0
 	})
 
+	ticksProcessed := 0
 	p.RegisterEventHandler(func(e events.FrameDone) {
+		ticksProcessed++
 		gs := p.GameState()
 		players := gs.Participants().Playing()
 		if len(players) != 10 {
+			return
+		}
+		// skip the first couple seconds as tick number  can have issues with these
+		// this should be fine as should just be warmup, which is 3 seconds despite fact I told cs to disable
+		if ticksProcessed < 30 {
 			return
 		}
 		matchStarted := 0
 		if gs.IsMatchStarted() {
 			matchStarted = 1
 		}
-		fmt.Printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,", p.CurrentFrame(), matchStarted, gs.GamePhase(), gs.TotalRoundsPlayed(),
-			roundStart, roundEnd, roundEndReason, freezeTimeEnded, gs.TeamTerrorists().Score(), gs.TeamCounterTerrorists().Score(), len(players))
+		isWarmup := 0
+		if gs.IsWarmupPeriod() {
+			isWarmup = 1
+		}
+		csvFile.WriteString(fmt.Sprintf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,", p.CurrentFrame(), matchStarted, gs.GamePhase(), gs.TotalRoundsPlayed(), isWarmup,
+			roundStart, roundEnd, roundEndReason, freezeTime, gs.TeamTerrorists().Score(), gs.TeamCounterTerrorists().Score(), len(players)))
 		sort.Slice(players, func(i int, j int) bool {
 			return players[i].Name < players[j].Name
 		})
 		for i := 0; i < 10; i++ {
 			if i >= len(players) {
-				fmt.Printf(",,,,,")
+				csvFile.WriteString(",,,,,")
 			} else {
-				fmt.Printf("%s,%d,%.2f,%.2f,%.2f,%.2f,%.2f", players[i].Name, teamToNum(players[i].Team),
+				csvFile.WriteString(fmt.Sprintf("%s,%d,%.2f,%.2f,%.2f,%.2f,%.2f", players[i].Name, teamToNum(players[i].Team),
 					players[i].Position().X,players[i].Position().Y, players[i].Position().Z,
-					players[i].ViewDirectionX(), players[i].ViewDirectionY())
+					players[i].ViewDirectionX(), players[i].ViewDirectionY()))
 			}
-			if i < 9 {
-				fmt.Printf(",")
-			}
+			csvFile.WriteString(",")
 		}
-		fmt.Print("\n")
-		/*
-		cts := gs.TeamCounterTerrorists()
-		for i := 0; i < len(cts.Members()); i++ {
-			member := cts.Members()[i]
-			fmt.Printf("Player %d position: %s\n", member.Name, cts.Members()[i].Position().String())
-		}
-
-		 */
+		csvFile.WriteString(demFilePath + "\n")
 		roundStart = 0
 		roundEnd = 0
 		roundEndReason = -1
-		freezeTimeEnded = 0
 	})
-	// Parse to end
-	err = p.ParseToEnd()
+	p.ParseToEnd()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func main() {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1")},
+		))
+
+	svc := s3.New(sess)
+
+	downloader := s3manager.NewDownloader(sess)
+	uploader := s3manager.NewUploader(sess)
+
+	var filesToMove []string
+
+	i := 0
+	svc.ListObjectsV2Pages(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+		Prefix: aws.String(unprocessedPrefix + "auto"),
+	}, func(p *s3.ListObjectsV2Output, last bool) bool {
+		fmt.Printf("Processing page %d\n", i)
+
+		for _, obj := range p.Contents {
+			fmt.Printf("Handling file: %s\n", *obj.Key)
+			downloadFile(downloader, *obj.Key)
+			processFile(*obj.Key)
+			uploadFile(uploader, *obj.Key)
+			filesToMove = append(filesToMove, *obj.Key)
+		}
+
+		return true
+	})
+
+	for _, fileName := range filesToMove {
+		svc.CopyObject(&s3.CopyObjectInput{
+			CopySource: aws.String(bucketName + "/" + fileName),
+			Bucket: aws.String(bucketName),
+			Key: aws.String(processedPrefix + "/" + filepath.Base(fileName)),
+		})
+		svc.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: aws.String(bucketName),
+			Key: aws.String(fileName),
+		})
 	}
 }
