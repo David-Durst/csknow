4609ef8e8f69eec70b170a6c75a4dc9bf59eb7f8
David Durst <davidbdurst@gmail.com>
2021-03-02 15:57:13 -0500

    Made really fast option

--- docs/src/controls.ts
+++ docs/src/controls.ts
@@ -6,9 +6,10 @@ let rwdImg: HTMLImageElement = null;
 let playSpeed: number = 0;
 let curInterval: number = null;
 const speed1x: number = 32
+let ticksPerUpdate: number = 1
 
 function incrementTick() {
-    const tickIncrement = playSpeed > 0 ? 1 : -1
+    const tickIncrement = ticksPerUpdate * (playSpeed > 0 ? 1 : -1)
     const newTick = parseInt(tickSelector.value) + tickIncrement
     if (newTick <= parseInt(tickSelector.max) && newTick >= parseInt(tickSelector.min)) {
         tickSelector.value = newTick.toString()
@@ -38,6 +39,7 @@ function playXSpeed(speed: number, playButton: boolean = false) {
         rwdImg.className = "btn btn-secondary playButton"
         ffImg.className = "btn btn-secondary playButton"
         isPlaying = false
+        ticksPerUpdate = 1
     }
     else {
         playSpeed = speed
@@ -53,8 +55,12 @@ function playFaster() {
     if (playSpeed <= 0) {
         playXSpeed(speed1x * 2)
     }
-    else if (playSpeed < 16 * speed1x) {
+    else if (playSpeed < 8 * speed1x) {
         playXSpeed(playSpeed * 2)
+    }
+    else if (playSpeed == 8 * speed1x) {
+        ticksPerUpdate = 10
+        playXSpeed(playSpeed)
     }
     if (playSpeed == speed1x * 2) {
         ffImg.className = "btn btn-blue playButton"
@@ -62,10 +68,10 @@ function playFaster() {
     else if (playSpeed == speed1x * 4) {
         ffImg.className = "btn btn-green playButton"
     }
-    else if (playSpeed == speed1x * 8) {
+    else if (playSpeed == speed1x * 8 && ticksPerUpdate == 1) {
         ffImg.className = "btn btn-yellow playButton"
     }
-    else if (playSpeed == speed1x * 16) {
+    else if (playSpeed == speed1x * 8 && ticksPerUpdate == 10) {
         ffImg.className = "btn btn-red playButton"
     }
 }
@@ -73,9 +79,14 @@ function playFaster() {
 function playSlower() {
     ffImg.className = "btn btn-secondary playButton"
     if (playSpeed >= 0) {
+        ticksPerUpdate = 1
         playXSpeed(speed1x * -1)
     }
-    else if (playSpeed > -8 * speed1x) {
+    else if (playSpeed > -4 * speed1x) {
+        playXSpeed(playSpeed * 2)
+    }
+    else if (playSpeed == -4 * speed1x) {
+        ticksPerUpdate = 10
         playXSpeed(playSpeed * 2)
     }
     if (playSpeed == speed1x * -1) {
