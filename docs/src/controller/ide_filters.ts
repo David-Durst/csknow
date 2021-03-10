6b07112d5a0ac6baaf61017507fe71dd81d55115
David Durst <davidbdurst@gmail.com>
2021-03-10 17:34:58 -0500

    part way to file uploads

--- docs/src/controller/ide_filters.ts
+++ docs/src/controller/ide_filters.ts
@@ -6,6 +6,7 @@ import {setCurTickIndex, setTickSelectorMax } from "./tickSelector";
 
 let customFilterEditor: HTMLDivElement = null
 let customFilterText: HTMLDivElement = null
+let filterUploadButton: HTMLInputElement = null
 
 
 let shouldFilterCustom: boolean = false
@@ -41,8 +42,26 @@ export function clearCustomFilter() {
     shouldFilterCustom = false
 }
 
+function updateCustomFilterProgram(e: any) {
+    filterUploadButton.disabled = true
+    e.currentTarget.files[0].text().then((text: string) => {
+        // @ts-ignore
+        window.editor.setValue(text)
+        filterUploadButton.disabled = false
+    })
+}
+
+function saveCustomFilterProgram() {
+    // @ts-ignore
+    const blob = new Blob([window.editor.getValue()],
+        {type: "text/javascript;charset=utf-8"});
+    //FileSaver.
+}
+
 export function setupCustomFilters() {
     customFilterEditor = document.querySelector<HTMLDivElement>("#editor")
     document.querySelector<HTMLButtonElement>("#custom_filter").addEventListener("click", customFilterButton)
     customFilterText = document.querySelector<HTMLDivElement>("#custom_filter_text")
+    filterUploadButton = document.querySelector<HTMLInputElement>("#upload_custom_filter")
+    document.querySelector<HTMLInputElement>("#upload_custom_filter").addEventListener("change", updateCustomFilterProgram)
 }
