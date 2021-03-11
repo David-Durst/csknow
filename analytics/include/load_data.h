12752904670c2920f83413e8306715199b1120cd
David Durst <davidbdurst@gmail.com>
2021-03-10 19:52:33 -0800

    starting c++ analytics

---
+++ analytics/include/load_data.h
@@ -0,0 +1,8 @@
+//
+// Created by durst on 3/10/21.
+//
+
+#ifndef CSKNOW_LOAD_DATA_H
+#define CSKNOW_LOAD_DATA_H
+
+#endif //CSKNOW_LOAD_DATA_H
---
+++ analytics/src/lib/load_data.cpp
@@ -0,0 +1,4 @@
+//
+// Created by durst on 3/10/21.
+//
+
