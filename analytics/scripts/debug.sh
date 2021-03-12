d36bf75edd658e8e2c4842d3c2dc63dffb779114
David Durst <davidbdurst@gmail.com>
2021-03-11 20:08:10 -0800

    scripts using generalized path

--- analytics/scripts/debug.sh
+++ analytics/scripts/debug.sh
@@ -1,5 +1,22 @@
-mkdir -p build
-cd build
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+
+mkdir -p ${script_dir}/../build
+cd ${script_dir}/../build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j4
-./csknow /home/durst/big_dev/csknow/local_data 
+./csknow ${script_dir}/../../local_data 
