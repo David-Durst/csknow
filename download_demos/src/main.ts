95a5bf4b5d7952c39d4ac535c6aa4bfc0966607d
David Durst <davidbdurst@gmail.com>
2021-04-08 16:24:23 -0700

    printing old events

--- download_demos/src/main.ts
+++ download_demos/src/main.ts
@@ -1,1 +1,14 @@
-console.log('hi')
+import HLTV from 'hltv';
+const date = require('date-and-time');
+
+const now = new Date();
+const nowString = date.format(now, 'YYYY-MM-DD');
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 5);
+const yesterdayString = date.format(yesterday, 'YYYY-MM-DD');
+
+console.log(yesterdayString)
+console.log(nowString)
+HLTV.getPastEvents({ startDate: yesterdayString, endDate: nowString }).then(res => {
+    console.log(res)
+})
