1422ea9f94a0a1afe3861b17388bb381a0d9ca2a
David Durst <davidbdurst@gmail.com>
2021-04-10 22:59:54 -0700

    bash start setting up enviornment

--- download_pro_demos/bash_start.sh
+++ download_pro_demos/bash_start.sh
@@ -1,3 +1,25 @@
 mkdir -p ../demos
+
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+
+iam_role=$(cat ${script_dir}/../private/.aws_csgo_server_role)
+
+
 docker run --name durst_pro_demos_downloader \
+    -e RUNNING_IN_EC2=1 -e ROLE=${iam_role} \
     --rm -it durst/csgo-pro-demos-downloader:0.1 /bin/bash
