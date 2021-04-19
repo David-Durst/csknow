e9154897329875217c162c9439ac1923416d7cdb
David Durst <davidbdurst@gmail.com>
2021-04-19 16:13:13 -0700

    adding mysql config

--- sql_loader/scripts/bash_start.sh
+++ sql_loader/scripts/bash_start.sh
@@ -2,7 +2,7 @@ docker run --name durst_sql_loader \
     --rm -it \
     --mount type=bind,source="$(pwd)"/../local_data,target=/go/src/local_data2 \
     --entrypoint /bin/bash \
-    --cap-add=SYS_PTRACE \
-    --cap-add=SYS_ADMIN \
+    -p 127.0.0.1:3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=local_password \
     durst/sql_loader:0.1
 
--- sql_loader/scripts/start.sh
+++ sql_loader/scripts/start.sh
@@ -1,6 +1,6 @@
 docker run --name durst_sql_loader \
     --rm \
     --mount type=bind,source="$(pwd)"/../local_data,target=/go/src/local_data2 \
-    --cap-add=SYS_PTRACE \
-    --cap-add=SYS_ADMIN \
+    -p 127.0.0.1:3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=local_password \
     durst/sql_loader:0.1
