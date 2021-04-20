60313d58aa16a27e28ad15ee399dbabe17a69058
David Durst <davidbdurst@gmail.com>
2021-04-20 08:19:10 -0700

    loading password

--- sql_loader/scripts/start.sh
+++ sql_loader/scripts/start.sh
@@ -1,6 +1,24 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+pass=$(cat ${script_dir}/../private/.mysql_password)
+
 docker run --name durst_sql_loader \
     --rm \
     --mount type=bind,source="$(pwd)"/../local_data,target=/go/src/local_data2 \
     -p 127.0.0.1:3306:3306 \
-    -e MYSQL_ROOT_PASSWORD=local_password \
+    -e MYSQL_ROOT_PASSWORD=${pass} \
     durst/sql_loader:0.1
