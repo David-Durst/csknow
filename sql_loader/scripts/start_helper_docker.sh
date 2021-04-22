063d1b433f8bcb4f5d49c129dae59e569a12d6fc
David Durst <davidbdurst@gmail.com>
2021-04-22 15:03:36 -0700

    loading fact tables

--- sql_loader/scripts/start_helper_docker.sh
+++ sql_loader/scripts/start_helper_docker.sh
@@ -2,6 +2,10 @@ mysql --host=localhost --user=root --password=${pass} -e "source /sql/create_sch
 
 dir_path=/local_data2
 
+mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/fact_table_equipment.csv' INTO TABLE equipment FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/fact_table_game_types.csv' INTO TABLE game_types FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/fact_table_hit_groups.csv' INTO TABLE hit_groups FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+
 for f in ${dir_path}/defusals/*.csv
 do
     mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${f}' INTO TABLE defusals FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
