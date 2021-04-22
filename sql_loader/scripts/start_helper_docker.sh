93ed3dbdce300a4119d7717b897b72ff40ad2b92
David Durst <davidbdurst@gmail.com>
2021-04-22 15:15:30 -0700

    loading all files into mysql

--- sql_loader/scripts/start_helper_docker.sh
+++ sql_loader/scripts/start_helper_docker.sh
@@ -5,10 +5,13 @@ dir_path=/local_data2
 mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/fact_table_equipment.csv' INTO TABLE equipment FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/fact_table_game_types.csv' INTO TABLE game_types FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/fact_table_hit_groups.csv' INTO TABLE hit_groups FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/global_games.csv' INTO TABLE games FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 
-for f in ${dir_path}/defusals/*.csv
+for name in players rounds ticks player_at_tick spotted weapon_fire kills hurt grenades flashed grenade_trajectories plants defusals explosions
 do
-    mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${f}' INTO TABLE defusals FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+    for f in ${dir_path}/${name}/*.csv
+    do
+        mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${f}' INTO TABLE ${name} FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+    done
 done
-
 #defusals  explosions flashed  grenade_trajectories  grenades  hurt  kills  plants  player_at_tick  players  spotted  ticks  weapon_fire
