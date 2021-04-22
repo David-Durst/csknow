abd37851ce0c22a48bca9d4f41fd8b7a266add9c
David Durst <davidbdurst@gmail.com>
2021-04-22 13:51:13 -0700

    setting password in helper in docker

--- sql_loader/scripts/start_helper.sh
+++ sql_loader/scripts/start_helper.sh
@@ -2,4 +2,4 @@ until docker exec durst_sql_loader mysql --host=localhost --user=root --password
 do
     sleep 2;
 done
-docker exec durst_sql_loader bash /scripts/start_helper_docker.sh
+docker exec -e pass=${pass} durst_sql_loader bash /scripts/start_helper_docker.sh
