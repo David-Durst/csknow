fad312ecd289bc2506b6e7a1276e90a774d266c6
David Durst <davidbdurst@gmail.com>
2021-04-24 12:41:05 -0700

    cleaning up error message

--- demo_parser/parse.go
+++ demo_parser/parse.go
@@ -705,7 +705,7 @@ func processFile(unprocessedKey string, idState * IDState, firstRun bool, gameTy
 
 	err = p.ParseToEnd()
 	if err != nil {
-		fmt.Printf("Error in parsing. T score %d, CT score %d, progress: %f, error:\n %s\n. ",
+		fmt.Printf("Error in parsing. T score %d, CT score %d, progress: %f, error:\n %s\n",
 			p.GameState().TeamTerrorists().Score(), p.GameState().TeamCounterTerrorists().Score(), p.Progress(), err.Error())
 	}
 }
