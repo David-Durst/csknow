72fd07f795a14dc38e06b1008de8e2c69d40728f
David Durst <davidbdurst@gmail.com>
2021-04-28 15:27:47 -0700

    removing headers

--- download_s3_csvs/merge.sh
+++ download_s3_csvs/merge.sh
@@ -2,7 +2,7 @@ dir_path=../local_data
 
 for name in players rounds ticks player_at_tick spotted weapon_fire kills hurt grenades flashed grenade_trajectories plants defusals explosions
 do
-    awk '(NR == 1) || (FNR > 1)' ${dir_path}/${name}/*.csv > ${dir_path}/${name}.csv
+    awk 'FNR > 1' ${dir_path}/${name}/*.csv > ${dir_path}/${name}.csv
 done
 
 #for name in ticks player_at_tick spotted weapon_fire kills hurt grenades flashed grenade_trajectories plants defusals explosions
