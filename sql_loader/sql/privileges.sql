9486c1fde113a4b2ed193e78f34d0bc8ad0c16f4
David Durst <davidbdurst@gmail.com>
2021-04-29 10:54:06 -0700

    added postgres schema creation

--- sql_loader/sql/privileges.sql
+++ sql_loader/sql/privileges.sql
@@ -1,3 +1,6 @@
+CREATE USER readonly WITH PASSWORD 'readonly';
+USE csknow;
+GRANT CONNECT ON DATABASE csknow TO readonly;
 CREATE USER 'readonly'@'%' IDENTIFIED BY 'readonly';
 GRANT SELECT ON *.* TO 'readonly'@'%';
 FLUSH PRIVILEGES;
