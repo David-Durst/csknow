6ace6a45c04017b94d1d396e7e44c648acbdee62
David Durst <davidbdurst@gmail.com>
2021-04-29 12:39:28 -0700

    trying postgres loading everything

--- sql_loader/scripts/start_helper_docker.sh
+++ sql_loader/scripts/start_helper_docker.sh
@@ -2,15 +2,19 @@ psql --user=postgres -f /sql/create_schema_v7_postgres.sql -d csknow
 
 dir_path=/local_data
 
-#mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/dimension_table_equipment.csv' INTO TABLE equipment FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
+psql --user=postgres -d csknow -c "\\copy equipment FROM '${dir_path}/dimension_table_equipment.csv' csv header;"
+psql --user=postgres -d csknow -c "\\copy game_types FROM '${dir_path}/dimension_table_game_types.csv' csv header;"
+psql --user=postgres -d csknow -c "\\copy hit_groups FROM '${dir_path}/dimension_table_hit_groups.csv' csv header;"
+psql --user=postgres -d csknow -c "\\copy games FROM '${dir_path}/global_games.csv' csv header;"
+#mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE  INTO TABLE equipment FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 #mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/dimension_table_game_types.csv' INTO TABLE game_types FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 #mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/dimension_table_hit_groups.csv' INTO TABLE hit_groups FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 #mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/global_games.csv' INTO TABLE games FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 #
-#for name in players rounds ticks player_at_tick spotted weapon_fire kills hurt grenades flashed grenade_trajectories plants defusals explosions
-#do
-#    echo "loading ${name}$"
-#    mysqlimport --fields-terminated-by=, --user=root --password=${pass} csknow ${dir_path}/${name}.csv
-#done
+for name in players rounds ticks player_at_tick spotted weapon_fire kills hurt grenades flashed grenade_trajectories plants defusals explosions
+do
+    echo "loading ${name}$"
+    psql --user=postgres -d csknow -c "\\copy ${name} FROM '${dir_path}/${name}.csv' csv header;"
+done
 echo "done loading"
 #defusals  explosions flashed  grenade_trajectories  grenades  hurt  kills  plants  player_at_tick  players  spotted  ticks  weapon_fire
