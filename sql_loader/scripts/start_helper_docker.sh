b6e1dc5594d42ddf22e81457ac10b38ae50f12dc
David Durst <davidbdurst@gmail.com>
2021-04-29 13:01:26 -0700

    hadnling nulls correctly for postgres

--- sql_loader/scripts/start_helper_docker.sh
+++ sql_loader/scripts/start_helper_docker.sh
@@ -2,10 +2,10 @@ psql --user=postgres -f /sql/create_schema_v7_postgres.sql -d csknow
 
 dir_path=/local_data
 
-psql --user=postgres -d csknow -c "\\copy equipment FROM '${dir_path}/dimension_table_equipment.csv' csv header;"
-psql --user=postgres -d csknow -c "\\copy game_types FROM '${dir_path}/dimension_table_game_types.csv' csv header;"
-psql --user=postgres -d csknow -c "\\copy hit_groups FROM '${dir_path}/dimension_table_hit_groups.csv' csv header;"
-psql --user=postgres -d csknow -c "\\copy games FROM '${dir_path}/global_games.csv' csv header;"
+psql --user=postgres -d csknow -c "\\copy equipment FROM '${dir_path}/dimension_table_equipment.csv' csv header null '\\N';"
+psql --user=postgres -d csknow -c "\\copy game_types FROM '${dir_path}/dimension_table_game_types.csv' csv header null '\\N';"
+psql --user=postgres -d csknow -c "\\copy hit_groups FROM '${dir_path}/dimension_table_hit_groups.csv' csv header null '\\N';"
+psql --user=postgres -d csknow -c "\\copy games FROM '${dir_path}/global_games.csv' csv header null '\\N';"
 #mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE  INTO TABLE equipment FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 #mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/dimension_table_game_types.csv' INTO TABLE game_types FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
 #mysql --host=localhost --user=root --password=${pass} -e "LOAD DATA INFILE '${dir_path}/dimension_table_hit_groups.csv' INTO TABLE hit_groups FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" csknow
@@ -15,7 +15,7 @@ for name in players rounds ticks player_at_tick spotted weapon_fire kills hurt g
 do
     echo "loading ${name}$"
     psql --user=postgres -d csknow -c "ALTER TABLE ${name} SET UNLOGGED;"
-    psql --user=postgres -d csknow -c "\\copy ${name} FROM '${dir_path}/${name}.csv' csv;"
+    psql --user=postgres -d csknow -c "\\copy ${name} FROM '${dir_path}/${name}.csv' csv null '\\N';"
     psql --user=postgres -d csknow -c "ALTER TABLE ${name} SET LOGGED;"
 done
 psql --user=postgres -f /sql/create_fk_v7_postgres.sql -d csknow
