fc2a3e8e77703f2c297b9e495f769382b79f68c4
David Durst <davidbdurst@gmail.com>
2021-04-29 15:32:39 -0700

    added temporary tables

--- cstore_loader/sql/privileges.sql
+++ cstore_loader/sql/privileges.sql
@@ -1,4 +1,5 @@
 CREATE USER 'readonly'@'%' IDENTIFIED BY 'readonly';
 GRANT SELECT ON *.* TO 'readonly'@'%';
+GRANT CREATE TEMPORARY TABLES ON *.* TO 'readonly'@'%';
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS csknow
