4e38b6f974e79723761a25f1e313b127253de178
David Durst <davidbdurst@gmail.com>
2021-04-30 13:49:33 -0400

    added base postgres running headshots
    but it's really really slow

---
+++ sql_analytics/running_headshots_postgres.sql
@@ -0,0 +1,29 @@
+select h.tick_id                                        as base_tick_id,
+       pat.tick_id                                      as tick_id,
+       pat.player_id,
+       h.hit_group,
+       pat.pos_x,
+       pat.pos_y,
+       pat.pos_z,
+       g.game_tick_rate,
+       t.demo_tick_number,
+       t.game_tick_number,
+       pat.is_airborne,
+       warmup,
+       lead(pos_x) over (order by pat.tick_id)              as leadx,
+       lag(pos_x) over (order by pat.tick_id)               as lagx,
+       lead(pos_y) over (order by pat.tick_id)              as leady,
+       lag(pos_y) over (order by pat.tick_id)               as lagy,
+       lead(pos_z) over (order by pat.tick_id)              as leadz,
+       lag(pos_z) over (order by pat.tick_id)               as lagz,
+       lead(t.demo_tick_number) over (order by pat.tick_id) as lead_tick,
+       lag(t.demo_tick_number) over (order by pat.tick_id)  as lag_tick
+from hurt h
+         join player_at_tick pat on
+                h.tick_id - 1 <= pat.tick_id and h.tick_id + 1 >= pat.tick_id and h.attacker = pat.player_id
+         join ticks t on pat.tick_id = t.id
+         join rounds r on t.round_id = r.id
+         join games g on r.game_id = g.id
+where hit_group = 1
+  and warmup = false
+order by tick_id
