5cecf1c0468e817cc40640d5a2a5a01a082d3350
David Durst <davidbdurst@gmail.com>
2021-04-30 08:39:52 -0700

    postgres running two scripts in wile loop

--- sql_loader/scripts/start_helper.sh
+++ sql_loader/scripts/start_helper.sh
@@ -2,6 +2,9 @@ until docker exec durst_sql_loader createdb csknow --user=postgres
 do
     sleep 2;
 done
-docker exec durst_sql_loader psql --user=postgres -f /sql/privileges.sql -d csknow
+until docker exec durst_sql_loader psql --user=postgres -f /sql/privileges.sql -d csknow
+do
+    sleep 2;
+done
 docker exec durst_sql_loader bash /scripts/start_helper_docker.sh
 
