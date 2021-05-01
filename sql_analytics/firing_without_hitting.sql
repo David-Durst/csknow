197fef0609951a6a44b6260b4d6a2d4cce2c67df
David Durst <davidbdurst@gmail.com>
2021-05-01 18:09:34 -0400

    burts seem reasonable

--- sql_analytics/firing_without_hitting.sql
+++ sql_analytics/firing_without_hitting.sql
@@ -7,40 +7,47 @@ create temporary table firing_no_hits (
                             when victim is not null then game_tick_number
                             when num_hits = 0 then -1
                             else 0 end)
-                        over (partition by round_id, num_hits order by tick_id) as last_hit_neg
+                        over (partition by round_id, burst_id, num_hits order by tick_id) as last_hit_neg
              from (
-                      select g.id                                           as game_id,
-                             r.id                                           as round_id,
-                             t.id                                           as tick_id,
-                             t.game_tick_number,
-                             wf.shooter,
-                             e.name                                         as weapon_name,
-                             h.victim,
-                             start_ticks.game_tick_number                   as start_game_tick_number,
-                             sum(case when h.victim is not null then 1 else 0 end)
-                                 over (partition by round_id order by t.id, shooter, victim) as num_hits
-                      from ticks t
-                               join rounds r on t.round_id = r.id
-                               join games g on r.game_id = g.id
-                               inner join weapon_fire wf on t.id = wf.tick_id
-                               join equipment e on e.id = wf.weapon
-                               left join hurt h on t.id = h.tick_id
-                               join ticks start_ticks on start_ticks.id = r.start_tick
-                  ) i1
-         ) i2
+                  select *,
+                         sum(case when game_tick_number - last_game_tick < 128 then 0 else 1 end)
+                             over (partition by round_id order by tick_id) as burst_id
+                  from (
+                           select g.id                                                            as game_id,
+                                  r.id                                                            as round_id,
+                                  t.id                                                            as tick_id,
+                                  t.game_tick_number,
+                                  wf.shooter,
+                                  e.name                                                          as weapon_name,
+                                  h.victim,
+                                  start_ticks.game_tick_number                                    as start_game_tick_number,
+                                  sum(case when h.victim is not null then 1 else 0 end)
+                                      over (partition by round_id order by t.id, shooter, victim) as num_hits,
+                                  lag(t.game_tick_number) over (order by tick_id, shooter, victim) as last_game_tick
+                           from ticks t
+                                    join rounds r on t.round_id = r.id
+                                    join games g on r.game_id = g.id
+                                    inner join weapon_fire wf on t.id = wf.tick_id
+                                    join equipment e on e.id = wf.weapon
+                                    left join hurt h on t.id = h.tick_id
+                                    join ticks start_ticks on start_ticks.id = r.start_tick
+                       ) i1
+                  ) i2
+         ) i3
     where last_hit_neg > 0
 );
 
 # this finds longest times between hits, not tight sequences of misses - the one in round 166 is a save, where just knifing at end without point
-select ticks_between_hits, round_id, game_tick_number
+select ticks_between_hits, round_id, game_tick_number, burst_id
 from (
-     select ticks_between_hits, round_id, game_tick_number
+     select ticks_between_hits, round_id, game_tick_number, burst_id
      from (
               select ticks_between_hits,
                      last_hit_neg,
                      lead(last_hit_neg) over (order by tick_id, shooter, victim) as next_last_hit,
                      round_id,
-                     game_tick_number
+                     game_tick_number,
+                     burst_id
               from firing_no_hits
               order by tick_id, shooter, victim
           ) i1
@@ -48,18 +55,3 @@ from (
      ) i2
 order by ticks_between_hits DESC
 limit 10000;
-
-
-select *,
-       sum(case when game_tick_number - last_game_tick < 128 then 0 else 1 end)
-           over (partition by round_id order by tick_id) as burst_id
-from (
-         select ticks_between_hits,
-                last_hit_neg,
-                lag(game_tick_number) over (order by tick_id, shooter, victim) as last_game_tick,
-                round_id,
-                game_tick_number,
-                tick_id
-         from firing_no_hits
-         order by tick_id, shooter, victim
-     ) i1
