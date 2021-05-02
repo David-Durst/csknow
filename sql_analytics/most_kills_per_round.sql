1172a4f7cd07f6545574eb8a6cf155c851570f91
David Durst <davidbdurst@gmail.com>
2021-05-02 17:50:29 -0400

    filtering by warmup in kills per round

--- sql_analytics/most_kills_per_round.sql
+++ sql_analytics/most_kills_per_round.sql
@@ -2,5 +2,6 @@ select killer, round_id, count(*) as kills_per_round
 from kills k
     join ticks t on k.tick_id = t.id
     join rounds r on r.id = t.round_id
+where not r.warmup
 group by killer, round_id
 order by kills_per_round desc
