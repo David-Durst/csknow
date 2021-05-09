73f518b118c763cfa09b2db2a0f1f4ed366c66d3
David Durst <davidbdurst@gmail.com>
2021-05-09 16:11:48 -0400

    running headshot postgres runs well

--- sql_analytics/running_headshots_postgres.sql
+++ sql_analytics/running_headshots_postgres.sql
@@ -1,4 +1,57 @@
-select h.tick_id                                        as base_tick_id,
+select *
+from (
+         select base_tick_id,
+                tick_id,
+                player_id,
+                pos_x,
+                pos_y,
+                pos_z,
+                leadx,
+                lagx,
+                lead_tick,
+                lag_tick,
+                sqrt(pow(leadx - lagx, 2) + pow(leady - lagy, 2) + pow(leadz - lagz, 2)) /
+                NULLIF((lead_game_tick - lag_game_tick),0) * game_tick_rate as velocity,
+                game_tick_rate
+         from (
+                  select h.tick_id                                        as base_tick_id,
+                         pat.tick_id                                      as tick_id,
+                         pat.player_id,
+                         h.hit_group,
+                         pat.pos_x,
+                         pat.pos_y,
+                         pat.pos_z,
+                         g.game_tick_rate,
+                         t.demo_tick_number,
+                         t.game_tick_number,
+                         pat.is_airborne,
+                         warmup,
+                         lead(pos_x) over (order by pat.tick_id)              as leadx,
+                         lag(pos_x) over (order by pat.tick_id)               as lagx,
+                         lead(pos_y) over (order by pat.tick_id)              as leady,
+                         lag(pos_y) over (order by pat.tick_id)               as lagy,
+                         lead(pos_z) over (order by pat.tick_id)              as leadz,
+                         lag(pos_z) over (order by pat.tick_id)               as lagz,
+                         lead(t.demo_tick_number) over (order by pat.tick_id) as lead_tick,
+                         lag(t.demo_tick_number) over (order by pat.tick_id)  as lag_tick,
+                         lead(t.game_tick_number) over (order by pat.tick_id) as lead_game_tick,
+                         lag(t.game_tick_number) over (order by pat.tick_id)  as lag_game_tick
+                  from hurt h
+                           join player_at_tick pat on
+                          (pat.tick_id between h.tick_id - 1 and h.tick_id + 1) and h.attacker = pat.player_id
+                           join ticks t on pat.tick_id = t.id
+                           join rounds r on t.round_id = r.id
+                           join games g on r.game_id = g.id
+                  where hit_group = 1
+                    and warmup = false
+                    and not is_airborne
+                  order by base_tick_id
+              ) i1
+         ) i2
+where base_tick_id = tick_id and velocity > 170;
+
+
+explain analyze select h.tick_id                                        as base_tick_id,
        pat.tick_id                                      as tick_id,
        pat.player_id,
        h.hit_group,
@@ -18,19 +71,20 @@ select h.tick_id                                        as base_tick_id,
        lag(pos_z) over (order by pat.tick_id)               as lagz,
        lead(t.demo_tick_number) over (order by pat.tick_id) as lead_tick,
        lag(t.demo_tick_number) over (order by pat.tick_id)  as lag_tick
-from ticks t
-         join player_at_tick pat on
-                t.id = pat.tick_id - 1 or
-                t.id = pat.tick_id + 1 or
-                t.id = pat.tick_id
-         join hurt h on h.tick_id = t.id and h.attacker = pat.player_id
-         join rounds r on t.round_id = r.id
-         join games g on r.game_id = g.id
-where hit_group = 1
-  and warmup = false
-  and not is_airborne
-order by tick_id;
+        from hurt h
+            join player_at_tick pat on
+                (pat.tick_id between h.tick_id - 1 and h.tick_id + 1) and h.attacker = pat.player_id
+            join ticks t on pat.tick_id = t.id
+            join rounds r on t.round_id = r.id
+            join games g on r.game_id = g.id
+        where hit_group = 1
+          and warmup = false
+          and not is_airborne
+        order by base_tick_id;
 
+set enable_hashjoin = off;
+set enable_mergejoin = off;
+set join_collapse_limit = 8;
 
 select tbl.table_schema,
        tbl.table_name
--- sql_analytics/shot_from_behind.sql
+++ sql_analytics/shot_from_behind.sql
@@ -1,4 +1,5 @@
 select *
 from hurt h
-    join player_at_tick pat1 on h.tick_id = pat1.tick_id and h.victim = pat1.player_id
-    join player_at_tick pat2 on h.tick_id = pat2.tick_id and h.attacker = pat2.player_id
+    join ticks t on h.tick_id = t.id
+    join player_at_tick pat1 on t.id = pat1.tick_id and h.victim = pat1.player_id
+    join player_at_tick pat2 on t.id = pat2.tick_id and h.attacker = pat2.player_id
