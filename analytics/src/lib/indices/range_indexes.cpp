2628a2cd2a2f4b4d8bb7efa104bae205eea6e005
David Durst <davidbdurst@gmail.com>
2021-05-15 15:55:32 -0700

    added hashmap indexes, rnage indexing fails on spotted

--- analytics/src/lib/indices/range_indexes.cpp
+++ analytics/src/lib/indices/range_indexes.cpp
@@ -2,16 +2,29 @@
 #include <algorithm>
 #include <iostream>
 #include <assert.h>
+using std::cout;
+using std::endl;
 
-template <class T>
-void buildRangeIndex(const vector<T> primaryKeyCol, int64_t primarySize, const T * foreignKeyCol,
+void buildRangeIndex(const vector<int64_t> primaryKeyCol, int64_t primarySize, const int64_t * foreignKeyCol,
                           int64_t foreignSize, RangeIndex rangeIndexCol) {
     for (int64_t primaryIndex = 0, foreignIndex = 0; primaryIndex < primarySize; primaryIndex++) {
         // sometimes have mistakes where point to 0 as uninitalized, skip entries
         if(foreignKeyCol[foreignIndex] == 0 &&  primaryKeyCol[primaryIndex] > 0) {
             for (; foreignKeyCol[foreignIndex] == 0; foreignIndex++) ;
         }
-        assert(foreignKeyCol[foreignIndex] >= primaryKeyCol[primaryIndex]);
+        if (foreignKeyCol[foreignIndex] < primaryKeyCol[primaryIndex]) {
+            cout << "bad foreign " << foreignIndex  << " val " << foreignKeyCol[foreignIndex]
+                 << " and primary " << primaryIndex << " val " << primaryKeyCol[primaryIndex] << endl;
+            assert(foreignKeyCol[foreignIndex] >= primaryKeyCol[primaryIndex]);
+        }
+        if (foreignIndex >= foreignSize) {
+            cout << "foreign index " << foreignIndex  << " larger than size " << foreignSize << endl;
+            assert(foreignKeyCol[foreignIndex] < primaryKeyCol[primaryIndex]);
+        }
+        if (primaryIndex == 29) {
+            int x = 1;
+        }
+        assert(foreignIndex < foreignSize);
         if (primaryIndex >= foreignSize || foreignKeyCol[foreignIndex] > primaryKeyCol[primaryIndex]) {
             rangeIndexCol[primaryIndex].minId = -1;
             rangeIndexCol[primaryIndex].maxId = -1;
@@ -23,32 +36,21 @@ void buildRangeIndex(const vector<T> primaryKeyCol, int64_t primarySize, const T
     }
 }
 
-template <class T>
-void buildHashmapIndex(const vector<T> primaryKeyCol, int64_t primarySize, const T * foreignKeyStartCol,
-                       const vector<T *> foreignKeyEndCols, int64_t foreignSize, RangeIndex rangeIndexCol) {
-    int64_t primaryIndex = 0, startForeignIndex = 0, endForeignIndex = 0;
-    while (primaryIndex < primarySize) {
-        // sometimes have mistakes where point to 0 as uninitalized, skip entries
-        if(foreignKeyStartCol[startForeignIndex] == 0 &&  primaryKeyCol[primaryIndex] > 0) {
-            for (; foreignKeyStartCol[startForeignIndex] == 0; startForeignIndex++) ;
+void buildHashmapIndex(const vector<int64_t *> foreignKeyCols, int64_t foreignSize, HashmapIndex hashIndexCol) {
+    for (int64_t foreignIndex = 0; foreignIndex < foreignSize; foreignIndex++) {
+        // collect all primary key entries that are in range the foreign keys
+        int64_t minPrimaryIndex = foreignKeyCols[0][foreignIndex], maxPrimaryIndex = foreignKeyCols[0][foreignIndex];
+        for (int col = 1; col < foreignKeyCols.size(); col++) {
+            minPrimaryIndex = std::min(minPrimaryIndex, foreignKeyCols[col][foreignIndex]);
+            maxPrimaryIndex = std::max(maxPrimaryIndex, foreignKeyCols[col][foreignIndex]);
         }
-        assert(foreignKeyStartCol[startForeignIndex] >= primaryKeyCol[primaryIndex]);
-        if (primaryIndex >= foreignSize || foreignKeyStartCol[startForeignIndex] > primaryKeyCol[primaryIndex]) {
-            rangeIndexCol[primaryIndex].minId = -1;
-            rangeIndexCol[primaryIndex].maxId = -1;
-        }
-
-        rangeIndexCol[primaryIndex].minId = startForeignIndex;
-        for (; foreignKeyStartCol[startForeignIndex] == primaryKeyCol[primaryIndex]; primaryIndex++) {
-            for (endForeignIndex = startForeignIndex;
-                 foreignKeyStartCol[endForeignIndex] != 0 && foreignKeyStartCol[endForeignIndex] <= primaryKeyCol[primaryIndex];
-                 endForeignIndex++) ;
-            for (const auto foreignKeyEndCol : foreignKeyEndCols) {
-                if (foreignKeyEndCol[endForeignIndex] <= prim)
+        // insert into all key cols in range
+        for (int64_t primaryIndex = minPrimaryIndex; primaryIndex <= maxPrimaryIndex; primaryIndex++) {
+            if (hashIndexCol.find(primaryIndex) == hashIndexCol.end()) {
+                hashIndexCol.insert({primaryIndex, {}});
             }
-            rangeIndexCol[primaryIndex].maxId = endForeignIndex;
+            hashIndexCol[primaryIndex].push_back(foreignIndex);
         }
-        for (; foreignKeyStartCol[startForeignIndex] == primaryKeyCol[primaryIndex]; startForeignIndex++) ;
     }
 }
 
@@ -60,13 +62,23 @@ void buildRangeIndexes(Equipment & equipment, GameTypes & gameTypes, HitGroups &
     buildRangeIndex(rounds.id, rounds.size, ticks.roundId, ticks.size, rounds.ticksPerRound);
     buildRangeIndex(ticks.id, ticks.size, playerAtTick.tickId, playerAtTick.size, ticks.playersPerTick);
     buildRangeIndex(ticks.id, ticks.size, spotted.tickId, spotted.size, ticks.spottedPerTick);
-    buildRangeIndex(ticks.id, ticks.size, kills.tickId, kills.size, ticks.killsPerTick);
-    buildRangeIndex(ticks.id, ticks.size, hurt.tickId, hurt.size, ticks.hurtPerTick);
-    buildOverlappingRangeIndex(ticks.id, )
-    buildPointRangeIndex(ticks.id, ticks.size, grenades.throwTick, grenades.size, ticks.grenadesThrowPerTick);
-    buildPointRangeIndex(ticks.id, ticks.size, grenades.activeTick, grenades.size, ticks.grenadesActivePerTick);
-    buildPointRangeIndex(ticks.id, ticks.size, grenades.expiredTick, grenades.size, ticks.grenadesExpiredPerTick);
-    buildPointRangeIndex(ticks.id, ticks.size, grenades.destroyTick, grenades.size, ticks.grenadesDestroyedPerTick);
-    buildPointRangeIndex(ticks.id, ticks.size, flashed.tickId, flashed.size, ticks.flashedPerTick);
-    buildRangeIndex(ticks.id, ticks.size, plants.st, plants.size, ticks.plantsPerTick);
+    buildHashmapIndex({kills.tickId}, kills.size, ticks.killsPerTick);
+    buildHashmapIndex({hurt.tickId}, hurt.size, ticks.hurtPerTick);
+    buildHashmapIndex({grenades.throwTick, grenades.activeTick, grenades.expiredTick, grenades.destroyTick}, grenades.size, ticks.grenadesPerTick);
+    buildHashmapIndex({grenades.throwTick}, grenades.size, ticks.grenadesThrowPerTick);
+    buildHashmapIndex({grenades.activeTick}, grenades.size, ticks.grenadesActivePerTick);
+    buildHashmapIndex({grenades.expiredTick}, grenades.size, ticks.grenadesExpiredPerTick);
+    buildHashmapIndex({grenades.activeTick}, grenades.size, ticks.grenadesActivePerTick);
+    buildHashmapIndex({flashed.tickId}, flashed.size, ticks.flashedPerTick);
+    buildHashmapIndex({plants.startTick, plants.endTick}, plants.size, ticks.plantsPerTick);
+    buildHashmapIndex({plants.startTick}, plants.size, ticks.plantsStartPerTick);
+    buildHashmapIndex({plants.endTick}, plants.size, ticks.plantsEndPerTick);
+    buildHashmapIndex({defusals.startTick, defusals.endTick}, defusals.size, ticks.defusalsPerTick);
+    buildHashmapIndex({defusals.startTick}, defusals.size, ticks.defusalsStartPerTick);
+    buildHashmapIndex({defusals.endTick}, defusals.size, ticks.defusalsEndPerTick);
+    buildHashmapIndex({explosions.tickId}, explosions.size, ticks.explosionsPerTick);
+    buildRangeIndex(grenades.id, grenades.size, flashed.grenadeId, flashed.size, grenades.flashedPerGrenade);
+    buildRangeIndex(grenades.id, grenades.size, grenadeTrajectories.grenadeId, grenadeTrajectories.size, grenades.trajectoryPerGrenade);
+    buildRangeIndex(plants.id, plants.size, defusals.plantId, defusals.size, plants.defusalsPerGrenade);
+    buildRangeIndex(plants.id, plants.size, explosions.plantId, explosions.size, plants.explosionsPerGrenade);
 }
