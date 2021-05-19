ed47258403f77fd26610e43937d900f053afc63b
David Durst <davidbdurst@gmail.com>
2021-05-19 13:26:19 -0700

    pip install python analyitcs project

---
+++ python_analytics/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='csknow-python-analytics',
+    version='0.0.1',
+    url='https://github.com/David-Durst/csknow/python_analytics',
+    license='MIT',
+    maintainer='David Durst',
+    maintainer_email='davidbdurst@gmail.com',
+    description='CSKnow Python Analytics',
+    packages=[
+        "csknow-python-analytics",
+    ],
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn"
+    ],
+    python_requires='>=3.7',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
