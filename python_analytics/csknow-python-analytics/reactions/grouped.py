384f49f2839520d654640f124d1a44998c94c897
David Durst <davidbdurst@gmail.com>
2021-09-29 17:49:45 -0700

    fire reaction time added

--- python_analytics/csknow-python-analytics/reactions/grouped.py
+++ python_analytics/csknow-python-analytics/reactions/grouped.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import seaborn as sn
 import numpy as np
+from dataclasses import dataclass
 
 parser = argparse.ArgumentParser()
 parser.add_argument("password", help="database password",
@@ -39,16 +40,28 @@ cpu_react_reasonable = '(select * from react_final where abs(cpu_aim_react_s) <=
 select_cols = 'game_id, min(round_id) as min_round_id, max(round_id) as max_round_id, spotter_id, spotter, hacking, ' + \
     'distinct_others_spotted_during_time, ' + \
     'avg(hand_aim_react_s) as avg_aim_hand_react, avg(cpu_aim_react_s) as avg_aim_cpu_react, ' + \
+    'avg(hand_fire_react_s) as avg_fire_hand_react, avg(cpu_fire_react_s) as avg_fire_cpu_react, ' + \
     'sum(case when abs(hand_aim_react_s) <= 0.2 then 1 else 0 end) as hand_preaims, ' + \
     'sum(case when abs(cpu_aim_react_s) <= 0.2 then 1 else 0 end) as cpu_preaims'
 group_cols = f'''group by game_id, round_id / {args.grouping_rounds}, spotter_id, spotter, hacking, distinct_others_spotted_during_time'''
 hand_filtered_df = sqlio.read_sql_query(f'''select {select_cols} from {hand_react_reasonable} {group_cols}''', conn)
 cpu_filtered_df = sqlio.read_sql_query(f'''select {select_cols} from {cpu_react_reasonable} {group_cols}''', conn)
 
+
+@dataclass(frozen=True)
+class LabeledData:
+    hacks_hand_filtered_df: pd.DataFrame
+    hacks_cpu_filtered_df: pd.DataFrame
+    legit_hand_filtered_df: pd.DataFrame
+    legit_cpu_filtered_df: pd.DataFrame
+
 hacks_hand_filtered_df = hand_filtered_df[hand_filtered_df['hacking']]
 hacks_cpu_filtered_df = cpu_filtered_df[cpu_filtered_df['hacking']]
 legit_hand_filtered_df = hand_filtered_df[~hand_filtered_df['hacking']]
 legit_cpu_filtered_df = cpu_filtered_df[~cpu_filtered_df['hacking']]
+
+dfs = LabeledData(hacks_hand_filtered_df, hacks_cpu_filtered_df, legit_hand_filtered_df, legit_cpu_filtered_df)
+
 
 print(f'''hacks hand size {len(hacks_hand_filtered_df)}, legit hand size {len(legit_hand_filtered_df)} \n ''' +
       f'''hacks cpu size {len(hacks_cpu_filtered_df)}, legit cpu size {len(legit_cpu_filtered_df)}''')
@@ -56,13 +69,15 @@ print(f'''hacks hand size {len(hacks_hand_filtered_df)}, legit hand size {len(le
 
 def makePlotterFunction(bin_width, pct):
     def plotPctWith200MSBins(df, col, ax):
-        num_bins = math.ceil((df[col].max() - df[col].min()) / bin_width)
-        df.hist(col, bins=num_bins, ax=ax, weights=np.ones(len(df[col])) / len(df[col]))
+        col_vals = df[col].dropna()
+        num_bins = math.ceil((col_vals.max() - col_vals.min()) / bin_width)
+        df.hist(col, bins=num_bins, ax=ax, weights=np.ones(len(col_vals)) / len(col_vals))
         ax.yaxis.set_major_formatter(PercentFormatter(1))
         return pd.cut(df[col], num_bins).value_counts().sort_index()
 
     def plotNumWith200MSBins(df, col, ax):
-        num_bins = math.ceil((df[col].max() - df[col].min()) / bin_width)
+        col_vals = df[col].dropna()
+        num_bins = math.ceil((col_vals.max() - col_vals.min()) / bin_width)
         df.hist(col, bins=num_bins, ax=ax)
         return pd.cut(df[col], num_bins).value_counts().sort_index()
 
@@ -72,12 +87,12 @@ def makePlotterFunction(bin_width, pct):
         return plotNumWith200MSBins
 
 
-def makeHistograms(hand_col, cpu_col, plotting_function, change_x_lim, change_y_lim, name, x_label):
+def makeHistograms(dfs, hand_col, cpu_col, plotting_function, change_x_lim, change_y_lim, name, x_label):
     fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(16, 16))
-    hacks_hand_distribution = plotting_function(hacks_hand_filtered_df, hand_col, ax[0][0])
-    legit_hand_distribution = plotting_function(legit_hand_filtered_df, hand_col, ax[0][1])
-    hacks_cpu_distribution = plotting_function(hacks_cpu_filtered_df, cpu_col, ax[1][0])
-    legit_cpu_distribution = plotting_function(legit_cpu_filtered_df, cpu_col, ax[1][1])
+    hacks_hand_distribution = plotting_function(dfs.hacks_hand_filtered_df, hand_col, ax[0][0])
+    legit_hand_distribution = plotting_function(dfs.legit_hand_filtered_df, hand_col, ax[0][1])
+    hacks_cpu_distribution = plotting_function(dfs.hacks_cpu_filtered_df, cpu_col, ax[1][0])
+    legit_cpu_distribution = plotting_function(dfs.legit_cpu_filtered_df, cpu_col, ax[1][1])
 
     for i in range(len(ax)):
         for j in range(len(ax[i])):
@@ -96,10 +111,15 @@ def makeHistograms(hand_col, cpu_col, plotting_function, change_x_lim, change_y_
     ax[1][0].set_title('CPU Labeled, Hacking', fontsize=18)
     ax[1][1].set_title('CPU Labeled, Not Hacking', fontsize=18)
 
-    ax[0][0].annotate('total points: ' + str(len(hacks_hand_filtered_df)), (0,2), fontsize="14")
-    ax[0][1].annotate('total points: ' + str(len(legit_hand_filtered_df)), (0,2), fontsize="14")
-    ax[1][0].annotate('total points: ' + str(len(hacks_cpu_filtered_df)), (0,2), fontsize="14")
-    ax[1][1].annotate('total points: ' + str(len(legit_cpu_filtered_df)), (0,2), fontsize="14")
+    def get_num_points_coordinate(ax, x_pct = 0.6, y_pct = 0.8):
+        (y_min, y_max) = ax.get_ylim()
+        (x_min, x_max) = ax.get_xlim()
+        return (x_min + (x_max-x_min) * x_pct, y_min + (y_max-y_min)*y_pct)
+
+    ax[0][0].annotate('total points: ' + str(len(hacks_hand_filtered_df[hand_col].dropna())), get_num_points_coordinate(ax[0][0]), fontsize="14")
+    ax[0][1].annotate('total points: ' + str(len(legit_hand_filtered_df[hand_col].dropna())), get_num_points_coordinate(ax[0][1]), fontsize="14")
+    ax[1][0].annotate('total points: ' + str(len(hacks_cpu_filtered_df[cpu_col].dropna())), get_num_points_coordinate(ax[1][0]), fontsize="14")
+    ax[1][1].annotate('total points: ' + str(len(legit_cpu_filtered_df[cpu_col].dropna())), get_num_points_coordinate(ax[1][1]), fontsize="14")
 
     plt.tight_layout()
     fig.savefig(args.plot_folder + name + '_grouped_histogram__hand_vs_cpu__hacking_vs_legit.png')
@@ -107,10 +127,12 @@ def makeHistograms(hand_col, cpu_col, plotting_function, change_x_lim, change_y_
     plt.clf()
 
 
-makeHistograms('avg_aim_hand_react', 'avg_aim_cpu_react', makePlotterFunction(0.2, False), True, True, 'count_avg', 'Reaction Time (s)')
-makeHistograms('avg_aim_hand_react', 'avg_aim_cpu_react', makePlotterFunction(0.2, True), True, False, 'pct_avg', 'Reaction Time (s)')
-makeHistograms('hand_preaims', 'cpu_preaims', makePlotterFunction(1, False), False, True, 'count_preaim', 'Number of Preaims')
-makeHistograms('hand_preaims', 'cpu_preaims', makePlotterFunction(1, True), False, False, 'pct_preaim', 'Number of Preaims')
+makeHistograms(dfs, 'avg_aim_hand_react', 'avg_aim_cpu_react', makePlotterFunction(0.2, False), True, True, 'count_aim', 'Aim Reaction Time (s)')
+makeHistograms(dfs, 'avg_aim_hand_react', 'avg_aim_cpu_react', makePlotterFunction(0.2, True), True, False, 'pct_aim', 'Aim Reaction Time (s)')
+makeHistograms(dfs, 'avg_fire_hand_react', 'avg_fire_cpu_react', makePlotterFunction(0.2, False), True, True, 'count_fire', 'Fire Reaction Time (s)')
+makeHistograms(dfs, 'avg_fire_hand_react', 'avg_fire_cpu_react', makePlotterFunction(0.2, True), True, False, 'pct_fire', 'Fire Reaction Time (s)')
+makeHistograms(dfs, 'hand_preaims', 'cpu_preaims', makePlotterFunction(1, False), False, True, 'count_preaim', 'Number of Preaims')
+makeHistograms(dfs, 'hand_preaims', 'cpu_preaims', makePlotterFunction(1, True), False, False, 'pct_preaim', 'Number of Preaims')
 
 def makeLogReg(df, col, name):
     plt.clf()
