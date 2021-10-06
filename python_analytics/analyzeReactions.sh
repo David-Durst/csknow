06a99d6926f3087d7783a26428ea1ad11f35378a
David Durst <davidbdurst@gmail.com>
2021-10-05 21:39:29 -0700

    adding in changes for best cpu visibility result

--- python_analytics/analyzeReactions.sh
+++ python_analytics/analyzeReactions.sh
@@ -22,4 +22,5 @@ conda activate csknow
 cd ${script_dir}/csknow-python-analytics/reactions/
 
 python individual.py ${pass} ${script_dir}/../sql_analytics/visibility.sql ${script_dir}/reactionPlots/
+# change 4 to 3 for best cpu result
 python grouped.py ${pass} ${script_dir}/../sql_analytics/visibility.sql ${script_dir}/reactionPlots/ 4
