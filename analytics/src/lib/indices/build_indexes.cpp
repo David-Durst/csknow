f71c1b3e68d31f3f9815115a5ec1cbaa1d631fd9
David Durst <davidbdurst@gmail.com>
2021-10-20 19:05:16 -0700

    improved index logging, fixed update local for more than 10 files

--- analytics/src/lib/indices/build_indexes.cpp
+++ analytics/src/lib/indices/build_indexes.cpp
@@ -6,7 +6,8 @@ using std::cout;
 using std::endl;
 
 void buildRangeIndex(const vector<int64_t> primaryKeyCol, int64_t primarySize, const int64_t * foreignKeyCol,
-                          int64_t foreignSize, RangeIndex rangeIndexCol) {
+                          int64_t foreignSize, RangeIndex rangeIndexCol,
+                          string primaryName, string foreignName) {
     for (int64_t primaryIndex = 0, foreignIndex = 0; primaryIndex < primarySize; primaryIndex++) {
         if (foreignIndex >= foreignSize || foreignKeyCol[foreignIndex] > primaryKeyCol[primaryIndex]) {
             rangeIndexCol[primaryIndex].minId = -1;
@@ -17,7 +18,9 @@ void buildRangeIndex(const vector<int64_t> primaryKeyCol, int64_t primarySize, c
             for(; foreignKeyCol[foreignIndex] <= 0 && foreignKeyCol[foreignIndex] < primaryKeyCol[primaryIndex]; foreignIndex++);
             if (foreignKeyCol[foreignIndex] < primaryKeyCol[primaryIndex]) {
                 cout << "bad foreign " << foreignIndex  << " val " << foreignKeyCol[foreignIndex]
-                     << " and primary " << primaryIndex << " val " << primaryKeyCol[primaryIndex] << endl;
+                     << " for foreign column " << foreignName
+                     << " and primary " << primaryIndex << " val " << primaryKeyCol[primaryIndex]
+                     << " for primary column " << primaryName << endl;
                 assert(foreignKeyCol[foreignIndex] >= primaryKeyCol[primaryIndex]);
             }
             assert(foreignIndex < foreignSize);
@@ -48,15 +51,15 @@ void buildIndexes(Equipment & equipment, GameTypes & gameTypes, HitGroups & hitG
                        WeaponFire & weaponFire, Kills & kills, Hurt & hurt, Grenades & grenades, Flashed & flashed,
                        GrenadeTrajectories & grenadeTrajectories, Plants & plants, Defusals & defusals, Explosions & explosions) {
     cout << "building range indexes" << endl;
-    buildRangeIndex(games.id, games.size, rounds.gameId, rounds.size, games.roundsPerGame);
-    buildRangeIndex(games.id, games.size, players.gameId, players.size, games.playersPerGame);
-    buildRangeIndex(rounds.id, rounds.size, ticks.roundId, ticks.size, rounds.ticksPerRound);
-    buildRangeIndex(ticks.id, ticks.size, playerAtTick.tickId, playerAtTick.size, ticks.patPerTick);
-    buildRangeIndex(ticks.id, ticks.size, spotted.tickId, spotted.size, ticks.spottedPerTick);
-    buildRangeIndex(grenades.id, grenades.size, flashed.grenadeId, flashed.size, grenades.flashedPerGrenade);
-    buildRangeIndex(grenades.id, grenades.size, grenadeTrajectories.grenadeId, grenadeTrajectories.size, grenades.trajectoryPerGrenade);
-    buildRangeIndex(plants.id, plants.size, defusals.plantId, defusals.size, plants.defusalsPerGrenade);
-    buildRangeIndex(plants.id, plants.size, explosions.plantId, explosions.size, plants.explosionsPerGrenade);
+    buildRangeIndex(games.id, games.size, rounds.gameId, rounds.size, games.roundsPerGame, "games", "rounds");
+    buildRangeIndex(games.id, games.size, players.gameId, players.size, games.playersPerGame, "games", "players");
+    buildRangeIndex(rounds.id, rounds.size, ticks.roundId, ticks.size, rounds.ticksPerRound, "rounds", "ticks");
+    buildRangeIndex(ticks.id, ticks.size, playerAtTick.tickId, playerAtTick.size, ticks.patPerTick, "ticks", "playerAtTick");
+    buildRangeIndex(ticks.id, ticks.size, spotted.tickId, spotted.size, ticks.spottedPerTick, "ticks", "spotted");
+    buildRangeIndex(grenades.id, grenades.size, flashed.grenadeId, flashed.size, grenades.flashedPerGrenade, "grenades", "flashed");
+    buildRangeIndex(grenades.id, grenades.size, grenadeTrajectories.grenadeId, grenadeTrajectories.size, grenades.trajectoryPerGrenade, "grenades", "grenadeTrajectories");
+    buildRangeIndex(plants.id, plants.size, defusals.plantId, defusals.size, plants.defusalsPerGrenade, "plants", "defusals");
+    buildRangeIndex(plants.id, plants.size, explosions.plantId, explosions.size, plants.explosionsPerGrenade, "plants", "explosions");
 
     cout << "building hashmap indexes" << endl;
     buildHashmapIndex({weaponFire.tickId}, weaponFire.size, ticks.weaponFirePerTick);
