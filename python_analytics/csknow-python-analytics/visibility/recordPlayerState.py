c85a80c65944b320dd8e5bf46ebde968362cc85e
David Durst <davidbdurst@gmail.com>
2021-11-06 22:10:36 -0700

    small change

--- python_analytics/csknow-python-analytics/visibility/recordPlayerState.py
+++ python_analytics/csknow-python-analytics/visibility/recordPlayerState.py
@@ -84,7 +84,7 @@ while True:
     # stop everything if z, skip if invalid key
     if key == 'z':
         break
-    elif key != 'o' and key not in [str(i) for  i  in range(1,len(action_strings)+1)]:
+    elif key != 'o' and key not in [str(i) for i in range(1,len(action_strings)+1)]:
         print(f'''{key} not in valid range 1 to {len(action_strings)+1}''')
         continue
 
