1e882e1629a90a082c69b7bebd6c28619fb58090
David Durst <davidbdurst@gmail.com>
2021-11-17 23:12:55 -0800

    fixing args

--- python_analytics/recordSnippets.bat
+++ python_analytics/recordSnippets.bat
@@ -3,6 +3,6 @@ SET script_dir=%~dp0
 
 cd %script_dir%csknow_python_analytics\visibility
 
-python recordSnippets.py %script_dir%visibilitySignImages C:\Users\Administrator\Videos\  2351898_128353_g2-vs-ence-m3-dust2_5f2f16a6-292a-11ec-8e27-0a58a9feac02.dem 2351898_128353_g2-vs-ence-m3-dust2_5f2f16a6-292a-11ec-8e27-0a58a9feac02_pre_load_NiKo_1.cfg
+python recordSnippets.py %script_dir%visibilitySignImages C:\Users\Administrator\Videos\ 2351898_128353_g2-vs-ence-m3-dust2_5f2f16a6-292a-11ec-8e27-0a58a9feac02_pre_load_NiKo_1.cfg %script_dir%snippets.csv
 
 cd %script_dir%
