934cf1551bb7d2f387f17d64465a746c1640f3ad
David Durst <davidbdurst@gmail.com>
2022-01-25 15:09:55 -0800

    adding bot gdb

---
+++ analytics/scripts/bot_gdb.sh
@@ -0,0 +1,24 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+
+mkdir -p ${script_dir}/../build
+cd ${script_dir}/../build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+chmod a+x csknow_bot
+if make -j4; then
+    sudo su - steam -c "gdb --args ${script_dir}/../build/csknow_bot /home/steam/csgo-ds/csgo/addons/sourcemod/bot-link-data 128"
+fi
