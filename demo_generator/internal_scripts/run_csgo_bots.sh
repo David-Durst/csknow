69e005d8061073ef6eb3b39536922944233b46da
David Durst <davidbdurst@gmail.com>
2022-02-06 17:54:24 -0800

    figured out that sm_nextmap does what I needed from mapgroup

--- demo_generator/internal_scripts/run_csgo_bots.sh
+++ demo_generator/internal_scripts/run_csgo_bots.sh
@@ -1,2 +1,3 @@
 cd csgo-ds
-./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 3 +game_mode 0 +mapgroup mg_bot +map bot_playground -tickrate 128
+./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 0 +game_mode 0 +sm_nextmap bot_playground
+ +map bot_playground -tickrate 128
