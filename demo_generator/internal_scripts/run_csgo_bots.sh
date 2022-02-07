197b5da53362657cc6999bbe5ba91561345694bd
David Durst <davidbdurst@gmail.com>
2022-02-06 18:00:07 -0800

    custom game mod doesn't like my current slt location

--- demo_generator/internal_scripts/run_csgo_bots.sh
+++ demo_generator/internal_scripts/run_csgo_bots.sh
@@ -1,3 +1,3 @@
 cd csgo-ds
-./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 0 +game_mode 0 +sm_nextmap bot_playground
+./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 3 +game_mode 0 +sm_nextmap bot_playground
  +map bot_playground -tickrate 128
