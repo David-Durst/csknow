f68cea5b28fab531d2540c25dd6b8d9cb8fb3540
David Durst <davidbdurst@gmail.com>
2022-02-07 16:28:41 -0800

    need wrong group to switch to next map

--- demo_generator/internal_scripts/run_csgo_bots.sh
+++ demo_generator/internal_scripts/run_csgo_bots.sh
@@ -1,2 +1,2 @@
 cd csgo-ds
-./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 0 +game_mode 0 +sm_nextmap bot_playground +map bot_playground -tickrate 128
+./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 0 +game_mode 0 +sm_nextmap bot_playground +mapgroup mg_active +map bot_playground -tickrate 128
