481aee48e11a1422a9312a9e79c8806dcdb6bda4
David Durst <davidbdurst@gmail.com>
2022-02-18 01:16:36 -0800

    csgo server runs but fails

--- demo_generator/entry.sh
+++ demo_generator/entry.sh
@@ -3,29 +3,9 @@ set -x
 bash update.sh
 python3 -u upload_logs.py >> upload.log 2>> upload.log &
 
-cd ~/csknow/analytics.sh
-bash bot_run_docker.sh &
+bash ~/csknow/analytics/scripts/bot_run_docker.sh &
 
-# We assume that if the config is missing, that this is a fresh container
-#if [ ! -f "${NONVOLUMESTEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg" ]; then
-#	# Download & extract the config
-#	wget -qO- "${DLURL}/master/etc/cfg.tar.gz" | tar xvzf - -C "${NONVOLUMESTEAMAPPDIR}/${STEAMAPP}"
-#	
-#	# Are we in a metamod container?
-#	if [ ! -z "$METAMOD_VERSION" ]; then
-#		LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
-#		wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${NONVOLUMESTEAMAPPDIR}/${STEAMAPP}"	
-#	fi
-#
-#	# Are we in a sourcemod container?
-#	if [ ! -z "$SOURCEMOD_VERSION" ]; then
-#		LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
-#		wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${NONVOLUMESTEAMAPPDIR}/${STEAMAPP}"
-#	fi
-#
-#	# Change hostname on first launch (you can comment this out if it has done it's purpose)
-#	sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${NONVOLUMESTEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
-#fi
+bash ~/install_link.sh
 
 # Believe it or not, if you don't do this srcds_run shits itself
 cd ${NONVOLUMESTEAMAPPDIR}
