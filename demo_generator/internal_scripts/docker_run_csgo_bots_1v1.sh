f8870e1feaff36456b427bbeafd0107fbabb67b6
David Durst <davidbdurst@gmail.com>
2022-02-18 16:33:30 -0800

    change port so don't conflict with human reachable server on same machine

--- demo_generator/internal_scripts/docker_run_csgo_bots_1v1.sh
+++ demo_generator/internal_scripts/docker_run_csgo_bots_1v1.sh
@@ -1,1 +1,1 @@
-./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 0 +game_mode 0 +sm_nextmap bot_playground +mapgroup mg_active +map bot_playground -tickrate 128
+./srcds_run  -game csgo -console -usercon +sv_autoexec_mapname_cfg 1 +game_type 0 +game_mode 0 +sm_nextmap bot_playground +mapgroup mg_active +map bot_playground -tickrate 128 -port 27016
