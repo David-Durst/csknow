71ccef91e778cc7fce013e51be08cdd094bacd7c
David Durst <davidbdurst@gmail.com>
2022-02-18 16:45:41 -0800

    1v1 bots runs correctly

--- demo_generator/install_sourcemod.sh
+++ demo_generator/install_sourcemod.sh
@@ -1,5 +1,5 @@
 wget https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1145-linux.tar.gz .
 tar -xvzf mmsource-1.11.0-git1145-linux.tar.gz -C $1
 
-wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6516-linux.tar.gz .
-tar -xvzf sourcemod-1.10.0-git6516-linux.tar.gz -C $1
+wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6528-linux.tar.gz .
+tar -xvzf sourcemod-1.10.0-git6528-linux.tar.gz -C $1
