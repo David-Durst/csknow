b7ec2ebb45fd36d781688b1e1e61b8c0d57dfcd9
David Durst <davidbdurst@gmail.com>
2022-02-22 18:14:13 -0800

    added start of nn

--- learn_bot/learn_bot/bot.py
+++ learn_bot/learn_bot/bot.py
@@ -41,6 +41,26 @@ for X, Y in train_dataloader:
     print(f"Shape of Y: {Y.shape} {Y.dtype}")
     break
 
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(89, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+        )
+        self.
+
+    def forward(self, x):
+        logits = self.linear_relu_stack(x)
+        return logits
+
 """
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
