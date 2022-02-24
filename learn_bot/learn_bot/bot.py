0f97fbb3f65bb681663ac9f560da3cbc6473b544
David Durst <davidbdurst@gmail.com>
2022-02-24 00:00:18 -0800

    learning with simpler problem

--- learn_bot/learn_bot/bot.py
+++ learn_bot/learn_bot/bot.py
@@ -8,35 +8,41 @@ from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from pathlib import Path
 
 all_data_df = pd.read_csv(Path(__file__).parent / '..' / 'data' / 'train_dataset.csv')
 train_df, test_df = train_test_split(all_data_df, test_size=0.2)
-min_max_scaler = preprocessing.MinMaxScaler()
-min_max_scaler.fit(all_data_df.iloc[:, 95:].values)
+#min_max_scaler = preprocessing.MinMaxScaler()
+#min_max_scaler.fit(all_data_df.iloc[:, 95:].values)
 
 unique_player_id = all_data_df.iloc[:,2].unique()
 player_id_to_ix = {player_id: i for (i, player_id) in enumerate(unique_player_id)}
 embedding_dim = 5
 
-
+non_embedding_features = 0
 # https://androidkt.com/load-pandas-dataframe-using-dataset-and-dataloader-in-pytorch/
 class BotDataset(Dataset):
     def __init__(self, df):
+        global non_embedding_features
         self.id = df.iloc[:, 0]
         self.tick_id = df.iloc[:, 1]
         self.source_player_id = df.iloc[:, 2]
         self.source_player_name = df.iloc[:, 3]
         self.demo_name = df.iloc[:, 4]
-        x_cols = [all_data_df.columns[2]] + all_data_df.columns[5:94].tolist()
+        x_cols = [all_data_df.columns[2]] + all_data_df.columns[5:77].tolist()
+        non_embedding_features = len(x_cols) - 1
         # convert player id's to indexes
         df_with_ixs = df.replace({all_data_df.columns[2]: player_id_to_ix})
         self.X = torch.tensor(df_with_ixs.loc[:, x_cols].values).float()
-        Y_prescale_df = df.iloc[:, 95:].values
-        Y_scaled_df = min_max_scaler.transform(Y_prescale_df)
-        self.Y = torch.tensor(Y_scaled_df).float()
+        Y_prescale_df = df.iloc[:, 77:]
+        #Y_scaled_df = min_max_scaler.transform(Y_prescale_df)
+        df['moving'] = np.where((df['delta x']**2 + df['delta y']**2) ** 0.5 > 0.5, 1.0, 0.0)
+        df['not moving'] = np.where((df['delta x']**2 + df['delta y']**2) ** 0.5 > 0.5, 0.0, 1.0)
+        sub_df = df[['moving', 'not moving']].values
+        self.Y = torch.tensor(sub_df).float()
 
     def __len__(self):
         return len(self.id)
@@ -73,7 +79,7 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.embeddings = nn.Embedding(len(unique_player_id), embedding_dim)
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(89 + embedding_dim, 128),
+            nn.Linear(non_embedding_features + embedding_dim, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
@@ -82,30 +88,28 @@ class NeuralNetwork(nn.Module):
         )
         self.moveLayer = nn.Sequential(
             nn.Linear(128, 2),
-            nn.ReLU(),
         )
-        self.crouchLayer = nn.Sequential(
-            nn.Linear(128, 2),
-            nn.ReLU(),
-        )
-        self.shootLayer = nn.Sequential(
-            nn.Linear(128, 2),
-            nn.ReLU(),
-        )
-        self.moveSigmoid = nn.Sigmoid()
-        self.crouchSigmoid = nn.Softmax(dim=1)
-        self.shootSigmoid = nn.Softmax(dim=1)
+        #self.crouchLayer = nn.Sequential(
+        #    nn.Linear(128, 2),
+        #)
+        #self.shootLayer = nn.Sequential(
+        #    nn.Linear(128, 2),
+        #)
+        #self.moveSoftmax = nn.Softmax(dim=1)
+        #self.moveSigmoid = nn.Sigmoid()
+        #self.crouchSoftmax = nn.Softmax(dim=1)
+        #self.shootSoftmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        idx, x_vals = torch.split(x, [1, 89], dim=1)
+        idx, x_vals = torch.split(x, [1, non_embedding_features], dim=1)
         idx_long = idx.long()
         embeds = self.embeddings(idx_long).view((-1, embedding_dim))
         x_all = torch.cat((embeds, x_vals), 1)
         logits = self.linear_relu_stack(x_all)
-        moveOutput = self.moveSigmoid(self.moveLayer(logits))
-        crouchOutput = self.crouchSigmoid(self.crouchLayer(logits))
-        shootOutput = self.shootSigmoid(self.shootLayer(logits))
-        return torch.cat((moveOutput, crouchOutput, shootOutput), dim=1)
+        #moveOutput = self.moveSigmoid(self.moveLayer(logits))
+        #crouchOutput = self.crouchSoftmax(self.crouchLayer(logits))
+        #shootOutput = self.shootSoftmax(self.shootLayer(logits))
+        return self.moveLayer(logits) #torch.cat((moveOutput, crouchOutput, shootOutput), dim=1)
 
 
 model = NeuralNetwork().to(device)
