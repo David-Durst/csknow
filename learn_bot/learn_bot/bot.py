e51025c1602e27aa5ef8234193031a48ad3b815b
David Durst <davidbdurst@gmail.com>
2022-02-28 13:05:14 -0800

    applying transformation in python

--- learn_bot/learn_bot/bot.py
+++ learn_bot/learn_bot/bot.py
@@ -9,40 +9,78 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from pathlib import Path
 from learn_bot.baseline import *
 
 all_data_df = pd.read_csv(Path(__file__).parent / '..' / 'data' / 'train_dataset.csv')
-config = pd.read_csv(Path(__file__).parent / '..' / 'data' / 'train_config.csv')
-train_df, test_df = train_test_split(all_data_df, test_size=0.2)
-min_max_scaler = preprocessing.MinMaxScaler()
-min_max_scaler.fit(all_data_df.iloc[:, config.at[0, 'label main min']:].values)
 
-unique_player_id = all_data_df.iloc[:,2].unique()
+# load config
+def get_config_line(i):
+    return config_lines[i].strip().split(',')
+
+
+config_file = open(Path(__file__).parent / '..' / 'data' / 'train_config.csv', 'r')
+config_lines = config_file.readlines()
+config_file.close()
+player_id_col = get_config_line(0)[0]
+non_player_id_input_cols = get_config_line(1)
+input_cols = [player_id_col] + non_player_id_input_cols
+input_one_hot_cols = get_config_line(2)
+input_min_max_cols = get_config_line(3)
+output_cols = get_config_line(4)
+output_one_hot_cols = get_config_line(5)
+output_min_max_cols = get_config_line(6)
+all_data_cols = input_cols + output_cols
+
+train_df, test_df = train_test_split(all_data_df, test_size=0.2)
+
+
+def compute_passthrough_cols(all_cols, *non_passthrough_lists):
+    non_passthrough_cols = []
+    for non_passthrough_list in non_passthrough_lists:
+        non_passthrough_cols += non_passthrough_list
+    return [c for c in all_cols if c not in non_passthrough_cols]
+
+
+input_ct = ColumnTransformer(transformers=[
+    ('one-hot', OneHotEncoder(), input_one_hot_cols),
+    ('zero-to-one', MinMaxScaler(), input_min_max_cols),
+    ('pass', 'passthrough', compute_passthrough_cols(input_cols, input_one_hot_cols, input_min_max_cols))
+])
+output_ct = ColumnTransformer(transformers=[
+    ('one-hot', OneHotEncoder(), output_one_hot_cols),
+    ('zero-to-one', MinMaxScaler(), output_min_max_cols),
+    ('pass', 'passthrough', compute_passthrough_cols(output_cols, output_one_hot_cols, output_min_max_cols))
+])
+# remember: fit Y is ignored for this fitting as not SL
+input_ct.fit(all_data_df.loc[:, input_cols])
+output_ct.fit(all_data_df.loc[:, output_cols])
+
+unique_player_id = all_data_df.loc[:, player_id_col].unique()
 player_id_to_ix = {player_id: i for (i, player_id) in enumerate(unique_player_id)}
 embedding_dim = 5
 
-non_embedding_features = 0
 num_target_cols = 0
 # https://androidkt.com/load-pandas-dataframe-using-dataset-and-dataloader-in-pytorch/
 class BotDataset(Dataset):
     def __init__(self, df):
-        global non_embedding_features, num_target_cols
+        global num_target_cols
         self.id = df.iloc[:, 0]
         self.tick_id = df.iloc[:, 1]
-        self.source_player_id = df.iloc[:, config.at[0,'data player id']]
+        self.source_player_id = df.loc[:, player_id_col]
         self.source_player_name = df.iloc[:, 3]
         self.demo_name = df.iloc[:, 4]
 
-        x_cols = [all_data_df.columns[config.at[0,'data player id']]] + \
-                 all_data_df.columns[config.at[0,'data main min']:config.at[0,'data main max']].tolist()
-        non_embedding_features = len(x_cols) - 1
         # convert player id's to indexes
         df_with_ixs = df.replace({all_data_df.columns[2]: player_id_to_ix})
-        self.X = torch.tensor(df_with_ixs.loc[:, x_cols].values).float()
-        Y_prescale_df = df.iloc[:, config.at[0, 'label main min']:]
+        input_df = df_with_ixs.loc[:, input_cols]
+        input_df_transformed = input_ct.transform(input_df)
+        self.X = torch.tensor(input_df_transformed.values).float()
+        Y_prescale_df = df.loc[:, output_cols]
         target_cols = [column for column in df.columns if column.startswith('nav') and column.endswith('target')]
         num_target_cols = len(target_cols)
         #Y_scaled_df = min_max_scaler.transform(Y_prescale_df)
@@ -87,7 +125,7 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.embeddings = nn.Embedding(len(unique_player_id), embedding_dim)
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(non_embedding_features + embedding_dim, 128),
+            nn.Linear(embedding_dim + len(non_player_id_input_cols), 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
@@ -106,7 +144,7 @@ class NeuralNetwork(nn.Module):
         #self.moveSigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        idx, x_vals = torch.split(x, [1, non_embedding_features], dim=1)
+        idx, x_vals = torch.split(x, [1, len(non_embedding_features)], dim=1)
         idx_long = idx.long()
         embeds = self.embeddings(idx_long).view((-1, embedding_dim))
         x_all = torch.cat((embeds, x_vals), 1)
