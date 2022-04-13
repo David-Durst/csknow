6616038f7c5b908c027692689af06ce68eecadb3
David Durst <davidbdurst@gmail.com>
2022-04-13 01:29:17 +0000

    adding linux raingin script

---
+++ analytics/scripts/train_data_run_linux.sh
@@ -0,0 +1,25 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+
+mkdir -p ${script_dir}/../build
+cd ${script_dir}/../build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+if make -j4; then
+    mkdir -p ../csv_outputs
+    ./csknow_train ${script_dir}/../../local_data ${script_dir}/../nav ../csv_outputs
+    cp ../csv_outputs/train_* ../../learn_bot/data/engagement/
+fi
