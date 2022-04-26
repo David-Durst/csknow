599e98d46a3d39c4e2cbf394a8c72d664b3ffa7d
David Durst <davidbdurst@gmail.com>
2022-04-26 22:56:29 +0000

    more stuff on bt

--- analytics/include/bots/behavior_tree/nodes.h
+++ analytics/include/bots/behavior_tree/nodes.h
@@ -6,15 +6,62 @@
 #define CSKNOW_NODES_H
 
 #include "load_save_bot_data.h"
+#include <vector>
+using std::vector;
+
+struct Blackboard {
+
+};
 
 class Node {
+    int64_t index;
+
 public:
-    virtual bool relevant(const ServerState & state)
-    virtual void onEntry()
+    virtual bool relevant(const ServerState & state);
+    virtual bool exec(const ServerState & state);
+    virtual void onEntry();
+    virtual void onExit();
 };
 
-class RootNode {
+class RootNode : Node {
+    Node child;
 
+public:
+    RootNode(Node node) : child(node) { };
+
+    bool relevant(const ServerState &state) override {
+        return true;
+    }
+
+    bool exec(const ServerState & state) override {
+        child.exec(state);
+    }
+
+    void onEntry() override { }
+    void onExit() override { }
 };
+
+class ParSelectorNode : Node {
+    vector<Node> children;
+
+public:
+    ParSelectorNode(vector<Node> nodes) : children(nodes) { };
+
+    bool relevant(const ServerState &state) override {
+        return true;
+    }
+
+    bool exec(const ServerState & state) override {
+        for (auto & child : children) {
+            child.exec(state);
+        }
+    }
+
+    void onEntry() override { }
+    void onExit() override { }
+};
+
+RootNode buildTeamTree();
+RootNode buildPlayerTree();
 
 #endif //CSKNOW_NODES_H
---
+++ analytics/src/lib/bots/behavior_tree/team.cpp
@@ -0,0 +1,9 @@
+//
+// Created by durst on 4/26/22.
+//
+
+#include "bots/behavior_tree/nodes.h"
+
+RootNode buildTeamTree() {
+
+}
