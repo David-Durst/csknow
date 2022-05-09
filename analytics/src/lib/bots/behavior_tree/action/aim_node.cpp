d9c622b9b8feb9a0974ddcee269195ff7724c388
David Durst <davidbdurst@gmail.com>
2022-05-09 02:38:02 -0700

    aim node implemented

--- analytics/src/lib/bots/behavior_tree/action/aim_node.cpp
+++ analytics/src/lib/bots/behavior_tree/action/aim_node.cpp
@@ -19,68 +19,37 @@ namespace action {
     }
 
     NodeState AimTaskNode::exec(const ServerState &state, TreeThinker &treeThinker) {
-        const Priority & curPriority = blackboard.playerToPriority[treeThinker.csgoId];
         const ServerState::Client & curClient = state.getClient(treeThinker.csgoId);
+        Action & curAction = blackboard.playerToAction[treeThinker.csgoId];
+        Priority & curPriority = blackboard.playerToPriority[treeThinker.csgoId];
+        Path & curPath = blackboard.playerToPath[treeThinker.csgoId];
         Vec3 aimTarget;
 
-        // if no visible enemy to target, look at c4 (if planted and close) or next location
-        if (curPriority.priorityType != PriorityType::Player) {
-            if (curPriority.priorityType == PriorityType::C4 && state.c4IsPlanted &&
-                computeDistance(curClient.getFootPosForPlayer(), state.getC4Pos()) <= MAX_LOOK_AT_C4_DISTANCE) {
-                aimTarget = state.getC4Pos();
-            }
-            else {
-
-            }
+        // aim at target player if one exists, otherwise the next way point
+        // this should handle c4 as end of path will be c4
+        if (curPriority.targetPlayer.playerId == INVALID_ID) {
+            aimTarget = curPath.waypoints[curPath.curWaypoint];
+        }
+        else {
+            aimTarget = state.getClient(curPriority.targetPlayer.playerId).getEyePosForPlayer();
         }
 
+        Vec3 targetVector = aimTarget - curClient.getEyePosForPlayer();
+        Vec2 targetViewAngle = vectorAngles(targetVector);
+        targetViewAngle.makePitchNeg90To90();
+        // clamp within max of -89 to 89
+        targetViewAngle.y = std::max(-1 * MAX_PITCH_MAGNITUDE,
+                                  std::min(MAX_PITCH_MAGNITUDE, targetViewAngle.y));
+
+        // https://stackoverflow.com/a/7428771
+        Vec2 deltaAngles = targetViewAngle - curClient.getCurrentViewAnglesWithAimpunch();
+        deltaAngles.makeYawNeg180To180();
+
+        // TODO: use better angle velocity control
+        curAction.inputAngleDeltaPctX = computeAngleVelocity(deltaAngles.x, curAction.inputAngleDeltaPctX);
+        curAction.inputAngleDeltaPctY = computeAngleVelocity(deltaAngles.y, curAction.inputAngleDeltaPctY);
+
+        nodeState = NodeState::Success;
+        return nodeState;
     }
-
-}
-
-
-
-void Thinker::aimAt(ServerState::Client & curClient, const ServerState::Client & targetClient,
-                    const Vec2 & aimOffset, const ServerState::Client & priorClient) {
-    // if no plant and no enemies, then nothing to look at
-    if (targetClient.csgoId == INVALID_ID && !liveState.c4IsPlanted) {
-        curClient.inputAngleDeltaPctX = 0.;
-        curClient.inputAngleDeltaPctY = 0.;
-        return;
-    }
-
-    // look at an enemy if it exists, otherwise look at bomb
-    Vec3 targetVector;
-    if (targetClient.csgoId != INVALID_ID) {
-        targetVector = {
-                targetClient.lastEyePosX - curClient.lastEyePosX,
-                targetClient.lastEyePosY - curClient.lastEyePosY,
-                targetClient.lastEyePosZ - curClient.lastEyePosZ
-        };
-        Vec3 targetView = angleVectors({curClient.lastEyeWithRecoilAngleX,
-                                        curClient.lastEyeWithRecoilAngleY});
-        targetVector = targetVector + targetView * HEAD_ADJUSTMENT;
-    }
-    else {
-        targetVector = {
-                liveState.c4X - curClient.lastEyePosX,
-                liveState.c4Y - curClient.lastEyePosY,
-                liveState.c4Z - curClient.lastEyePosZ
-        };
-    }
-
-    Vec2 currentAngles = {
-            curClient.lastEyeAngleX + curClient.lastAimpunchAngleX,
-            curClient.lastEyeAngleY + curClient.lastAimpunchAngleY};
-    Vec2 targetAngles = vectorAngles(targetVector) + aimOffset;
-    targetAngles.makePitchNeg90To90();
-    targetAngles.y = std::max(-1 * MAX_PITCH_MAGNITUDE,
-                              std::min(MAX_PITCH_MAGNITUDE, targetAngles.y));
-
-    // https://stackoverflow.com/a/7428771
-    Vec2 deltaAngles = targetAngles - currentAngles;
-    deltaAngles.makeYawNeg180To180();
-
-    curClient.inputAngleDeltaPctX = computeAngleVelocity(deltaAngles.x, priorClient.inputAngleDeltaPctX);
-    curClient.inputAngleDeltaPctY = computeAngleVelocity(deltaAngles.y, priorClient.inputAngleDeltaPctY);
 }
