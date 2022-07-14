c2f609d164dbaf65e865a1dd313fd458273b5108
David Durst <davidbdurst@gmail.com>
2022-07-14 02:01:41 -0700

    labels render, but player labels not correct for deaths

--- docs/src/drawing/events.ts
+++ docs/src/drawing/events.ts
@@ -32,22 +32,33 @@ export function getPlayersText(tickData: TickRow, gameData: GameData): string[] 
     const index = getTickToOtherTableIndex(gameData, curEvent)
     const players: PlayerAtTickRow[] = gameData.getPlayersAtTick(tickData)
     // if no event, do nothing special
-    if (curEvent == "none" || !index.intersect_any([tickData.id, tickData.id])) {
+    if (curEvent == "none" || !index.intersect_any([tickData.demoTickNumber, tickData.demoTickNumber]) ||
+        !gameData.parsers.get(curEvent).havePlayerLabels) {
         for (let p = 0; p < players.length; p++) {
             result.push(basicPlayerText(gameData, tickData, p))
         }
         return result
     }
 
-    const playerIDToLocalPATIndex: Map<number, number> = new Map<number, number>()
     // if event, get min key player number for each player
     const eventArray = gameData.tables.get(curEvent)
-    const eventsForTick = index.search([tickData.id, tickData.id])
+    const eventsForTick = index.search([tickData.demoTickNumber, tickData.demoTickNumber])
+    const firstEvent = eventArray[Math.min(...eventsForTick)]
+    const parser = gameData.parsers.get(curEvent)
+    const playersToLabel = firstEvent.otherColumnValues[parser.playersToLabelColumn].split(";").map(x => parseInt(x))
+    const playerLabelIndices = firstEvent.otherColumnValues[parser.playerLabelIndicesColumn].split(";").map(x => parseInt(x))
     for (let p = 0; p < players.length; p++) {
-        result.push(basicPlayerText(gameData, tickData, p))
-        playerIDToLocalPATIndex.set(players[p].playerId, p)
+        const indexOfPlayerInLabelsArray = playersToLabel.indexOf(players[p].playerId)
+        if (players[p].isAlive && indexOfPlayerInLabelsArray != -1) {
+            result.push(parser.playerLabels[playerLabelIndices[indexOfPlayerInLabelsArray]])
+        }
+        else {
+            result.push(basicPlayerText(gameData, tickData, p))
+        }
     }
+    /*
 
+    const playerIDToLocalPATIndex: Map<number, number> = new Map<number, number>()
     let minKeyPlayerNumbers: number[] = [];
     const bogusMinKeyPlayerNumber = 100
     for (let p = 0; p < players.length; p++) {
@@ -70,6 +81,8 @@ export function getPlayersText(tickData: TickRow, gameData: GameData): string[] 
             result[p] = minKeyPlayerNumbers[p].toString()
         }
     }
+
+     */
     return result
 }
 
@@ -86,8 +99,8 @@ export function setEventText(tickData: TickRow, gameData: GameData) {
     }
     else {
         const index = getTickToOtherTableIndex(gameData, curEvent)
-        if (index.intersect_any([tickData.id, tickData.id])) {
-            const events = index.search([tickData.id, tickData.id])
+        if (index.intersect_any([tickData.demoTickNumber, tickData.demoTickNumber])) {
+            const events = index.search([tickData.demoTickNumber, tickData.demoTickNumber])
             const eventArray = gameData.tables.get(curEvent)
             for (let eIndex = events.length - 1; eIndex >= 0; eIndex--) {
                 eventDiv.innerHTML += eventArray[events[eIndex]].getHTML()
