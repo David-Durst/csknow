b33c7329508fc4941d2836658f527a3cd8dd39b5
David Durst <davidbdurst@gmail.com>
2022-07-25 23:24:00 -0700

    setting coordinates for danger query

--- analytics/src/lib/queries/nav_danger.cpp
+++ analytics/src/lib/queries/nav_danger.cpp
@@ -7,6 +7,10 @@
 NavDangerResult queryNavDanger(const VisPoints & visPoints, const nav_mesh::nav_file & navFile) {
     NavDangerResult result;
     result.numAreas = visPoints.getVisPoints().size();
+    result.coordinate = {};
+    for (const auto & visPoint : visPoints.getVisPoints()) {
+        result.coordinate.push_back(visPoint.areaCoordinates);
+    }
     result.dangerMatrix.resize(result.numAreas * result.numAreas, false);
 
     for (int64_t i = 0; i < result.numAreas; i++) {
