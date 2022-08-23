7bc1bc3cc8a32e8bc449902d02b652b970974e2c
David Durst <davidbdurst@gmail.com>
2022-08-23 13:14:57 -0700

    still looks good when turning

--- analytics/include/bots/testing/scripts/test_head_position.h
+++ analytics/include/bots/testing/scripts/test_head_position.h
@@ -82,6 +82,25 @@ public:
                     make_unique<Teleport>(blackboard, neededBots[0].id, state),
                     make_unique<movement::WaitNode>(blackboard, 0.1),
                     make_unique<DrawHeadPos>(blackboard, neededBots[0].id),
+                    make_unique<movement::WaitNode>(blackboard, 5.0),
+                    // angled
+                    make_unique<SetPos>(blackboard, Vec3({1417.528564, 1652.856445, -7.28}), Vec2({-45., -88.})),
+                    make_unique<movement::WaitNode>(blackboard, 0.1),
+                    make_unique<Teleport>(blackboard, neededBots[0].id, state),
+                    make_unique<movement::WaitNode>(blackboard, 0.1),
+                    make_unique<DrawHeadPos>(blackboard, neededBots[0].id),
+                    make_unique<movement::WaitNode>(blackboard, 5.0),
+                    make_unique<SetPos>(blackboard, Vec3({1417.528564, 1652.856445, -7.28}), Vec2({-45., 0.})),
+                    make_unique<movement::WaitNode>(blackboard, 0.1),
+                    make_unique<Teleport>(blackboard, neededBots[0].id, state),
+                    make_unique<movement::WaitNode>(blackboard, 0.1),
+                    make_unique<DrawHeadPos>(blackboard, neededBots[0].id),
+                    make_unique<movement::WaitNode>(blackboard, 5.0),
+                    make_unique<SetPos>(blackboard, Vec3({1417.528564, 1652.856445, -7.28}), Vec2({-45., 88.})),
+                    make_unique<movement::WaitNode>(blackboard, 0.1),
+                    make_unique<Teleport>(blackboard, neededBots[0].id, state),
+                    make_unique<movement::WaitNode>(blackboard, 0.1),
+                    make_unique<DrawHeadPos>(blackboard, neededBots[0].id),
                     make_unique<movement::WaitNode>(blackboard, 5.0)
             ), "MoveHead");
             commands = make_unique<SequenceNode>(blackboard, Node::makeList(
