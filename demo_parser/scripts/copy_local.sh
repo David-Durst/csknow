1137512e30023582d0fbee415159f5523e5441f7
David Durst <davidbdurst@gmail.com>
2022-09-09 11:24:18 -0700

    copy local runs correctly, merge no longer uses old files for visibilites/actions/skills

--- demo_parser/scripts/copy_local.sh
+++ demo_parser/scripts/copy_local.sh
@@ -1,3 +1,21 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+cd ${script_dir}/..
+
 cp csv_outputs/dimension_table*.csv ../local_data/
 cp csv_outputs/global_games.csv ../local_data/
 cp csv_outputs/local_defusals.csv ../local_data/defusals/
@@ -16,3 +34,6 @@ cp csv_outputs/local_players.csv ../local_data/players/
 cp csv_outputs/local_spotted.csv ../local_data/spotted/
 cp csv_outputs/local_footstep.csv ../local_data/footstep/
 cp csv_outputs/local_weapon_fire.csv ../local_data/weapon_fire/
+
+cd ${script_dir}/../../download_s3_csvs
+./merge.sh
