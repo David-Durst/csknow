5f0e2c8843d77536492f2c0ad1c2a24ccacaf310
David Durst <davidbdurst@gmail.com>
2022-09-23 19:37:53 -0700

    rmeoing fs in test

--- analytics/src/bin/csknow_test.cpp
+++ analytics/src/bin/csknow_test.cpp
@@ -3,7 +3,6 @@
 #include "queries/moments/engagement.h"
 #include "queries/moments/non_engagement_trajectory.h"
 #include "queries/moments/trajectory_segments.h"
-namespace fs = std::filesystem;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char * argv[]) {
     makeMapBasic();
