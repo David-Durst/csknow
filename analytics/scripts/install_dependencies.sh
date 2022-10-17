1707776ef0214bc7008dd0ddafc046d776925de1
David Durst <davidbdurst@gmail.com>
2022-10-17 10:24:38 -0700

    removing extra wget

--- analytics/scripts/install_dependencies.sh
+++ analytics/scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 sudo apt install gcc g++ libssl-dev make
 sudo snap install cmake --classic
-wget wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.12.1%2Bcpu.zip
+wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.12.1%2Bcpu.zip
 unzip libtorch-shared-with-deps-1.12.1+cpu.zip -d external/
 rm libtorch-shared-with-deps-1.12.1+cpu.zip
