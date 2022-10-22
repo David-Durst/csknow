14fa9250c8668f568d48d2d8882ac3cad03d5471
David Durst <davidbdurst@gmail.com>
2022-10-21 20:16:13 -0700

    trying brennan's

--- analytics/src/lib/bots/analysis/vis_geometry.cpp
+++ analytics/src/lib/bots/analysis/vis_geometry.cpp
@@ -10,9 +10,17 @@
 #include <glm/ext/matrix_clip_space.hpp> // glm::perspective
 #include <glm/ext/scalar_constants.hpp> // glm::pi
 
+static glm::mat4 makePerspectiveMatrix(float hfov, float aspect, float near, float far) {
+    float half_tan = tan(glm::radians(hfov) / 2.f);
+
+    return glm::mat4(1.f / half_tan, 0.f, 0.f, 0.f, 0.f, -aspect / half_tan,
+                     0.f, 0.f, 0.f, 0.f, far / (near - far), -1.f, 0.f, 0.f,
+                     far * near / (near - far), 0.f);
+}
+
 CellBits getCellsInFOV(const VisPoints & visPoints, const Vec3 & pos, const Vec2 & viewAngle) {
     CellBits result;
-    glm::mat4 Projection = glm::perspective(glm::radians(verticalFOV), aspectRatio, 1.f, 1000.f);
+    glm::mat4 Projection = makePerspectiveMatrix(horizontalFOV, aspectRatio, 0.001f, 100000.f);
     glm::mat4 View = glm::translate(glm::mat4(1.0f), (pos * -1).toGLM());
     View = glm::rotate(View, viewAngle.toGLM().y, glm::vec3(-1.0f, 0.0f, 0.0f));
     View = glm::rotate(View, viewAngle.toGLM().x, glm::vec3(0.0f, 1.0f, 0.0f));
