17fe2ae832b8acf3b82d18552de1507ccefe1d8b
David Durst <davidbdurst@gmail.com>
2022-10-22 00:51:07 -0700

    removing dead horizontal fov

--- analytics/include/bots/analysis/vis_geometry.h
+++ analytics/include/bots/analysis/vis_geometry.h
@@ -9,7 +9,6 @@
 #include "geometry.h"
 
 constexpr float aspectRatio = 16. / 9.;
-constexpr float horizontalFOV = 104.;
 constexpr float verticalFOV = 74.;
 
 CellBits getCellsInFOV(const VisPoints & visPoints, const Vec3 & pos, const Vec2 & viewAngle);
