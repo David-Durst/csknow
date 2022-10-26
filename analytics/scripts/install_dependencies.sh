57f6d9d57fd80d8fbd61d49aaad16847e787f2a4
David Durst <davidbdurst@gmail.com>
2022-10-25 23:10:01 -0700

    adding tar cmake requireement, gdb install

--- analytics/scripts/install_dependencies.sh
+++ analytics/scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gcc g++ libssl-dev make libopencv-dev python3-opencv cmake
+sudo apt install -y gcc g++ gdb libssl-dev make libopencv-dev python3-opencv cmake
 wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.12.1%2Bcpu.zip
 unzip libtorch-cxx11-abi-shared-with-deps-1.12.1+cpu.zip -d external/
 rm libtorch-cxx11-abi-shared-with-deps-1.12.1+cpu.zip
