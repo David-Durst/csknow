de45d48e4bb3952d9afa18f7923d391f6e400a9f
David Durst <davidbdurst@gmail.com>
2022-11-25 18:17:37 -0800

    added hit indicators to speed graph

--- learn_bot/learn_bot/engagement_aim/vis_ax_objs.py
+++ learn_bot/learn_bot/engagement_aim/vis_ax_objs.py
@@ -75,96 +75,138 @@ class PerspectiveColumns:
 
 
 class DataFrameTemporalSlices:
-    pos_prior_df: pd.DataFrame
-    pos_prior_x_np: np.ndarray
-    pos_prior_y_np: np.ndarray
+    prior_df: pd.DataFrame
+    prior_x_np: np.ndarray
+    prior_y_np: np.ndarray
 
-    pos_present_df: pd.DataFrame
-    pos_present_x_np: np.ndarray
-    pos_present_y_np: np.ndarray
+    present_df: pd.DataFrame
+    present_x_np: np.ndarray
+    present_y_np: np.ndarray
 
     # shows series used in prediction, not just single point
-    pos_present_series_df: Optional[pd.DataFrame]
-    pos_present_series_x_np: Optional[np.ndarray]
-    pos_present_series_y_np: Optional[np.ndarray]
+    present_series_df: Optional[pd.DataFrame]
+    present_series_x_np: Optional[np.ndarray]
+    present_series_y_np: Optional[np.ndarray]
 
-    pos_future_df: pd.DataFrame
-    pos_future_x_np: np.ndarray
-    pos_future_y_np: np.ndarray
+    future_df: pd.DataFrame
+    future_x_np: np.ndarray
+    future_y_np: np.ndarray
 
-    pos_all_x_np: np.ndarray
-    pos_all_y_np: np.ndarray
+    all_x_np: np.ndarray
+    all_y_np: np.ndarray
+
+    fire_df: pd.DataFrame
+    fire_x_np: np.ndarray
+    fire_y_np: np.ndarray
+
+    hold_attack_df: pd.DataFrame
+    hold_attack_x_np: np.ndarray
+    hold_attack_y_np: np.ndarray
+
+    hit_victim_df: pd.DataFrame
+    hit_victim_x_np: np.ndarray
+    hit_victim_y_np: np.ndarray
 
     def __init__(self, data_df: pd.DataFrame, tick_id: int, columns: PerspectiveColumns,
                  x_col: str, y_col: str, include_present_series: bool):
         # pos data
-        self.pos_prior_df = data_df[data_df['tick id'] < tick_id]
-        self.pos_prior_x_np = self.pos_prior_df.loc[:, x_col].to_numpy()
-        self.pos_prior_y_np = self.pos_prior_df.loc[:, y_col].to_numpy()
+        self.prior_df = data_df[data_df['tick id'] < tick_id]
+        self.prior_x_np = self.prior_df.loc[:, x_col].to_numpy()
+        self.prior_y_np = self.prior_df.loc[:, y_col].to_numpy()
 
-        self.pos_present_df = data_df[data_df['tick id'] == tick_id]
-        self.pos_present_x_np = self.pos_present_df.loc[:, x_col].to_numpy()
-        self.pos_present_y_np = self.pos_present_df.loc[:, y_col].to_numpy()
+        self.present_df = data_df[data_df['tick id'] == tick_id]
+        self.present_x_np = self.present_df.loc[:, x_col].to_numpy()
+        self.present_y_np = self.present_df.loc[:, y_col].to_numpy()
 
         # shows series used in prediction, not just single point
         if include_present_series:
-            self.pos_present_series_df = data_df[data_df['tick id'] == tick_id].iloc[0, :]
-            self.pos_present_series_x_np = self.pos_present_series_df.loc[columns.all_view_angle_x_columns].to_numpy()
-            self.pos_present_series_y_np = self.pos_present_series_df.loc[columns.all_view_angle_y_columns].to_numpy()
+            self.present_series_df = data_df[data_df['tick id'] == tick_id].iloc[0, :]
+            self.present_series_x_np = self.present_series_df.loc[columns.all_view_angle_x_columns].to_numpy()
+            self.present_series_y_np = self.present_series_df.loc[columns.all_view_angle_y_columns].to_numpy()
         else:
-            self.pos_present_series_df = None
-            self.pos_present_series_x_np = None
-            self.pos_present_series_y_np = None
+            self.present_series_df = None
+            self.present_series_x_np = None
+            self.present_series_y_np = None
 
-        self.pos_future_df = data_df[data_df['tick id'] > tick_id]
-        self.pos_future_x_np = self.pos_future_df.loc[:, x_col].to_numpy()
-        self.pos_future_y_np = self.pos_future_df.loc[:, y_col].to_numpy()
+        self.future_df = data_df[data_df['tick id'] > tick_id]
+        self.future_x_np = self.future_df.loc[:, x_col].to_numpy()
+        self.future_y_np = self.future_df.loc[:, y_col].to_numpy()
 
-        self.pos_all_x_np = data_df.loc[:, x_col].to_numpy()
-        self.pos_all_y_np = data_df.loc[:, y_col].to_numpy()
+        self.all_x_np = data_df.loc[:, x_col].to_numpy()
+        self.all_y_np = data_df.loc[:, y_col].to_numpy()
+
+        self.fire_df = data_df[data_df['ticks until next fire (t)'] == 0.]
+        self.fire_x_np = self.fire_df.loc[:, x_col].to_numpy()
+        self.fire_y_np = self.fire_df.loc[:, y_col].to_numpy()
+
+        self.hold_attack_df = data_df[data_df['ticks until next holding attack (t)'] == 0.]
+        self.hold_attack_x_np = self.hold_attack_df.loc[:, x_col].to_numpy()
+        self.hold_attack_y_np = self.hold_attack_df.loc[:, y_col].to_numpy()
+
+        self.hit_victim_df = data_df[data_df['hit victim (t)'] == 1.]
+        self.hit_victim_x_np = self.hit_victim_df.loc[:, x_col].to_numpy()
+        self.hit_victim_y_np = self.hit_victim_df.loc[:, y_col].to_numpy()
 
 
 class TemporalLines:
     # pos ax lines
-    pos_all_line: Line2D
-    pos_prior_line: Line2D
-    pos_present_line: Line2D
-    pos_present_series_line: Optional[Line2D]
-    pos_future_line: Line2D
+    all_line: Line2D
+    prior_line: Line2D
+    present_line: Line2D
+    present_series_line: Optional[Line2D]
+    future_line: Line2D
+    fire_line: Optional[Line2D]
+    hold_attack_line: Optional[Line2D]
+    hit_victim_line: Optional[Line2D]
 
     def __init__(self, ax: plt.Axes, df_temporal_slices: DataFrameTemporalSlices, include_present_series: bool):
-        self.pos_all_line, = ax.plot(df_temporal_slices.pos_all_x_np, df_temporal_slices.pos_all_y_np,
-                                     color=all_gray, label="_nolegend_")
+        self.all_line, = ax.plot(df_temporal_slices.all_x_np, df_temporal_slices.all_y_np,
+                                 color=all_gray, label="_nolegend_")
         if include_present_series:
-            self.pos_present_series_line, = ax.plot(df_temporal_slices.pos_present_series_x_np,
-                                                    df_temporal_slices.pos_present_series_y_np,
-                                                    linestyle="None", label="Model Feature",
-                                                    marker='o', mfc="None", mec=present_series_yellow,
-                                                    markersize=10)
+            self.present_series_line, = ax.plot(df_temporal_slices.present_series_x_np,
+                                                df_temporal_slices.present_series_y_np,
+                                                linestyle="None", label="Model Feature",
+                                                marker='o', mfc="None", mec=present_series_yellow,
+                                                markersize=10)
         else:
-            self.pos_present_series_line = None
-        self.pos_prior_line, = ax.plot(df_temporal_slices.pos_prior_x_np, df_temporal_slices.pos_prior_y_np,
-                                       linestyle="None", label="Past",
-                                       marker='o', mfc=prior_blue, mec=prior_blue)
-        self.pos_future_line, = ax.plot(df_temporal_slices.pos_future_x_np, df_temporal_slices.pos_future_y_np,
-                                        linestyle="None", label="Future",
-                                        marker='o', mfc=future_gray, mec=future_gray)
-        self.pos_present_line, = ax.plot(df_temporal_slices.pos_present_x_np, df_temporal_slices.pos_present_y_np,
-                                         linestyle="None", label="Present",
-                                         marker='o', mfc=present_red, mec=present_red)
+            self.present_series_line = None
+        self.prior_line, = ax.plot(df_temporal_slices.prior_x_np, df_temporal_slices.prior_y_np,
+                                   linestyle="None", label="Past",
+                                   marker='o', mfc=prior_blue, mec=prior_blue)
+        self.future_line, = ax.plot(df_temporal_slices.future_x_np, df_temporal_slices.future_y_np,
+                                    linestyle="None", label="Future",
+                                    marker='o', mfc=future_gray, mec=future_gray)
+        self.present_line, = ax.plot(df_temporal_slices.present_x_np, df_temporal_slices.present_y_np,
+                                     linestyle="None", label="Present",
+                                     marker='o', mfc=present_red, mec=present_red)
+        self.fire_line, = ax.plot(df_temporal_slices.fire_x_np, df_temporal_slices.fire_y_np,
+                                  linestyle="None", label="Fire",
+                                  marker='|', mfc=fire_attack_hit_white, mec=fire_attack_hit_white)
+        self.hold_attack_line, = ax.plot(df_temporal_slices.hold_attack_x_np, df_temporal_slices.hold_attack_y_np,
+                                         linestyle="None", label="Attack",
+                                         marker='_', mfc=fire_attack_hit_white, mec=fire_attack_hit_white)
+        self.hit_victim_line, = ax.plot(df_temporal_slices.hit_victim_x_np, df_temporal_slices.hit_victim_y_np,
+                                        linestyle="None", label="Hit",
+                                        marker='x', mfc=fire_attack_hit_white, mec=fire_attack_hit_white)
 
     def update(self, df_temporal_slices: DataFrameTemporalSlices):
-        self.pos_all_line.set_data(df_temporal_slices.pos_all_x_np,
-                                   df_temporal_slices.pos_all_y_np)
-        if self.pos_present_series_line is not None:
-            self.pos_present_series_line.set_data(df_temporal_slices.pos_present_series_x_np,
-                                                  df_temporal_slices.pos_present_series_y_np)
-        self.pos_prior_line.set_data(df_temporal_slices.pos_prior_x_np,
-                                     df_temporal_slices.pos_prior_y_np)
-        self.pos_future_line.set_data(df_temporal_slices.pos_future_x_np,
-                                      df_temporal_slices.pos_future_y_np)
-        self.pos_present_line.set_data(df_temporal_slices.pos_present_x_np,
-                                       df_temporal_slices.pos_present_y_np)
+        self.all_line.set_data(df_temporal_slices.all_x_np,
+                               df_temporal_slices.all_y_np)
+        if self.present_series_line is not None:
+            self.present_series_line.set_data(df_temporal_slices.present_series_x_np,
+                                              df_temporal_slices.present_series_y_np)
+        self.prior_line.set_data(df_temporal_slices.prior_x_np,
+                                 df_temporal_slices.prior_y_np)
+        self.future_line.set_data(df_temporal_slices.future_x_np,
+                                  df_temporal_slices.future_y_np)
+        self.present_line.set_data(df_temporal_slices.present_x_np,
+                                   df_temporal_slices.present_y_np)
+        self.fire_line.set_data(df_temporal_slices.fire_x_np,
+                                df_temporal_slices.fire_y_np)
+        self.hold_attack_line.set_data(df_temporal_slices.hold_attack_x_np,
+                                       df_temporal_slices.hold_attack_y_np)
+        self.hit_victim_line.set_data(df_temporal_slices.hit_victim_x_np,
+                                      df_temporal_slices.hit_victim_y_np)
 
 
 # https://stackoverflow.com/questions/11690597/there-is-a-class-matplotlib-axes-axessubplot-but-the-module-matplotlib-axes-has
@@ -180,9 +222,6 @@ class AxObjs:
     cur_head_columns: PerspectiveColumns
     pos_temporal_lines: Optional[TemporalLines] = None
     speed_temporal_lines: Optional[TemporalLines] = None
-    pos_fire_line: Optional[Line2D] = None
-    pos_hold_attack_line: Optional[Line2D] = None
-    pos_hit_victim_line: Optional[Line2D] = None
     pos_recoil_line: Optional[Line2D] = None
     pos_victim_head_circle: Optional[Circle] = None
     pos_victim_aabb: Optional[Rectangle] = None
@@ -194,48 +233,39 @@ class AxObjs:
                                                          columns.cur_view_angle_y_column,
                                                          True)
 
-        pos_fire_df = data_df[data_df['ticks until next fire (t)'] == 0.]
-        pos_fire_x_np = pos_fire_df.loc[:, columns.cur_view_angle_x_column].to_numpy()
-        pos_fire_y_np = pos_fire_df.loc[:, columns.cur_view_angle_y_column].to_numpy()
-
-        pos_hold_attack_df = data_df[data_df['ticks until next holding attack (t)'] == 0.]
-        pos_hold_attack_x_np = pos_hold_attack_df.loc[:, columns.cur_view_angle_x_column].to_numpy()
-        pos_hold_attack_y_np = pos_hold_attack_df.loc[:, columns.cur_view_angle_y_column].to_numpy()
-
-        pos_hit_victim_df = data_df[data_df['hit victim (t)'] == 1.]
-        pos_hit_victim_x_np = pos_hit_victim_df.loc[:, columns.cur_view_angle_x_column].to_numpy()
-        pos_hit_victim_y_np = pos_hit_victim_df.loc[:, columns.cur_view_angle_y_column].to_numpy()
-
-        pos_recoil_x_np = pos_df_temporal_slices.pos_present_df.loc[:, columns.recoil_x_column].to_numpy() + \
-                          pos_df_temporal_slices.pos_present_x_np
-        pos_recoil_y_np = pos_df_temporal_slices.pos_present_df.loc[:, columns.recoil_y_column].to_numpy() + \
-                          pos_df_temporal_slices.pos_present_y_np
+        pos_recoil_x_np = pos_df_temporal_slices.present_df.loc[:, columns.recoil_x_column].to_numpy() + \
+                          pos_df_temporal_slices.present_x_np
+        pos_recoil_y_np = pos_df_temporal_slices.present_df.loc[:, columns.recoil_y_column].to_numpy() + \
+                          pos_df_temporal_slices.present_y_np
 
         aabb_min = (
-            pos_df_temporal_slices.pos_present_df.loc[:, columns.victim_min_view_angle_x_column].item(),
-            pos_df_temporal_slices.pos_present_df.loc[:, columns.victim_min_view_angle_y_column].item()
+            pos_df_temporal_slices.present_df.loc[:, columns.victim_min_view_angle_x_column].item(),
+            pos_df_temporal_slices.present_df.loc[:, columns.victim_min_view_angle_y_column].item()
         )
         aabb_max = (
-            pos_df_temporal_slices.pos_present_df.loc[:, columns.victim_max_view_angle_x_column].item(),
-            pos_df_temporal_slices.pos_present_df.loc[:, columns.victim_max_view_angle_y_column].item()
+            pos_df_temporal_slices.present_df.loc[:, columns.victim_max_view_angle_x_column].item(),
+            pos_df_temporal_slices.present_df.loc[:, columns.victim_max_view_angle_y_column].item()
         )
         aabb_size = (
             aabb_max[0] - aabb_min[0],
             aabb_max[1] - aabb_min[1]
         )
         head_center = (
-            pos_df_temporal_slices.pos_present_df.loc[:, columns.victim_cur_head_view_angle_x_column].item(),
-            pos_df_temporal_slices.pos_present_df.loc[:, columns.victim_cur_head_view_angle_y_column].item()
+            pos_df_temporal_slices.present_df.loc[:, columns.victim_cur_head_view_angle_x_column].item(),
+            pos_df_temporal_slices.present_df.loc[:, columns.victim_cur_head_view_angle_y_column].item()
         )
         head_radius = (aabb_max[0] - aabb_min[0]) / 2. * head_scale
-        victim_visible = pos_df_temporal_slices.pos_present_df.loc[:, 'victim visible (t)'].item()
-        victim_alive = pos_df_temporal_slices.pos_present_df.loc[:, 'victim alive (t)'].item()
+        victim_visible = pos_df_temporal_slices.present_df.loc[:, 'victim visible (t)'].item()
+        victim_alive = pos_df_temporal_slices.present_df.loc[:, 'victim alive (t)'].item()
 
         # speed data
         speed_cols = []
         speed_df = pd.DataFrame()
         min_game_time = min(data_df['game time'])
         speed_df['tick id'] = data_df['tick id']
+        speed_df['ticks until next fire (t)'] = data_df['ticks until next fire (t)']
+        speed_df['ticks until next holding attack (t)'] = data_df['ticks until next holding attack (t)']
+        speed_df['hit victim (t)'] = data_df['hit victim (t)']
         speed_df['delta game time'] = (data_df['game time'] - min_game_time) / 1000.
         for i in range(-2, 3):
             x_speed_col = get_temporal_field_str("x speed at", i)
@@ -262,16 +292,6 @@ class AxObjs:
             self.pos_recoil_line, = self.pos_ax.plot(pos_recoil_x_np, pos_recoil_y_np, linestyle="None", label="Recoil",
                                                      marker='o', mfc=recoil_pink, mec=recoil_pink)
             self.pos_temporal_lines = TemporalLines(self.pos_ax, pos_df_temporal_slices, True)
-            self.pos_fire_line, = self.pos_ax.plot(pos_fire_x_np, pos_fire_y_np, linestyle="None", label="Fire",
-                                                   marker='|', mfc=fire_attack_hit_white, mec=fire_attack_hit_white)
-            self.pos_hold_attack_line, = self.pos_ax.plot(pos_hold_attack_x_np, pos_hold_attack_y_np,
-                                                          linestyle="None", label="Attack",
-                                                          marker='_', mfc=fire_attack_hit_white,
-                                                          mec=fire_attack_hit_white)
-            self.pos_hit_victim_line, = self.pos_ax.plot(pos_hit_victim_x_np, pos_hit_victim_y_np,
-                                                         linestyle="None", label="Hit",
-                                                         marker='x', mfc=fire_attack_hit_white,
-                                                         mec=fire_attack_hit_white)
             self.pos_victim_aabb = Rectangle(aabb_min, aabb_size[0], aabb_size[1],
                                              linewidth=2, edgecolor=aabb_green, facecolor='none')
             self.pos_ax.add_patch(self.pos_victim_aabb)
@@ -282,9 +302,6 @@ class AxObjs:
             self.pos_recoil_line.set_data(pos_recoil_x_np,
                                           pos_recoil_y_np)
             self.pos_temporal_lines.update(pos_df_temporal_slices)
-            self.pos_fire_line.set_data(pos_fire_x_np, pos_fire_y_np)
-            self.pos_hold_attack_line.set_data(pos_hold_attack_x_np, pos_hold_attack_y_np)
-            self.pos_hit_victim_line.set_data(pos_hit_victim_x_np, pos_hit_victim_y_np)
             self.pos_victim_aabb.set_xy(aabb_min)
             self.pos_victim_aabb.set_width(aabb_size[0])
             self.pos_victim_aabb.set_height(aabb_size[1])
