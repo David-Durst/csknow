fixed a few bugs with encoding angles using rotation matrix

learn_bot/learn_bot/engagement_aim/io_transforms.py
@@ -175,7 +175,9 @@ class ColumnTypes:
 
     def delta_float_target_column_names(self) -> List[str]:
         if self.delta_float_target_column_names_ is None:
-            self.delta_float_target_column_names_ = target_delta_columns(self.float_delta_cols)
+            self.delta_float_target_column_names_ = target_delta_columns(self.float_delta_cols +
+                                                                         self.float_180_angle_delta_cols +
+                                                                         self.float_90_angle_delta_cols)
         return self.delta_float_target_column_names_
 
 
@@ -313,8 +315,8 @@ class PTDelta180AngleColumnTransformer(PT180AngleColumnTransformer):
         return super().convert(delta_value)
 
     def delta_inverse(self, delta_value: torch.Tensor, reference_value: torch.Tensor):
-        delta_value = super().inverse(delta_value)
-        return delta_value + reference_value
+        angular_delta_value = super().inverse(delta_value)
+        return angular_delta_value + reference_value
 
 
 class PT90AngleColumnTransformer(PT180AngleColumnTransformer):
@@ -397,6 +399,8 @@ class IOColumnTransformers:
         angular_relative_cols, _ = split_delta_columns(self.input_types.float_angular_delta_cols +
                                                        self.output_types.float_angular_delta_cols)
         angular_cols = angular_standard_cols + angular_standard_cols
+        if not angular_cols:
+            return
         self.angular_mean = np.mean(all_data_df.loc[:, angular_cols].to_numpy()).item()
         self.angular_std = np.std(all_data_df.loc[:, angular_cols].to_numpy()).item()
 
@@ -611,10 +615,11 @@ class IOColumnTransformers:
             input_reference_cols = self.input_types.float_90_angle_cols
 
         cur_col_index = 0
+        all_cols = self.input_types.column_names()
         # input has baseline to compare to
         for reference_col in output_reference_cols:
             if reference_col in input_reference_cols:
-                result.append(input_reference_cols.index(reference_col))
+                result.append(all_cols.index(reference_col))
             cur_col_index += 1
 
         return result

learn_bot/learn_bot/libs/accuracy_and_loss.py
@@ -74,7 +74,9 @@ def compute_loss(pred, y, transformed_targets, attacking, transformed_last_input
     attacking = attacking.to(CPU_DEVICE_STR)
     # duplicate columns for yaw and pitch
     attacking_duplicated = torch.cat([attacking, attacking], dim=1)
+    attacking_duplicated = torch.flatten(torch.unsqueeze(attacking_duplicated, -1).expand(-1, -1, 2), 1)
     time_weights_duplicated = torch.cat([time_weights, time_weights], dim=1)
+    time_weights_duplicated = torch.flatten(torch.unsqueeze(time_weights_duplicated, -1).expand(-1, -1, 2), 1)
     #transformed_last_input_angles = transformed_last_input_angles.to(CPU_DEVICE_STR)
     #last_input_angles_x_duplicated = transformed_last_input_angles[:, [0]].expand(-1, time_weights.shape[1])
     #last_input_angles_y_duplicated = transformed_last_input_angles[:, [1]].expand(-1, time_weights.shape[1])
@@ -95,9 +97,9 @@ def compute_loss(pred, y, transformed_targets, attacking, transformed_last_input
             float_loss_fn(pred_transformed[:, col_range] * attacking_duplicated, y[:, col_range] * attacking_duplicated,
                           time_weights_duplicated)
 
-        pred_target_distances = norm_2d((pred_transformed[:, col_range] - transformed_targets))
-        y_target_distances = norm_2d(y[:, col_range] - transformed_targets)
-        losses.target_float_loss += float_loss_fn(pred_target_distances, y_target_distances, time_weights)
+        #pred_target_distances = norm_2d((pred_transformed[:, col_range] - transformed_targets))
+        #y_target_distances = norm_2d(y[:, col_range] - transformed_targets)
+        #losses.target_float_loss += float_loss_fn(pred_target_distances, y_target_distances, time_weights)
 
         #pred_speed = norm_2d(pred_transformed[:, col_range] - last_input_angles_duplicated)
         #y_speed = norm_2d(y[:, col_range] - last_input_angles_duplicated)