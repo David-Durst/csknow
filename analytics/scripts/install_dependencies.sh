369b811f9d80e83e08892253933f08907be3be5d
David Durst <davidbdurst@gmail.com>
2023-02-15 15:54:49 -0800

    adding highfive for hdf5

--- analytics/scripts/install_dependencies.sh
+++ analytics/scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
-sudo apt install -y gcc g++ gdb libssl-dev make libopencv-dev python3-opencv cmake
+sudo apt install -y gcc g++ gdb libssl-dev make libopencv-dev python3-opencv cmake libhdf5-dev
 wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcpu.zip
 unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip -d external/
 rm libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip
