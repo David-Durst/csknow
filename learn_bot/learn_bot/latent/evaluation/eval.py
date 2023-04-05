f2864333d7153228e56902758bb3c31e3a10b7b9
David Durst <davidbdurst@gmail.com>
2023-04-05 02:00:14 -0700

    graphing 500ms, cur tick, 1s and 2s nearest enemy distributions

--- learn_bot/learn_bot/latent/evaluation/eval.py
+++ learn_bot/learn_bot/latent/evaluation/eval.py
@@ -2,19 +2,21 @@ from pathlib import Path
 
 import pandas as pd
 
+from learn_bot.latent.dataset import latent_hdf5_data_path
 from learn_bot.libs.hdf5_to_pd import load_hdf5_to_pd
 from learn_bot.latent.train import plot_path
 
 rounds_hdf5_data_path = Path(__file__).parent / '..' / '..' / '..' / '..' / 'analytics' / 'csv_outputs' / 'rounds.hdf5'
 weapon_fire_hdf5_data_path = Path(__file__).parent / '..' / '..' / '..' / '..' / 'analytics' / 'csv_outputs' / 'weaponFire.hdf5'
 kills_hdf5_data_path = Path(__file__).parent / '..' / '..' / '..' / '..' / 'analytics' / 'csv_outputs' / 'kills.hdf5'
+hurt_hdf5_data_path = Path(__file__).parent / '..' / '..' / '..' / '..' / 'analytics' / 'csv_outputs' / 'hurt.hdf5'
 latent_engagement_hdf5_data_path = Path(__file__).parent / '..' / '..' / '..' / '..' / 'analytics' / 'csv_outputs' / 'latentEngagement.hdf5'
 inference_latent_engagement_hdf5_data_path = Path(__file__).parent / '..' / '..' / '..' / '..' / 'analytics' / 'csv_outputs' / 'inferenceLatentEngagement.hdf5'
 
 valid_round_numbers = [2]
 
 
-def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills_df: pd.DataFrame,
+def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills_df: pd.DataFrame, hurt_df: pd.DataFrame,
                     latent_engagement_df: pd.DataFrame, inference_latent_engagement_df: pd.DataFrame):
     # get data in valid rounds
     valid_rounds_df = rounds_df[(rounds_df['game id'] == 0) & rounds_df['round number'].isin(valid_round_numbers)]
@@ -24,6 +26,7 @@ def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills
     valid_latent_engagement = latent_engagement_df['start tick id'] < -1
     valid_inference_latent_engagement = inference_latent_engagement_df['start tick id'] < -1
     valid_kills = kills_df['tick id'] < -1
+    valid_hurt = hurt_df['tick id'] < -1
     for round_range in round_ranges:
         valid_latent_engagement = valid_latent_engagement | (
                 latent_engagement_df['start tick id'].between(round_range[0], round_range[1])
@@ -34,10 +37,14 @@ def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills
         valid_kills = valid_kills | (
             kills_df['tick id'].between(round_range[0], round_range[1])
         )
+        valid_hurt = valid_hurt | (
+            hurt_df['tick id'].between(round_range[0], round_range[1])
+        )
 
     valid_latent_engagement_df = latent_engagement_df[valid_latent_engagement].copy()
     valid_inference_latent_engagement_df = inference_latent_engagement_df[valid_inference_latent_engagement]
     valid_kills_df = kills_df[valid_kills].copy()
+    valid_hurt_df = hurt_df[valid_hurt].copy()
 
     # graph engagement length
     valid_latent_engagement_df['Engagement Length'] = \
@@ -58,9 +65,83 @@ def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills
     ax[0,0].text(3., 40., str(valid_inference_latent_engagement_df['Engagement Length'].describe()))
     ax[0,0].figure.savefig(plot_path / 'inference_latent_engagement_length.png')
 
+    # graph percent match nearest crosshair enemy cur tick
+    nearest_crosshair_enemy_col = "percent match nearest crosshair enemy cur tick"
+    bins=[i / 20. for i in range(20)]
+    ax = valid_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Percent of Frames Match Nearest Crosshair Enemy Cur Tick")
+    ax[0,0].text(0.3, 0.5, str(valid_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'latent_percent_match_cur_tick.png')
+    ax = valid_inference_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Inference Percent of Frames Match Nearest Crosshair Enemy Cur Tick")
+    ax[0,0].text(0.3, 0.5, str(valid_inference_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'inference_latent_percent_match_cur_tick.png')
+
+    # graph percent match nearest crosshair enemy 500ms
+    nearest_crosshair_enemy_col = "percent match nearest crosshair enemy 500ms"
+    bins=[i / 20. for i in range(20)]
+    ax = valid_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Percent of Frames Match Nearest Crosshair Enemy 500ms")
+    ax[0,0].text(0.3, 0.5, str(valid_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'latent_percent_match_500ms.png')
+    ax = valid_inference_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Inference Percent of Frames Match Nearest Crosshair Enemy 500ms")
+    ax[0,0].text(0.3, 0.5, str(valid_inference_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'inference_latent_percent_match_500ms.png')
+
+    # graph percent match nearest crosshair enemy 1s
+    nearest_crosshair_enemy_col = "percent match nearest crosshair enemy 1s"
+    bins=[i / 20. for i in range(20)]
+    ax = valid_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Percent of Frames Match Nearest Crosshair Enemy 1s")
+    ax[0,0].text(0.3, 0.5, str(valid_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'latent_percent_match_1s.png')
+    ax = valid_inference_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Inference Percent of Frames Match Nearest Crosshair Enemy 1s")
+    ax[0,0].text(0.3, 0.5, str(valid_inference_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'inference_latent_percent_match_1s.png')
+
+    # graph percent match nearest crosshair enemy 2s
+    nearest_crosshair_enemy_col = "percent match nearest crosshair enemy 2s"
+    bins=[i / 20. for i in range(20)]
+    ax = valid_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Percent of Frames Match Nearest Crosshair Enemy 2s")
+    ax[0,0].text(0.3, 0.5, str(valid_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'latent_percent_match_2s.png')
+    ax = valid_inference_latent_engagement_df.hist(column=nearest_crosshair_enemy_col, bins=bins)
+    ax[0,0].set_xlabel("percent of frames")
+    ax[0,0].set_ylabel("number of engagements")
+    ax[0,0].set_title("Inference Percent of Frames Match Nearest Crosshair Enemy 2s")
+    ax[0,0].text(0.3, 0.5, str(valid_inference_latent_engagement_df[nearest_crosshair_enemy_col].describe()),
+                 transform=ax[0,0].transAxes)
+    ax[0,0].figure.savefig(plot_path / 'inference_latent_percent_match_2s.png')
+
     # measure kill agreement
     num_engagement_kill_matches = 0
+    missing_engagement_kills = []
     num_inference_engagement_kill_matches = 0
+    missing_inference_engagement_kills = []
     for i in range(len(valid_kills_df)):
         killer = valid_kills_df.iloc[i].loc['killer']
         victim = valid_kills_df.iloc[i].loc['victim']
@@ -75,7 +156,8 @@ def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills
         elif len(latent_engagement_row) == 1:
             num_engagement_kill_matches += 1
         else:
-            print("latent engagement missing")
+            missing_engagement_kills.append(i)
+        #    print("latent engagement missing")
 
         inference_latent_engagement_row = valid_inference_latent_engagement_df[
             (valid_inference_latent_engagement_df['attacker id'] == killer) &
@@ -88,17 +170,66 @@ def compare_results(rounds_df: pd.DataFrame, weapon_fire_df: pd.DataFrame, kills
         elif len(inference_latent_engagement_row) == 1:
             num_inference_engagement_kill_matches += 1
         else:
-            print(f'''inference latent engagement missing {valid_kills_df.iloc[i].to_string()}''')
+            missing_inference_engagement_kills.append(i)
+        #    print(f'''inference latent engagement missing {valid_kills_df.iloc[i].to_string()}''')
     print(f'''kills {len(valid_kills_df)}, latent engagement matches {num_engagement_kill_matches}, inference latent engagement matches {num_inference_engagement_kill_matches}''')
+    if len(missing_engagement_kills) > 0:
+        print("latent missing")
+        print(valid_hurt_df.iloc[missing_engagement_kills].to_string())
+    if len(missing_inference_engagement_kills) > 0:
+        print("inference latent missing")
+        print(valid_hurt_df.iloc[missing_inference_engagement_kills].to_string())
 
+    # measure hurt agreement
+    num_engagement_hurt_matches = 0
+    missing_engagement_hurt = []
+    num_inference_engagement_hurt_matches = 0
+    missing_inference_engagement_hurt = []
+    for i in range(len(valid_hurt_df)):
+        attacker = valid_hurt_df.iloc[i].loc['attacker']
+        victim = valid_hurt_df.iloc[i].loc['victim']
+        hurt_tick_id = valid_hurt_df.iloc[i].loc['tick id']
+        latent_engagement_row = valid_latent_engagement_df[
+            (valid_latent_engagement_df['attacker id'] == attacker) &
+            (valid_latent_engagement_df['target id'] == victim) &
+            (valid_latent_engagement_df['start tick id'] <= hurt_tick_id) &
+            (valid_latent_engagement_df['end tick id'] >= hurt_tick_id - 1)]
+        if len(latent_engagement_row) > 1:
+            print("latent engagement not unique")
+        elif len(latent_engagement_row) == 1:
+            num_engagement_hurt_matches += 1
+        else:
+            missing_engagement_hurt.append(i)
+        #    print("latent engagement missing")
 
+        inference_latent_engagement_row = valid_inference_latent_engagement_df[
+            (valid_inference_latent_engagement_df['attacker id'] == attacker) &
+            (valid_inference_latent_engagement_df['target id'] == victim) &
+            (valid_inference_latent_engagement_df['start tick id'] <= hurt_tick_id) &
+            (valid_inference_latent_engagement_df['end tick id'] >= hurt_tick_id - 1)]
 
+        if len(inference_latent_engagement_row) > 1:
+            print("inference latent engagement not unique")
+        elif len(inference_latent_engagement_row) == 1:
+            num_inference_engagement_hurt_matches += 1
+        else:
+            missing_inference_engagement_hurt.append(i)
+        #    print(f'''inference latent engagement missing {valid_kills_df.iloc[i].to_string()}''')
+    print(f'''hurt {len(valid_hurt_df)}, latent engagement matches {num_engagement_hurt_matches}, inference latent engagement matches {num_inference_engagement_hurt_matches}''')
+    if len(missing_engagement_hurt) > 0:
+        print("latent missing")
+        print(valid_hurt_df.iloc[missing_engagement_hurt].to_string())
+    if len(missing_inference_engagement_hurt) > 0:
+        print("inference latent missing")
+        print(valid_hurt_df.iloc[missing_inference_engagement_hurt].to_string())
 
 
 if __name__ == "__main__":
+    ticks_df = load_hdf5_to_pd(latent_hdf5_data_path)
     rounds_df = load_hdf5_to_pd(rounds_hdf5_data_path)
     weapon_fire_df = load_hdf5_to_pd(weapon_fire_hdf5_data_path)
     kills_df = load_hdf5_to_pd(kills_hdf5_data_path)
+    hurt_df = load_hdf5_to_pd(hurt_hdf5_data_path)
     latent_engagement_df = load_hdf5_to_pd(latent_engagement_hdf5_data_path)
     inference_latent_engagement_df = load_hdf5_to_pd(inference_latent_engagement_hdf5_data_path)
-    compare_results(rounds_df, weapon_fire_df, kills_df, latent_engagement_df, inference_latent_engagement_df)
+    compare_results(rounds_df, weapon_fire_df, kills_df, hurt_df, latent_engagement_df, inference_latent_engagement_df)
