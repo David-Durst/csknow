5ff2b16dad13c11867572c8f6cb1a0dcaa888123
David Durst <davidbdurst@gmail.com>
2023-04-20 15:32:34 -0700

    all 5 models train to good top1 accuracy scores

--- learn_bot/scripts/latent_train.sh
+++ learn_bot/scripts/latent_train.sh
@@ -1,4 +1,2 @@
 python -m learn_bot.latent.train
-cp learn_bot/latent/checkpoints/engagement_* saved_models/latent_model/
-cp learn_bot/latent/checkpoints/aggression_* saved_models/latent_model/
-cp learn_bot/latent/checkpoints/order_* saved_models/latent_model/
+./scripts/save_models.sh
