03a573a8e6d1259bf3ee21870aa85bae4a02088c
David Durst <davidbdurst@gmail.com>
2023-04-20 17:09:30 -0700

    adding save models script

---
+++ learn_bot/scripts/save_models.sh
@@ -0,0 +1,5 @@
+cp learn_bot/latent/checkpoints/engagement_* saved_models/latent_model/
+cp learn_bot/latent/checkpoints/aggression_* saved_models/latent_model/
+cp learn_bot/latent/checkpoints/order_* saved_models/latent_model/
+cp learn_bot/latent/checkpoints/place_* saved_models/latent_model/
+cp learn_bot/latent/checkpoints/area_* saved_models/latent_model/
