938b5ebff78882d9c60abc6e247574028c20ac05
David Durst <davidbdurst@gmail.com>
2023-04-21 17:31:01 -0700

    adding place and are switches

--- analytics/include/bots/analysis/learned_models.h
+++ analytics/include/bots/analysis/learned_models.h
@@ -8,6 +8,8 @@
 constexpr bool useOrderModelProbabilities = true;
 constexpr bool useAggressionModelProbabilities = true;
 constexpr bool useTargetModelProbabilities = true;
+constexpr bool usePlaceModelProbabilities = true;
+constexpr bool useAreaModelProbabilities = true;
 constexpr bool runAllRounds = false;
 
 #endif //CSKNOW_LEARNED_MODELS_H
