43e4cf2fb510dcf90ee57bd7678245864c19376e
David Durst <davidbdurst@gmail.com>
2023-04-23 21:17:56 -0700

    adding keygen, moving install deps up

--- demo_generator/entry.sh
+++ demo_generator/entry.sh
@@ -7,6 +7,13 @@ python3 -u upload_logs.py >> upload.log 2>> upload.log &
 cd ~/csknow
 git pull --tags
 git checkout v0.2.0
+
+cd analytics
+scripts/install_dependencies.sh
+cd ..
+
+ssh-keygen -b 2048 -t rsa -f /tmp/sshkey -q -N ""
+
 bash ~/csknow/analytics/scripts/bot_build_docker.sh
 
 bash ~/install_link.sh
@@ -15,9 +22,6 @@ cd learn_bot
 scripts/deploy_latent_models.sh
 cd ..
 
-cd analytics
-scripts/install_dependencies.sh
-cd ..
 
 
 if [ ! -v HEURISTICS ]; then
