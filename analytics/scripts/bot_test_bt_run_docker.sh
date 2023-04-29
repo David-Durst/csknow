e2fb07e82c5b141cc1c4ef2017468900fd9beb9c
David Durst <davidbdurst@gmail.com>
2023-04-29 11:44:59 -0700

    adding actually bto test bt runner script

---
+++ analytics/scripts/bot_test_bt_run_docker.sh
@@ -0,0 +1,20 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+get_script_dir
+
+
+cd ${script_dir}/../build
+./csknow_test_bt_bot ${script_dir}/../nav ${NONVOLUMESTEAMAPPDIR}/csgo/addons/sourcemod/bot-link-data ${script_dir}/../ ${script_dir}/../../learn_bot/models \
+    ${CSKNOW_BOT_STYLE} ${CSGO_BOT_STYLE}
