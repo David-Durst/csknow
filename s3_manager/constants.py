32bc450a25c6cc63a8ef311bcefb6080ebad0508
David Durst <davidbdurst@gmail.com>
2023-04-29 14:50:47 -0700

    adding bot_retakes to s3_manager

--- s3_manager/constants.py
+++ s3_manager/constants.py
@@ -11,6 +11,16 @@ MANUAL_PROCESSED_PROS = MANUAL_DATA / "processed" / "pros"
 MANUAL_UNPROCESSED = MANUAL_DATA / "unprocessed"
 MANUAL_UNPROCESSED_BOTS = MANUAL_DATA / "unprocessed" / "bots"
 MANUAL_UNPROCESSED_PROS = MANUAL_DATA / "unprocessed" / "pros"
+BOT_RETAKES_DATA = BUCKET / "demos" / "bot_retakes_data"
+BOT_RETAKES_DISABLED = BOT_RETAKES_DATA / "disabled"
+BOT_RETAKES_CSVS = BOT_RETAKES_DATA / "csvs"
+BOT_RETAKES_PROCESSED = BOT_RETAKES_DATA / "processed"
+BOT_RETAKES_PROCESSED_BOTS = BOT_RETAKES_DATA / "processed" / "bots"
+BOT_RETAKES_PROCESSED_PROS = BOT_RETAKES_DATA / "processed" / "pros"
+BOT_RETAKES_UNPROCESSED = BOT_RETAKES_DATA / "unprocessed"
+BOT_RETAKES_UNPROCESSED_BOTS = BOT_RETAKES_DATA / "unprocessed" / "bots"
+BOT_RETAKES_UNPROCESSED_PROS = BOT_RETAKES_DATA / "unprocessed" / "pros"
+
 
 
 def s3path_str(p: PureS3Path, is_dir) -> str:
