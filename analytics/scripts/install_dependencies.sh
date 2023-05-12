5b22e1093d4fb14439bc8430b23d20c9b2c475c9
David Durst <davidbdurst@gmail.com>
2023-05-11 20:51:09 -0700

    updating install dependencies

--- analytics/scripts/install_dependencies.sh
+++ analytics/scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 sudo apt install -y gcc g++ gdb libssl-dev make libopencv-dev python3-opencv cmake libhdf5-dev
-wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcpu.zip
-unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip -d external/
-rm libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip
+wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.1%2Bcpu.zip
+unzip libtorch-cxx11-abi-shared-with-deps-1.13.1+cpu.zip -d external/
+rm libtorch-cxx11-abi-shared-with-deps-1.13.1+cpu.zip
 git submodule update --init --recursive
