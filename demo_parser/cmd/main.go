153eb2cb79e66aac397c438c87128748fab599b0
David Durst <davidbdurst@gmail.com>
2023-05-17 13:13:06 -0700

    starting to simplify parser, adding local data folder

--- demo_parser/cmd/main.go
+++ demo_parser/cmd/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -32,7 +31,7 @@ const trainDemoProcessedPrefix = "demos/train_data/processed/"
 const trainCSVPrefixBase = "demos/train_data/csvs/"
 
 // these will be used to replace the AWS S3 prefixes if using big bot train data set
-const bigTrainDemoUnprocessedPrefix = "demos/big_train_data/unprocessed/"
+const bigTrainDemoUnprocessedPrefix = "demos/big_train_data/demos/"
 const bigTrainDemoProcessedPrefix = "demos/big_train_data/processed/"
 const bigTrainCSVPrefixBase = "demos/big_train_data/csvs/"
 
@@ -69,25 +68,14 @@ func main() {
 	botRetakesDataFlag := flag.Bool("brd", false, "set if using retakes data")
 	manualDataFlag := flag.Bool("m", false, "set if using manual data")
 	rolloutDataFlag := flag.Bool("ro", false, "set if using rollout data")
-	// if reprocessing, don't move the demos
-	firstRunPtr := flag.Bool("f", true, "set if first file processed is first overall")
-	reprocessFlag := flag.Bool("r", false, "set for reprocessing demos")
-	subsetReprocessFlag := flag.Bool("s", false, "set for reprocessing a subset of demos")
 	// if running locally, skip the aws stuff and just return
 	localFlag := flag.Bool("l", false, "set for non-aws (aka local) runs")
 	localDemName := flag.String("n", "local.dem", "set for demo's name on local file system")
 	flag.Parse()
-	firstRun := *firstRunPtr
-	if *reprocessFlag && *subsetReprocessFlag {
-		fmt.Printf("-s (reprocess subset) and -r (reprocess all) can't be set at same time\n")
-		os.Exit(0)
-	}
 
 	shouldFilterRounds := !*manualDataFlag && !*botRetakesDataFlag && !*rolloutDataFlag
+	firstRun := true
 	if *localFlag {
-		if !firstRun {
-			startIDState = d.ParseInputStateCSV()
-		}
 		d.ParseDemo(*localDemName, *localDemName, &startIDState, firstRun, c.Pro, shouldFilterRounds)
 		d.SaveOutputStateCSV(&startIDState)
 		os.Exit(0)
@@ -138,97 +126,32 @@ func main() {
 	uploader := s3manager.NewUploader(sess)
 
 	var filesToMove []string
-	var destinationAppendix []string
-	sourcePrefix := demoUnprocessedPrefix
-	if *reprocessFlag {
-		sourcePrefix = demoProcessedPrefix
-	} else if *subsetReprocessFlag {
-		sourcePrefix = demoProcessedSmallPrefix
-	}
-
-	idStateAWS := csvPrefixBase + c.BaseStateCSVName
-	result, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
-		Bucket: aws.String(d.BucketName),
-		Prefix: &idStateAWS,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	// if not reprocessing and already have an id state, start from there
-	if *result.KeyCount == 1 && !*reprocessFlag && !*subsetReprocessFlag {
-		d.DownloadFile(downloader, *result.Contents[0].Key, c.InputStateCSVName)
-		startIDState = d.ParseInputStateCSV()
-		// set first run to false since found an old state and not reprocessing
-		firstRun = false
-	}
-
-	gamesAWS := csvPrefixGlobal + c.GlobalGamesCSVName
-	gamesResult, gamesErr := svc.ListObjectsV2(&s3.ListObjectsV2Input{
-		Bucket: aws.String(d.BucketName),
-		Prefix: &gamesAWS,
-	})
-	if gamesErr != nil {
-		panic(gamesErr)
-	}
-
-	// if not reprocessing and already have an games file, start from there
-	if *gamesResult.KeyCount == 1 && !*reprocessFlag && !*subsetReprocessFlag {
-		d.DownloadFile(downloader, *gamesResult.Contents[0].Key, c.GlobalGamesCSVName)
-	}
+	demosFolder := demoUnprocessedPrefix
 
 	i := 0
-	localGameTypes := c.GameTypes()
-	for gameTypeIndex, gameTypeString := range localGameTypes {
-		sourcePrefixWithType := sourcePrefix + gameTypeString + "/"
-		svc.ListObjectsV2Pages(&s3.ListObjectsV2Input{
-			Bucket: aws.String(d.BucketName),
-			Prefix: aws.String(sourcePrefixWithType),
-		}, func(p *s3.ListObjectsV2Output, last bool) bool {
-			fmt.Printf("Processing page %d\n", i)
+	svc.ListObjectsV2Pages(&s3.ListObjectsV2Input{
+		Bucket: aws.String(d.BucketName),
+		Prefix: aws.String(demosFolder),
+	}, func(p *s3.ListObjectsV2Output, last bool) bool {
+		fmt.Printf("Processing page %d\n", i)
 
-			for _, obj := range p.Contents {
-				if !strings.HasSuffix(*obj.Key, ".dem") {
-					fmt.Printf("Skipping: %s\n", *obj.Key)
-					continue
-				}
-				fmt.Printf("Handling file: %s\n", *obj.Key)
-				d.DownloadFile(downloader, *obj.Key, *localDemName)
-				d.ParseDemo(*obj.Key, *localDemName, &startIDState, firstRun, c.GameType(gameTypeIndex), shouldFilterRounds)
-				firstRun = false
-				d.UploadCSVs(uploader, *obj.Key, csvPrefixLocal)
-				filesToMove = append(filesToMove, *obj.Key)
-				destinationAppendix = append(destinationAppendix, gameTypeString+"/")
+		for _, obj := range p.Contents {
+			if !strings.HasSuffix(*obj.Key, ".dem") {
+				fmt.Printf("Skipping: %s\n", *obj.Key)
+				continue
 			}
+			fmt.Printf("Handling file: %s\n", *obj.Key)
+			d.DownloadFile(downloader, *obj.Key, *localDemName)
+			d.ParseDemo(*obj.Key, *localDemName, &startIDState, firstRun, c.Pro, shouldFilterRounds)
+			firstRun = false
+			d.UploadCSVs(uploader, *obj.Key, csvPrefixLocal)
+			filesToMove = append(filesToMove, *obj.Key)
+		}
 
-			return true
-		})
-	}
+		return true
+	})
 
 	if len(filesToMove) == 0 {
 		os.Exit(0)
-	}
-
-	d.UploadFile(uploader, c.GlobalGamesCSVName, "global_games", csvPrefixGlobal)
-	d.UploadFile(uploader, c.LocalEquipmentDimTable, "dimension_table_equipment", csvPrefixGlobal)
-	d.UploadFile(uploader, c.LocalGameTypeDimTable, "dimension_table_game_types", csvPrefixGlobal)
-	d.UploadFile(uploader, c.LocalHitGroupDimTable, "dimension_table_hit_groups", csvPrefixGlobal)
-
-	// save the id state
-	d.SaveOutputStateCSV(&startIDState)
-	d.UploadFile(uploader, c.OutputStateCSVName, "global_id_state", csvPrefixBase)
-
-	if !*reprocessFlag && !*subsetReprocessFlag {
-		for i, fileName := range filesToMove {
-			svc.CopyObject(&s3.CopyObjectInput{
-				CopySource: aws.String(d.BucketName + "/" + fileName),
-				Bucket:     aws.String(d.BucketName),
-				Key:        aws.String(demoProcessedPrefix + destinationAppendix[i] + filepath.Base(fileName)),
-			})
-			svc.DeleteObject(&s3.DeleteObjectInput{
-				Bucket: aws.String(d.BucketName),
-				Key:    aws.String(fileName),
-			})
-		}
 	}
 }
