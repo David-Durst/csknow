22f957113e0deb3552333e9e7257197b5e69c0ff
David Durst <davidbdurst@gmail.com>
2023-05-19 17:34:20 -0700

    remove old model upload

--- learn_bot/scripts/save_models.sh
+++ learn_bot/scripts/save_models.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 #
-cp learn_bot/latent/checkpoints/engagement_* saved_models/latent_model/
-cp learn_bot/latent/checkpoints/aggression_* saved_models/latent_model/
-cp learn_bot/latent/checkpoints/order_* saved_models/latent_model/
-cp learn_bot/latent/checkpoints/place_* saved_models/latent_model/
-cp learn_bot/latent/checkpoints/area_* saved_models/latent_model/
 cp learn_bot/latent/checkpoints/delta_pos_* saved_models/latent_model/
 
 aws_timestamp=$(date '+%Y_%m_%d_%H_%M_%S')
