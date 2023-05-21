8edda77e414c7b1d0a4ad1f340013e2ef578fcd1
David Durst <davidbdurst@gmail.com>
2023-05-21 14:47:48 -0700

    faster loss divided by three as averaging over whole minibatch now

--- learn_bot/learn_bot/latent/accuracy_and_loss.py
+++ learn_bot/learn_bot/latent/accuracy_and_loss.py
@@ -9,17 +9,9 @@ from learn_bot.libs.io_transforms import IOColumnTransformers, ColumnTransformer
 from math import sqrt
 from torch import nn
 
-base_float_loss_fn = nn.HuberLoss(reduction='none')
-def float_loss_fn(input, target, weight):
-    return torch.sum(weight * base_float_loss_fn(input, target)) / torch.sum(weight)
-base_binary_loss_fn = nn.BCEWithLogitsLoss()
-def binary_loss_fn(input, target, weight):
-    return torch.sum(weight * base_binary_loss_fn(input, target)) / torch.sum(weight)
 # https://stackoverflow.com/questions/65192475/pytorch-logsoftmax-vs-softmax-for-crossentropyloss
 # no need to do softmax for classification output
-base_classification_loss_fn = nn.CrossEntropyLoss()
-def classification_loss_fn(input, target, weight, weight_sum):
-    return (weight * base_classification_loss_fn(input, target)) / weight_sum
+cross_entropy_loss_fn = nn.CrossEntropyLoss()
 
 
 class LatentLosses:
@@ -50,20 +42,19 @@ def compute_loss(pred, Y, column_transformers: IOColumnTransformers):
 
     losses = LatentLosses()
 
-    if column_transformers.output_types.categorical_distribution_cols:
-        col_ranges = column_transformers.get_name_ranges(False, True, frozenset({ColumnTransformerType.CATEGORICAL_DISTRIBUTION}))
-        # remove negatives where player isn't alive
-        for i, col_range in enumerate(col_ranges):
-            valid_rows = Y[:, col_range].sum(axis=1) > 0.1
-            valid_y_transformed = Y[valid_rows][:, col_range]
-            valid_pred_transformed = pred_transformed[valid_rows][:, col_range]
-            # don't add nan loss if no valid rows
-            if valid_y_transformed.shape[0] > 0:
-                loss = base_classification_loss_fn(valid_pred_transformed, valid_y_transformed)
-                if torch.isnan(loss).any():
-                    print('bad loss')
-                losses.cat_loss += loss
-        #losses.cat_loss += latent_to_prob(pred_transformed, y_transformed, col_ranges)
+    col_ranges = column_transformers.get_name_ranges(False, False, frozenset({ColumnTransformerType.CATEGORICAL_DISTRIBUTION}))
+
+    # merging time steps so can do filtering before cross entropy loss
+    Y_per_player = torch.flatten(torch.unflatten(Y, 1, [-1, len(col_ranges[0])]), 0, 1)
+    pred_transformed_per_player = torch.flatten(torch.unflatten(pred_transformed, 1, [-1, len(col_ranges[0])]), 0, 1)
+    valid_rows = Y_per_player.sum(axis=1) > 0.1
+    valid_Y_transformed = Y_per_player[valid_rows]
+    valid_pred_transformed = pred_transformed_per_player[valid_rows]
+    if valid_Y_transformed.shape[0] > 0:
+        loss = cross_entropy_loss_fn(valid_pred_transformed, valid_Y_transformed)
+        if torch.isnan(loss).any():
+            print('bad loss')
+        losses.cat_loss += loss
     return losses
 
 
@@ -73,6 +64,7 @@ def compute_accuracy(pred, Y, accuracy, valids_per_accuracy_column, column_trans
     name = column_transformers.output_types.categorical_distribution_first_sub_cols[0]
     col_ranges = column_transformers.get_name_ranges(False, False, frozenset({ColumnTransformerType.CATEGORICAL_DISTRIBUTION}))
 
+    # keeping time steps flattened since just summing across all at end
     Y_per_player = torch.unflatten(Y, 1, [-1, len(col_ranges[0])])
     pred_untransformed_per_player = torch.unflatten(pred_untransformed, 1, [-1, len(col_ranges[0])])
     accuracy_per_player = (torch.argmax(Y_per_player, -1) ==
