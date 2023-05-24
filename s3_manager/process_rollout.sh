aaa7fdf7a6328d4a1b2f2cd4fe9ec9caabfcc3a9
David Durst <davidbdurst@gmail.com>
2023-05-24 10:07:10 -0700

    improving rollout script

--- s3_manager/process_rollout.sh
+++ s3_manager/process_rollout.sh
@@ -1,8 +1,8 @@
-conda activate s3_manager
 scp steam@54.219.195.192:csgo-ds/csgo/$1 $2
 python disable.py rollout
 python upload.py rollout $2
 python list.py rollout
 cd ../demo_parser
 go run cmd/main.go -ro
-conda deactivate
+cd ../analytics
+./scripts/create_rollout_datasets.sh
