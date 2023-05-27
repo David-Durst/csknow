6f59a9d4dfda6bf032dc22781c8fa9267dcbe58f
David Durst <davidbdurst@gmail.com>
2023-05-27 11:10:56 -0700

    same lr, larger batch size is best

--- learn_bot/learn_bot/latent/train.py
+++ learn_bot/learn_bot/latent/train.py
@@ -71,7 +71,7 @@ class TrainType(Enum):
 @dataclass
 class HyperparameterOptions:
     num_epochs: int = 300
-    batch_size: int = 256
+    batch_size: int = 2048
     learning_rate: float = 4e-5
     weight_decay: float = 0.
     layers: int = 2
