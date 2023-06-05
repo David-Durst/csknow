80822201a4bfbe4042ac4511efad4f52ce8716f8
David Durst <davidbdurst@gmail.com>
2023-06-04 21:34:37 -0700

    adding multitrajectory similiarty so main

--- analytics/src/bin/csknow_compare_train_datasets.cpp
+++ analytics/src/bin/csknow_compare_train_datasets.cpp
@@ -11,6 +11,7 @@
 #include <iomanip>
 #include <ctime>
 #include "bots/analysis/feature_store_team.h"
+#include "queries/moments/multi_trajectory_similarity.h"
 
 #include <filesystem>
 namespace fs = std::filesystem;
@@ -22,15 +23,19 @@ using std::reference_wrapper;
 int main(int argc, char * argv[]) {
     if (argc != 3) {
         std::cout << "please call this code 2 arguments: " << std::endl;
-        std::cout << "1. path/to/generated_traces" << std::endl;
-        std::cout << "2. path/to/baseline_traces" << std::endl;
+        std::cout << "1. path/to/predicted_traces" << std::endl;
+        std::cout << "2. path/to/groundTruth_traces" << std::endl;
         return 1;
     }
 
-    string generatedPath = argv[1];
-    string baselinePath = argv[2];
+    string predictedPath = argv[1];
+    string groundTruthPath = argv[2];
 
-    csknow::feature_store::TeamFeatureStoreResult generatedTraces(1, {}), baselineTraces(1, {});
-    generatedTraces.load(generatedPath);
-    baselineTraces.load(baselinePath);
+    csknow::feature_store::TeamFeatureStoreResult predictedTraces(1, {}), groundTruthTraces(1, {});
+    predictedTraces.load(predictedPath);
+    groundTruthTraces.load(groundTruthPath);
+
+    vector<csknow::multi_trajectory_similarity::MultiTrajectorySimilarityResult> multiTrajectorySimilarityResults =
+            csknow::multi_trajectory_similarity::computeMultiTrajectorySimilarityForAllPredicted(predictedTraces,
+                                                                                                 groundTruthTraces);
 }
