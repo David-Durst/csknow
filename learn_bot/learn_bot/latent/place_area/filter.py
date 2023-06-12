54862aa87315fbe8ec520fdbe898bbc982ab2162
David Durst <davidbdurst@gmail.com>
2023-06-11 18:15:42 -0700

    verified that data looks good in region where sim fails

--- learn_bot/learn_bot/latent/place_area/filter.py
+++ learn_bot/learn_bot/latent/place_area/filter.py
@@ -1,6 +1,31 @@
 import pandas as pd
 
 from learn_bot.latent.place_area.column_names import *
+from learn_bot.latent.place_area.pos_abs_delta_conversion import *
 
-def filter_region(all_data_df: pd.DataFrame) -> pd.DataFrame:
-    return
+num_alive_col = 'num alive'
+
+
+def filter_region(all_data_df: pd.DataFrame, valid_range: AABB, must_plant_a: bool, must_plant_b: bool,
+                  required_num_alive: List[int]) -> pd.DataFrame:
+    # build or because allow any player to be in range
+    filter_builder: pd.Series = all_data_df[tick_id_column] != all_data_df[tick_id_column]
+    for player_columns in specific_player_place_area_columns:
+        filter_builder = filter_builder | \
+                         (all_data_df[player_columns.pos[0]].between(valid_range.min.x, valid_range.max.x) &
+                          all_data_df[player_columns.pos[1]].between(valid_range.min.y, valid_range.max.y))
+    if must_plant_a:
+        filter_builder = filter_builder & all_data_df[c4_plant_a_col]
+    if must_plant_b:
+        filter_builder = filter_builder & all_data_df[c4_plant_b_col]
+    pos_restricted_df = all_data_df[filter_builder].copy()
+
+    # after big cut by pos, restrict next by num alive
+    alive_cols = []
+    for player_columns in specific_player_place_area_columns:
+        alive_cols.append(player_columns.alive)
+    pos_restricted_df[num_alive_col] = pos_restricted_df[alive_cols].sum(axis=1)
+    pos_alive_restricted_df = pos_restricted_df[pos_restricted_df[num_alive_col].isin(required_num_alive)].copy()
+
+    return pos_alive_restricted_df
+
--- learn_bot/learn_bot/latent/vis/run_vis_checkpoint.py
+++ learn_bot/learn_bot/latent/vis/run_vis_checkpoint.py
@@ -6,7 +6,8 @@ import torch
 
 from learn_bot.latent.dataset import LatentDataset
 from learn_bot.latent.engagement.column_names import max_enemies
-from learn_bot.latent.place_area.pos_abs_delta_conversion import delta_pos_grid_num_cells
+from learn_bot.latent.place_area.filter import filter_region
+from learn_bot.latent.place_area.pos_abs_delta_conversion import delta_pos_grid_num_cells, AABB
 from learn_bot.latent.place_area.column_names import round_id_column, place_area_input_column_types, \
     delta_pos_output_column_types, test_success_col
 from learn_bot.libs.hdf5_to_pd import load_hdf5_to_pd
@@ -17,6 +18,7 @@ from learn_bot.latent.train import checkpoints_path, TrainResult, manual_latent_
 from learn_bot.libs.df_grouping import make_index_column, train_test_split_by_col_ids
 from learn_bot.latent.vis.off_policy_inference import off_policy_inference
 from learn_bot.latent.vis.vis import vis
+from learn_bot.libs.vec import Vec3
 
 
 def load_model_file(all_data_df: pd.DataFrame, model_file_name: str) -> TrainResult:
@@ -78,6 +80,9 @@ if __name__ == "__main__":
         all_data_df = all_data_df[(all_data_df['valid'] == 1.) & (all_data_df['c4 status'] < 2)]
     #all_data_df = all_data_df[all_data_df[test_success_col] == 1.]
     all_data_df = all_data_df.copy()
+
+    all_data_df = filter_region(all_data_df, AABB(Vec3(530., 1996., 0.), Vec3(1064., 2700., 0.)), True, False,
+                                [1, 2, 3])
 
     #for flip_column in [ColumnsToFlip(" CT 1", " CT 2")]:
     #    flip_column.apply_flip(all_data_df)
