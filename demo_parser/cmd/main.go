ddcdc2fe46e87f7f68c36cbdc3b3d6eb4008c4f7
David Durst <davidbdurst@gmail.com>
2023-06-22 18:44:33 -0700

    pritning parser state better

--- demo_parser/cmd/main.go
+++ demo_parser/cmd/main.go
@@ -135,7 +135,6 @@ func main() {
 					// reset state on each hdf5 write
 					startIDState = d.DefaultIDState()
 				}
-				fmt.Printf("%d valid demos / %d total demos, %d hdf5s\n", validDemos, totalDemos, hdf5Index)
 			}
 			totalDemos++
 			if *deleteLocalDemFlag {
@@ -155,6 +154,7 @@ func main() {
 					firstRun = false
 				}
 			*/
+			fmt.Printf("%d valid demos / %d total demos, %d hdf5s\n", validDemos, totalDemos, hdf5Index)
 		}
 
 		i++
