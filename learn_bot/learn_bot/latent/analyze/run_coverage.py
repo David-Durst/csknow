9533171feb5f2a551e24f498441c91e044923777
David Durst <davidbdurst@gmail.com>
2023-08-11 22:53:13 -0700

    logaritmic vmap makes heatmap look good

--- learn_bot/learn_bot/latent/analyze/run_coverage.py
+++ learn_bot/learn_bot/latent/analyze/run_coverage.py
@@ -1,12 +1,15 @@
+import pickle
+
 import numpy as np
 import pandas as pd
 import torch
 from matplotlib import pyplot as plt
-from matplotlib.colors import TwoSlopeNorm, LinearSegmentedColormap
+from matplotlib.colors import TwoSlopeNorm, LinearSegmentedColormap, LogNorm
 
 from learn_bot.latent.place_area.column_names import PlayerPlaceAreaColumns
 from learn_bot.latent.place_area.simulator import *
 
+tmp_file = Path(__file__).parent / 'plots' / 'coverage.pickle'
 
 def compute_coverage_metrics(loaded_model: LoadedModel):
     num_ticks = 0
@@ -16,48 +19,44 @@ def compute_coverage_metrics(loaded_model: LoadedModel):
     x_pos_bins = None
     y_pos_bins = None
 
-    for i, hdf5_wrapper in enumerate(loaded_model.dataset.data_hdf5s):
-        print(f"Processing hdf5 {i + 1} / {len(loaded_model.dataset.data_hdf5s)}: {hdf5_wrapper.hdf5_path}")
-        loaded_model.cur_hdf5_index = i
-        loaded_model.load_cur_hdf5_as_pd(load_cur_dataset=False)
+    if True:
+        for i, hdf5_wrapper in enumerate(loaded_model.dataset.data_hdf5s):
+            print(f"Processing hdf5 {i + 1} / {len(loaded_model.dataset.data_hdf5s)}: {hdf5_wrapper.hdf5_path}")
+            loaded_model.cur_hdf5_index = i
+            loaded_model.load_cur_hdf5_as_pd(load_cur_dataset=False)
 
-        num_ticks += len(loaded_model.cur_loaded_df)
-        for player_columns in specific_player_place_area_columns:
-            num_alive_pats += loaded_model.cur_loaded_df[player_columns.alive].sum()
-            alive_df = loaded_model.cur_loaded_df[loaded_model.cur_loaded_df[player_columns.alive].astype('bool')]
-            x_pos = alive_df[player_columns.pos[0]].to_numpy()
-            y_pos = alive_df[player_columns.pos[1]].to_numpy()
-            if x_pos_bins is None:
-                sum_pos_heatmap, x_pos_bins, y_pos_bins = np.histogram2d(x_pos, y_pos, bins=125,
-                                                                         range=[[d2_min[0], d2_max[0]], [d2_min[1], d2_max[1]]])
-            else:
-                pos_heatmap, _, _ = np.histogram2d(x_pos, y_pos, bins=[x_pos_bins, y_pos_bins])
-                sum_pos_heatmap += pos_heatmap
+            num_ticks += len(loaded_model.cur_loaded_df)
+            for player_columns in specific_player_place_area_columns:
+                num_alive_pats += loaded_model.cur_loaded_df[player_columns.alive].sum()
+                alive_df = loaded_model.cur_loaded_df[loaded_model.cur_loaded_df[player_columns.alive].astype('bool')]
+                x_pos = alive_df[player_columns.pos[0]].to_numpy()
+                y_pos = alive_df[player_columns.pos[1]].to_numpy()
+                if x_pos_bins is None:
+                    sum_pos_heatmap, x_pos_bins, y_pos_bins = np.histogram2d(x_pos, y_pos, bins=125,
+                                                                             range=[[d2_min[0], d2_max[0]], [d2_min[1], d2_max[1]]])
+                else:
+                    pos_heatmap, _, _ = np.histogram2d(x_pos, y_pos, bins=[x_pos_bins, y_pos_bins])
+                    sum_pos_heatmap += pos_heatmap
+        with open(tmp_file, "wb") as outfile:
+            # "wb" argument opens the file in binary mode
+            pickle.dump((sum_pos_heatmap, x_pos_bins, y_pos_bins), outfile)
+    else:
+        with open(tmp_file, "rb") as infile:
+            (sum_pos_heatmap, x_pos_bins, y_pos_bins) = pickle.load(infile)
 
-        if i == 4:
-            break
 
 
     fig = plt.figure(figsize=(10, 10), constrained_layout=True)
-    fig.suptitle("Bot vs Human Metrics")
+    fig.suptitle("Data Set Coverage Of de_dust2 Map")
     ax = fig.subplots(1, 1)
 
     sum_pos_heatmap = sum_pos_heatmap.T
 
     grid_x, grid_y = np.meshgrid(x_pos_bins, y_pos_bins)
-    colors_not_occupied = plt.cm.magma(np.linspace(0, 0.25, 256))
-    colors_occupied = plt.cm.viridis(np.linspace(0.25, 1, 256))
-    all_colors = np.vstack((colors_not_occupied, colors_occupied))
-    not_to_occupied_map = LinearSegmentedColormap.from_list(
-        'not_to_occupied_map', all_colors)
 
-    heatmap_im = ax.pcolormesh(grid_x, grid_y, sum_pos_heatmap,
-                           norm=TwoSlopeNorm(vmin=0, vcenter=2, vmax=sum_pos_heatmap.max()),
-                           cmap=not_to_occupied_map)
+    heatmap_im = ax.pcolormesh(grid_x, grid_y, sum_pos_heatmap, norm=LogNorm(vmin=1, vmax=sum_pos_heatmap.max()),
+                               cmap='viridis')
     fig.colorbar(heatmap_im, ax=ax)
-    ax.set_title(f"Hit Aim With Recoil Distribution")
-    ax.set_xlabel("Normalized Yaw Distance (1 = AABB width)")
-    ax.set_ylabel("Normalized Pitch Distance (1 = AABB height)")
 
     plt.savefig(Path(__file__).parent / 'plots' / 'coverage.png')
 
