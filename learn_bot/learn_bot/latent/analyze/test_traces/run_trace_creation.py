949460663d1d7f5b54cfee4c25974a5ba82beda8
David Durst <davidbdurst@gmail.com>
2023-08-21 16:35:05 -0700

    updating trace rounds, adding bot and team index

--- learn_bot/learn_bot/latent/analyze/test_traces/run_trace_creation.py
+++ learn_bot/learn_bot/latent/analyze/test_traces/run_trace_creation.py
@@ -11,6 +11,8 @@ from learn_bot.libs.hdf5_to_pd import save_pd_to_hdf5
 class RoundForTrace:
     demo_file: str
     round_number: int
+    ct_bot: bool
+    one_player_team_index: int
     start_index_in_hdf5: int = -1
     length: int = -1
 
@@ -18,17 +20,24 @@ class RoundForTrace:
         return {
             "demo file": self.demo_file,
             "round number": self.round_number,
+            "ct bot": self.ct_bot,
+            "one player team index": self.one_player_team_index,
             "start index in hdf5": self.start_index_in_hdf5,
             "length": self.length,
-            ""
         }
 
 
 rounds_for_traces: List[RoundForTrace] = [
-    # _28, 0/52, round id 84 (i think, roughly)
-    RoundForTrace("2354548_135549_outsiders-vs-ence-m2-dust2_9c77e360-a7ad-11ec-8656-0a58a9feac02.dem", 20),
-    # _31, 1/52, round id 25
-    RoundForTrace("2355685_136862_1620-kings-vs-dynasty-m2-dust2_1241692a-bb56-11ec-8b31-0a58a9feac02.dem", 0)
+    RoundForTrace("2355808_137170_spirit-vs-asg-dust2_38157d06-c265-11ec-b800-0a58a9feac02.dem", 15, False, 1),
+    RoundForTrace("2355804_137165_entropiq-vs-endpoint-dust2_e90097d6-c265-11ec-ba43-0a58a9feac02.dem", 2, False, 4),
+    RoundForTrace("2355741_137031_vitality-vs-eternal-fire-dust2_e0beba24-c00a-11ec-a32e-0a58a9feac02.dem", 14, False, 3),
+    RoundForTrace("2354576_136413_natus-vincere-vs-evil-geniuses-m2-dust2_7e07ebd2-b1e4-11ec-a36e-0a58a9feac02.dem", 23, False, 4),
+    RoundForTrace("2354551_135613_outsiders-vs-sprout-m3-dust2_960c03dc-a878-11ec-84df-0a58a9feac02.dem", 19, False, 4),
+    RoundForTrace("2355685_136862_1620-kings-vs-dynasty-m2-dust2_1241692a-bb56-11ec-8b31-0a58a9feac02.dem", 16, False, 0),
+    RoundForTrace("2355808_137170_spirit-vs-asg-dust2_38157d06-c265-11ec-b800-0a58a9feac02.dem", 15, True, 2),
+    RoundForTrace("2355669_136889_masonic-vs-young-ninjas-m2-dust2_9364d3e6-bb53-11ec-a7e7-0a58a9feac02.dem", 2, True, 4),
+    RoundForTrace("2354561_135904_liquid-vs-godsent-m2-dust2_3f92bd4e-ac64-11ec-9e6c-0a58a9feac02.dem", 10, True, 0),
+    RoundForTrace("2348918_120880_1win-vs-izako-boars-m1-dust2_8b824c6c-be4c-11eb-86da-0a58a9feac02.dem", 9, True, 0),
 ]
 
 trace_file_name = 'traces.hdf5'
