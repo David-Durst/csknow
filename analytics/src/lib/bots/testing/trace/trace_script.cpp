c18d270744d5527bac490b46b666414e613906c8
David Durst <davidbdurst@gmail.com>
2023-08-22 12:08:10 -0700

    fixing camera for traces

--- analytics/src/lib/bots/testing/trace/trace_script.cpp
+++ analytics/src/lib/bots/testing/trace/trace_script.cpp
@@ -12,35 +12,46 @@ namespace csknow::tests::trace {
                              roundIndex(roundIndex), numRounds(numRounds), oneTeam(oneTeam), oneBot(oneBot) {
         int64_t tickInFeatureStore = tracesData.startIndices[roundIndex];
 
-        name += tracesData.demoFile[roundIndex] + "_" + std::to_string(tracesData.roundNumber[roundIndex]);
+        name += tracesData.demoFile[roundIndex] + "_" + std::to_string(tracesData.roundNumber[roundIndex]) +
+                "_" + std::to_string(oneTeam) + "_" + std::to_string(oneBot);
         int numCT = 0, numT = 0;
         neededBots.clear();
 
         c4Pos = tracesData.teamFeatureStoreResult.c4Pos[tickInFeatureStore];
+        map<int64_t, int64_t> ctFeatureStoreIndexToBotIndex, tFeatureStoreIndexToBotIndex;
         for (size_t i = 0; i < feature_store::max_enemies; i++) {
             if (tracesData.teamFeatureStoreResult.columnCTData[i].alive[tickInFeatureStore] && numCT < maxCT) {
+                ctFeatureStoreIndexToBotIndex[numCT] = static_cast<int64_t>(i);
                 numCT++;
                 neededBots.push_back({0, ENGINE_TEAM_CT});
             }
             if (tracesData.teamFeatureStoreResult.columnTData[i].alive[tickInFeatureStore] && numT < maxT) {
+                tFeatureStoreIndexToBotIndex[numT] = static_cast<int64_t>(i);
                 numT++;
                 neededBots.push_back({0, ENGINE_TEAM_T});
             }
         }
 
-        /*
-        // spectate a bot if one team is bots
-        if (oneTeam) {
+        // if one team is bots, spectate a bot from that team
+        // if one player is a bot, spectate that bot
+        if (oneBot) {
             if (tracesData.ctBot[roundIndex]) {
-                for (size_t
-                observeSettings.neededBotIndex = 0;
-
+                observeSettings.neededBotIndex =
+                        ctFeatureStoreIndexToBotIndex[tracesData.oneBotFeatureStoreIndex[roundIndex]];
             }
             else {
-
+                observeSettings.neededBotIndex =
+                        tFeatureStoreIndexToBotIndex[tracesData.oneBotFeatureStoreIndex[roundIndex]];
             }
         }
-         */
+        if (oneTeam) {
+            if (tracesData.ctBot[roundIndex]) {
+                observeSettings.neededBotIndex = ctFeatureStoreIndexToBotIndex[0];
+            }
+            else {
+                observeSettings.neededBotIndex = tFeatureStoreIndexToBotIndex[0];
+            }
+        }
     }
 
     void TraceScript::initialize(Tree &tree, ServerState &state) {
@@ -83,7 +94,7 @@ namespace csknow::tests::trace {
 
         int64_t numRounds = 300;/*static_cast<int64_t>(tracesData.demoFile.size());*/
         for (int64_t i = 0; i < numRounds; i++) {
-            //result.push_back(make_unique<TraceScript>(tracesData, 0, numRounds, true, false));
+            result.push_back(make_unique<TraceScript>(tracesData, 0, numRounds, true, false));
             result.push_back(make_unique<TraceScript>(tracesData, 0, numRounds, true, true));
         }
         if (quitAtEnd) {
