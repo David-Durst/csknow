341702ce1484cb8a5fb868ff99595ade5b71d5cc
David Durst <davidbdurst@gmail.com>
2023-10-04 23:10:57 -0700

    running training on all epochs

--- learn_bot/learn_bot/latent/train.py
+++ learn_bot/learn_bot/latent/train.py
@@ -54,7 +54,7 @@ class TrainType(Enum):
 
 default_hyperparameter_options = HyperparameterOptions()
 hyperparameter_option_range = [HyperparameterOptions(num_input_time_steps=1),
-                               HyperparameterOptions(num_input_time_steps=3, bc_epochs=0, probabilistic_rollout_epochs=0, full_rollout_epochs=10),
+                               HyperparameterOptions(num_input_time_steps=3, full_rollout_epochs=10),
                                HyperparameterOptions(num_input_time_steps=5, bc_epochs=40),
                                HyperparameterOptions(num_input_time_steps=25, bc_epochs=40),
                                HyperparameterOptions(player_mask_type=PlayerMaskType.EveryoneTemporalOnlyMask),
