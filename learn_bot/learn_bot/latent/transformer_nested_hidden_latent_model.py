bb31412e066493b769fa22d263edaf286ca2ef09
David Durst <davidbdurst@gmail.com>
2023-10-27 17:03:20 -0700

    start of removing time

--- learn_bot/learn_bot/latent/transformer_nested_hidden_latent_model.py
+++ learn_bot/learn_bot/latent/transformer_nested_hidden_latent_model.py
@@ -86,8 +86,7 @@ class TransformerNestedHiddenLatentModel(nn.Module):
     latent_to_distributions: Callable
     noise_var: float
 
-    def __init__(self, cts: IOColumnTransformers, internal_width: int, num_players: int, num_input_time_steps: int,
-                 num_output_time_steps: int, num_radial_bins: int,
+    def __init__(self, cts: IOColumnTransformers, internal_width: int, num_players: int,
                  num_layers: int, num_heads: int, player_mask_type: PlayerMaskType, mask_non_pos: bool):
         super(TransformerNestedHiddenLatentModel, self).__init__()
         self.cts = cts
@@ -111,111 +110,43 @@ class TransformerNestedHiddenLatentModel(nn.Module):
 
         all_players_pos_columns = get_player_columns_by_str(cts, "player pos")
         all_players_pos_columns_tensor = torch.IntTensor(all_players_pos_columns)
-        nested_players_pos_columns_tensor = rearrange(all_players_pos_columns_tensor, '(p t d) -> p t d',
-                                                      p=self.num_players, t=all_prior_and_cur_ticks, d=self.num_dim)
-        self.players_pos_columns = rearrange(nested_players_pos_columns_tensor[:, 0:num_input_time_steps, :],
-                                             'p t d -> (p t d)',
-                                             p=self.num_players, t=num_input_time_steps, d=self.num_dim).tolist()
-        self.players_cur_pos_columns = rearrange(nested_players_pos_columns_tensor[:, 0, :], 'p d -> (p d)',
-                                                 p=self.num_players, d=self.num_dim).tolist()
-        all_players_nearest_crosshair_to_enemy_columns = \
-            get_player_columns_by_str(cts, "player nearest crosshair distance to enemy")
-        all_players_nearest_crosshair_to_enemy_columns_tensor = \
-            torch.IntTensor(all_players_nearest_crosshair_to_enemy_columns)
-        nested_players_nearest_crosshair_to_enemy_columns_tensor = \
-            rearrange(all_players_nearest_crosshair_to_enemy_columns_tensor, '(p t) -> p t', p=self.num_players,
-                      t=all_prior_and_cur_ticks)
+        nested_players_pos_columns_tensor = rearrange(all_players_pos_columns_tensor, '(p d) -> p d',
+                                                      p=self.num_players, d=self.num_dim)
+        self.players_pos_columns = rearrange(nested_players_pos_columns_tensor, 'p d -> (p d)',
+                                             p=self.num_players, d=self.num_dim).tolist()
         self.players_nearest_crosshair_to_enemy_columns = \
-            rearrange(nested_players_nearest_crosshair_to_enemy_columns_tensor[:, 0:num_input_time_steps],
-                      'p t -> (p t)', p=self.num_players, t=num_input_time_steps).tolist()
-        self.players_seconds_to_hit_enemy = range_list_to_index_list(cts.get_name_ranges(True, True, contained_str="player seconds"))
+            get_player_columns_by_str(cts, "player nearest crosshair distance to enemy")
+        self.players_seconds_to_hit_enemy = \
+            range_list_to_index_list(cts.get_name_ranges(True, True, contained_str="player seconds"))
 
-        #self.players_hurt_in_last_5s_columns = get_player_columns_by_str(cts, "player hurt in last 5s")
-        #self.players_fire_in_last_5s_columns = get_player_columns_by_str(cts, "player fire in last 5s")
-        #self.players_enemy_visible_in_last_5s_columns = get_player_columns_by_str(cts, "player enemy visible in last 5s")
-        #self.players_health_columns = get_player_columns_by_str(cts, "player health")
-        #self.players_armor_columns = get_player_columns_by_str(cts, "player armor")
-        #self.players_vel_columns = flatten_list(
-        #    [range_list_to_index_list(cts.get_name_ranges(True, False, contained_str="player velocity " + player_team_str(team_str, player_index)))
-        #     for team_str in team_strs for player_index in range(max_enemies)]
-        #)
-        self.player_all_temporal_columns = all_players_pos_columns + all_players_nearest_crosshair_to_enemy_columns
+        self.player_normalized_columns = all_players_pos_columns + self.players_nearest_crosshair_to_enemy_columns
         self.players_non_temporal_columns = flatten_list([
             [player_column for player_column in player_columns
-             if player_column not in (self.player_all_temporal_columns + self.players_seconds_to_hit_enemy)]
+             if player_column not in (self.players_normalized_columns + self.players_seconds_to_hit_enemy)]
             for player_columns in players_columns
         ])
         self.num_similarity_columns = 2
 
-        # may have more input time steps loaded than used
-        self.total_input_time_steps = len(all_players_pos_columns) // self.num_dim // self.num_players
-        assert all_prior_and_cur_ticks == self.total_input_time_steps
-        self.num_input_time_steps = num_input_time_steps
-        assert self.num_input_time_steps <= self.total_input_time_steps
-        self.num_output_time_steps = num_output_time_steps
-        assert self.num_output_time_steps <= self.num_input_time_steps or self.num_input_time_steps == 1
+        # ensure player counts right
         assert self.num_players == num_players
         self.num_players_per_team = self.num_players // 2
 
-        # only different players in same time step to talk to each other
-        def build_per_player_mask(per_player_mask: torch.Tensor, num_temporal_tokens: int, num_time_steps: int,
-                                  teammate_only_mask: bool, enemy_only_mask: bool, mask_self: bool):
-            for i in range(num_temporal_tokens):
-                for j in range(num_temporal_tokens):
-                    if teammate_only_mask:
-                        if mask_self:
-                            per_player_mask[i, j] = (i // num_time_steps // self.num_players_per_team) == \
-                                                    (j // num_time_steps // self.num_players_per_team)
-                        else:
-                            per_player_mask[i, j] = (i // num_time_steps) != (j // num_time_steps) and \
-                                                    (i // num_time_steps // self.num_players_per_team) == \
-                                                    (j // num_time_steps // self.num_players_per_team)
-                    elif enemy_only_mask:
-                        # include self in enemy mask
-                        per_player_mask[i, j] = (i // num_time_steps // self.num_players_per_team) != \
-                                                (j // num_time_steps // self.num_players_per_team)
-                    else:
-                        per_player_mask[i, j] = (i // num_time_steps) != (j // num_time_steps)
-
-        num_input_temporal_tokens = self.num_players * self.num_input_time_steps
-        self.input_per_player_mask_cpu = torch.zeros([num_input_temporal_tokens, num_input_temporal_tokens],
-                                                     dtype=torch.bool)
-        self.input_per_player_no_history_mask_cpu = torch.zeros([self.num_players, self.num_players],
+        # compute mask to isolate everyone
+        self.input_per_player_mask_cpu = torch.zeros([self.num_players, self.num_players],
                                                                 dtype=torch.bool)
-        teammate_only_mask = (player_mask_type == PlayerMaskType.TeammateTemporalOnlyMask) or \
-                             (player_mask_type == PlayerMaskType.TeammateFullMask)
-        enemy_only_mask = (player_mask_type == PlayerMaskType.EnemyTemporalOnlyMask) or \
-                          (player_mask_type == PlayerMaskType.EnemyFullMask)
-        if player_mask_type != PlayerMaskType.NoMask:
-            build_per_player_mask(self.input_per_player_mask_cpu, num_input_temporal_tokens, self.num_input_time_steps,
-                                  False, teammate_only_mask or enemy_only_mask, False)
-            build_per_player_mask(self.input_per_player_no_history_mask_cpu, self.num_players, 1, False,
-                                  teammate_only_mask or enemy_only_mask, False)
-
-        num_output_temporal_tokens = self.num_players * self.num_output_time_steps
-        self.output_per_player_mask_cpu = torch.zeros([num_output_temporal_tokens, num_output_temporal_tokens],
-                                                      dtype=torch.bool)
-        self.output_teammate_per_player_mask_cpu = torch.zeros([num_output_temporal_tokens, num_output_temporal_tokens],
-                                                               dtype=torch.bool)
-        self.output_self_teammate_per_player_mask_cpu = torch.zeros([num_output_temporal_tokens, num_output_temporal_tokens],
-                                                                    dtype=torch.bool)
-        if player_mask_type != PlayerMaskType.NoMask:
-            build_per_player_mask(self.output_per_player_mask_cpu, num_output_temporal_tokens,
-                                  self.num_output_time_steps, False, teammate_only_mask or enemy_only_mask, False)
-            build_per_player_mask(self.output_teammate_per_player_mask_cpu, num_output_temporal_tokens,
-                                  self.num_output_time_steps, teammate_only_mask, enemy_only_mask, False)
-            build_per_player_mask(self.output_self_teammate_per_player_mask_cpu, num_output_temporal_tokens,
-                                  self.num_output_time_steps, teammate_only_mask, enemy_only_mask, teammate_only_mask)
+        for i in range(self.num_players):
+            for j in range(self.num_players):
+                self.input_per_player_mask_cpu[i, j] = i != j
 
         self.player_mask_type = player_mask_type
 
+        # get alive columns
         self.alive_columns = flatten_list(
             [range_list_to_index_list(cts.get_name_ranges(True, True, contained_str=player_place_area_columns.alive))
              for player_place_area_columns in specific_player_place_area_columns]
         )
 
         self.noise_var = -1.
-        self.drop_history = False
 
         self.d2_min_cpu = torch.tensor(d2_min)
         self.d2_max_cpu = torch.tensor(d2_max)
@@ -236,12 +167,7 @@ class TransformerNestedHiddenLatentModel(nn.Module):
                                             #len(self.players_nearest_crosshair_to_enemy_columns) // self.num_players // self.num_input_time_steps # + \
                                             #(len(self.players_vel_columns) // self.num_players // self.num_time_steps)
 
-        # positional encoding library doesn't play well with torchscript, so I'll just make the
-        # encoding matrices upfront and add them during inference
-        temporal_positional_encoder = PositionalEncoding1D(self.internal_width)
-        self.temporal_positional_encoding = \
-            temporal_positional_encoder(torch.zeros([self.num_players, self.num_input_time_steps, self.internal_width]))
-
+        # build actual model layers
         self.embedding_model = nn.Sequential(
             nn.Linear(self.columns_per_player_time_step, self.internal_width),
             nn.LeakyReLU(),
@@ -253,24 +179,12 @@ class TransformerNestedHiddenLatentModel(nn.Module):
         self.nav_data_cpu = NavData(CPU_DEVICE_STR)
         self.nav_data_cuda = NavData(CUDA_DEVICE_STR)
 
-        temporal_transformer_encoder_layer = nn.TransformerEncoderLayer(d_model=self.internal_width, nhead=num_heads, batch_first=True)
-        self.temporal_transformer_encoder = nn.TransformerEncoder(temporal_transformer_encoder_layer, num_layers=num_layers, enable_nested_tensor=False)
-        spatial_transformer_encoder_layer = nn.TransformerEncoderLayer(d_model=self.internal_width, nhead=num_heads, batch_first=True)
-        self.spatial_transformer_encoder = nn.TransformerEncoder(spatial_transformer_encoder_layer, num_layers=num_layers, enable_nested_tensor=False)
-        teammate_mask_transformer_decoder_layer = nn.TransformerDecoderLayer(d_model=self.internal_width, nhead=num_heads, batch_first=True)
-        self.teammate_mask_transformer_decoder = nn.TransformerDecoder(teammate_mask_transformer_decoder_layer, num_layers=1)
-        #self.transformer_model = nn.Transformer(d_model=self.internal_width, nhead=num_heads,
-        #                                        num_encoder_layers=num_layers, num_decoder_layers=num_layers,
-        #                                        custom_encoder=transformer_encoder, batch_first=True)
+        transformer_encoder_layer = nn.TransformerEncoderLayer(d_model=self.internal_width, nhead=num_heads, batch_first=True)
+        self.transformer_encoder = nn.TransformerEncoder(transformer_encoder_layer, num_layers=num_layers, enable_nested_tensor=False)
 
         self.decoder = nn.Sequential(
             nn.Linear(self.internal_width, num_radial_bins),
         )
-
-        # dim 0 is batch, 1 is players, 2 is output time step, 3 is probabilities/logits
-        #self.logits_output = nn.Sequential(
-        #    nn.Flatten(1)
-        #)
 
         self.prob_output = nn.Sequential(
             nn.Softmax(dim=3),
@@ -318,32 +232,15 @@ class TransformerNestedHiddenLatentModel(nn.Module):
         return team_mask
 
     def forward(self, x, similarity, temperature):
-        x_pos = rearrange(x[:, self.players_pos_columns], "b (p t d) -> b p t d", p=self.num_players,
-                          t=self.num_input_time_steps, d=self.num_dim)
-        x_crosshair = rearrange(x[:, self.players_nearest_crosshair_to_enemy_columns], "b (p t) -> b p t 1",
-                                p=self.num_players, t=self.num_input_time_steps)
-        # delta encode prior pos
-        #if self.num_input_time_steps < self.total_input_time_steps:
-        #    x_pos = x_pos_all_time_steps[:, :, 0:self.num_input_time_steps, :]
-        #    x_crosshair = x_crosshair_all_time_steps[:, :, 0:self.num_input_time_steps, :]
-        #else:
-        #    x_pos = x_pos_all_time_steps
-        #    x_crosshair = x_crosshair_all_time_steps
-        #if self.num_input_time_steps > 1:
-        #    x_pos_lagged = torch.roll(x_pos, 1, 2)
-        #    x_pos[:, :, 1:] = x_pos_lagged[:, :, 1:] - x_pos[:, :, 1:]
+        x_pos = rearrange(x[:, self.players_pos_columns], "b (p t d) -> b p d", p=self.num_players, d=self.num_dim)
+        x_crosshair = rearrange(x[:, self.players_nearest_crosshair_to_enemy_columns], "b p -> b p 1",
+                                p=self.num_players)
         x_pos_encoded = self.encode_pos(x_pos)
 
-        #x_vel = rearrange(x[:, self.players_vel_columns], "b (p t d) -> b p t d", p=self.num_players,
-        #                  t=self.num_time_steps, d=self.num_dim)
-        #x_vel_scaled = x_vel / max_speed_per_second
-
-
         x_non_pos_without_similarity = \
-            rearrange(x[:, self.players_non_temporal_columns], "b (p d) -> b p 1 d", p=self.num_players) \
-            .repeat([1, 1, self.num_input_time_steps, 1])
-        similarity_expanded = rearrange(similarity, '(b p t) d -> b p t d', p=1, t=1) \
-            .repeat([1, self.num_players, self.num_input_time_steps, 1])
+            rearrange(x[:, self.players_non_temporal_columns], "b (p d) -> b p d", p=self.num_players)
+        similarity_expanded = rearrange(similarity, '(b p) d -> b p d', p=1) \
+            .repeat([1, self.num_players, 1])
         x_non_pos = torch.concat([x_non_pos_without_similarity, similarity_expanded], dim=-1)
         if self.mask_non_pos:
             x_gathered = torch.cat([x_pos_encoded, torch.zeros_like(x_crosshair), torch.zeros_like(x_non_pos)], -1)
@@ -455,6 +352,7 @@ class TransformerNestedHiddenLatentModel(nn.Module):
                                        p=self.num_players, t=self.num_output_time_steps)
         latent = self.decoder(transformed_nested)
         prob_output = self.prob_output(latent / temperature)
+        # output 0 is batch, 1 is players, 2 is output time step, 3 is probabilities/logits
         return latent, prob_output, one_hot_prob_to_index(prob_output)
 
 
