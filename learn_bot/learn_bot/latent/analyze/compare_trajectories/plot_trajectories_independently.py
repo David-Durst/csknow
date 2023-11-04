2e503ae15525581c9f3fb0d432a33649bd4f28e8
David Durst <davidbdurst@gmail.com>
2023-11-04 14:21:16 -0700

    updating for enw results, allowing multiple hdf5 files in plot independent

--- learn_bot/learn_bot/latent/analyze/compare_trajectories/plot_trajectories_independently.py
+++ learn_bot/learn_bot/latent/analyze/compare_trajectories/plot_trajectories_independently.py
@@ -22,7 +22,7 @@ from learn_bot.latent.place_area.column_names import round_test_name_col
 from learn_bot.latent.place_area.load_data import LoadDataResult, LoadDataOptions
 
 rollout_no_mask_load_data_option = dataclasses.replace(rollout_load_data_option,
-                                                       custom_rollout_extension= "_11_3_23_prebaked_no_mask_100_tries")
+                                                       custom_rollout_extension= "_11_4_23_prebaked_no_mask_100_tries")
 rollout_no_mask_config = dataclasses.replace(rollout_handcrafted_vs_all_human_config,
                                              predicted_load_data_options=rollout_no_mask_load_data_option,
                                              metric_cost_title="Rollout Learned No Mask Randomized vs")
@@ -89,6 +89,10 @@ def run_independent_trajectory_vis():
         else:
             print(f"invalid config case: {config_case}")
             exit(0)
+        if len(sys.argv) == 4:
+            data_option = dataclasses.replace(data_option,
+                                              custom_rollout_extension=sys.argv[3])
+            config = dataclasses.replace(config, predicted_load_data_options=data_option)
         result = plot_trajectories_for_one_config(data_option, config)
         trajectory_plots_by_config.append(result[0])
         trajectory_plots_names = result[1]
