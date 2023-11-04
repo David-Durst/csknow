a99138d5f9f9ada083939240c7d76dec104b7e4c
David Durst <davidbdurst@gmail.com>
2023-11-04 14:41:07 -0700

    adding appendices to files

--- s3_manager/process_rollout_specific_many_no_comparison.sh
+++ s3_manager/process_rollout_specific_many_no_comparison.sh
@@ -23,11 +23,11 @@ for demo in $1; do
     python upload.py rollout $2_$i.dem
     python list.py rollout
     cd ../demo_parser
-    go run cmd/main.go -ro -dn _$2
+    go run cmd/main.go -ro -dn _$2_$i
     cd ../analytics
-    ./scripts/create_rollout_other_datasets.sh $2
+    ./scripts/create_rollout_other_datasets.sh $2_$i
     cd ../learn_bot
-    python -m learn_bot.latent.analyze.compare_trajectories.plot_trajectories_independently 11_01_2023__15_13_54_iw_1024_bc_60_pr_0_fr_0_b_1024_it_5_lr_1e-06_wd_0.0_l_24_h_4_n_20.0_ros_2.0_ct_TimeControl_pm_NoMask_nm_False_om_NoMask_w_None_dh_None_c_just_human_all 0 _$2
+    python -m learn_bot.latent.analyze.compare_trajectories.plot_trajectories_independently 11_01_2023__15_13_54_iw_1024_bc_60_pr_0_fr_0_b_1024_it_5_lr_1e-06_wd_0.0_l_24_h_4_n_20.0_ros_2.0_ct_TimeControl_pm_NoMask_nm_False_om_NoMask_w_None_dh_None_c_just_human_all 0 _$2_$i
     cd ../s3_manager
     ((i+=1))
 done
