c24dd573d8b81a75a210348acaee55efd8afd68e
David Durst <davidbdurst@gmail.com>
2023-11-04 17:16:26 -0700

    adding learned bots prebaked reprocessing

---
+++ s3_manager/reprocess_learned_bots_prebaked_rounds.sh
@@ -0,0 +1,1 @@
+./reprocess_rollout_specific_many_no_comparison.sh 11_2_23_prebaked_no_mask_100_tries 0,1,2,3,4,5,6,7,8,9,10
---
+++ s3_manager/reprocess_rollout_specific_many_no_comparison.sh
@@ -0,0 +1,31 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+
+# create tick data
+get_script_dir
+export IFS=","
+i=0
+for appendix in $2; do
+    python disable.py rollout
+    python enable rollout $1_$appendix.dem
+    cd ../demo_parser
+    go run cmd/main.go -ro -dn _$2_$i
+    cd ../analytics
+    ./scripts/create_rollout_other_datasets.sh $2_$i
+    cd ../learn_bot
+    python -m learn_bot.latent.analyze.compare_trajectories.plot_trajectories_independently 11_01_2023__15_13_54_iw_1024_bc_60_pr_0_fr_0_b_1024_it_5_lr_1e-06_wd_0.0_l_24_h_4_n_20.0_ros_2.0_ct_TimeControl_pm_NoMask_nm_False_om_NoMask_w_None_dh_None_c_just_human_all 0 _$2_$i
+    cd ../s3_manager
+    ((i+=1))
+done
