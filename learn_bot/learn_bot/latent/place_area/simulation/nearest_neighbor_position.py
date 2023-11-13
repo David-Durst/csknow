9df2ea22682c2c0738e18c9ec89b5d781bf2ce84
David Durst <davidbdurst@gmail.com>
2023-11-12 16:53:14 -0800

    fixing bad assignment in nearst position when skipping points because round ends early

--- learn_bot/learn_bot/latent/place_area/simulation/nearest_neighbor_position.py
+++ learn_bot/learn_bot/latent/place_area/simulation/nearest_neighbor_position.py
@@ -21,10 +21,10 @@ def update_nn_position_rollout_tensor(loaded_model: LoadedModel, round_lengths: 
                                for tick_index in range(len(round_tick_range))
                                if tick_index % num_time_steps == 0]
 
-    #similarity_round_ids = [round_id
-    #                        for round_id, round_tick_range in round_lengths.round_to_tick_ids.items()
-    #                        for tick_index in range(len(round_tick_range))
-    #                        if tick_index % num_time_steps == 0]
+    similarity_round_ids = [round_id
+                            for round_id, round_tick_range in round_lengths.round_to_tick_ids.items()
+                            for tick_index in range(len(round_tick_range))
+                            if tick_index % num_time_steps == 0]
 
 
     points_for_nn_tensor = ground_truth_rollout_tensor[similarity_tick_indices]
@@ -33,6 +33,9 @@ def update_nn_position_rollout_tensor(loaded_model: LoadedModel, round_lengths: 
     prior_t_alive = 0
 
     for point_index in range(points_for_nn_tensor.shape[0]):
+        #if point_index != 12:
+        #    continue
+
         ct_pos: List[Vec3] = []
         t_pos: List[Vec3] = []
 
@@ -52,11 +55,11 @@ def update_nn_position_rollout_tensor(loaded_model: LoadedModel, round_lengths: 
 
         if prior_ct_alive != len(ct_pos) or prior_t_alive != len(t_pos):
             cached_nn_data = CachedNNData()
-        #print(f"round id {similarity_round_ids[point_index]}, num ct {len(ct_pos)}, num t {len(t_pos)}")
+        print(f"point index {point_index}, round id {similarity_round_ids[point_index]}, num ct {len(ct_pos)}, num t {len(t_pos)}")
         # 2 as 1 should be this, second should be second best
         same_and_nearest_nps, cached_nn_data = get_nearest_neighbors_one_situation(ct_pos, t_pos, 2, loaded_model, '', 0, False, False,
                                                                                    num_time_steps, cached_nn_data)
-        nn_rollout_tensor[similarity_tick_indices[point_index]:similarity_tick_indices[point_index + 1]] = \
+        nn_rollout_tensor[similarity_tick_indices[point_index]:similarity_tick_indices[point_index] + num_time_steps] = \
             torch.tensor(same_and_nearest_nps[1])
 
         prior_ct_alive = len(ct_pos)
