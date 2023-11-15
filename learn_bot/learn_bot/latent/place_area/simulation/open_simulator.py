c619520f17db96989ebb2b35ed0c7573611c8b7c
David Durst <davidbdurst@gmail.com>
2023-11-14 19:45:48 -0800

    enabling nearest neighbor benchamark again

--- learn_bot/learn_bot/latent/place_area/simulation/open_simulator.py
+++ learn_bot/learn_bot/latent/place_area/simulation/open_simulator.py
@@ -347,8 +347,8 @@ def run_analysis_per_mask(loaded_model: LoadedModel, player_mask_config: PlayerM
     displacement_errors = DisplacementErrors()
     mask_iterations = num_iterations if player_mask_config not in deterministic_configs else 1
     for i, hdf5_wrapper in enumerate(loaded_model.dataset.data_hdf5s):
-        #if i > 1:
-        #    break
+        if i > 1:
+            break
 
         print(f"Processing hdf5 {i} / {len(loaded_model.dataset.data_hdf5s)}: {hdf5_wrapper.hdf5_path}")
         per_iteration_displacement_errors: List[DisplacementErrors] = []
@@ -403,7 +403,7 @@ def run_analysis(loaded_model: LoadedModel):
                            PlayerMaskConfig.STARTING_POSITION,
                            PlayerMaskConfig.INTERPOLATION_ROLLOUT_POSITION,
                            PlayerMaskConfig.INTERPOLATION_ROUND_POSITION,
-                           #PlayerMaskConfig.NN_POSITION,
+                           PlayerMaskConfig.NN_POSITION,
                            PlayerMaskConfig.GROUND_TRUTH_CMD,
                            PlayerMaskConfig.GROUND_TRUTH_POSITION]
     ades_per_mask_config: List[pd.Series] = []
