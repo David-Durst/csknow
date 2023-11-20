1f07653a017b79e6a8411ae095fcd70ef6bca54e
David Durst <davidbdurst@gmail.com>
2023-11-20 10:58:55 -0800

    using pillow to draw lines

--- learn_bot/learn_bot/latent/analyze/plot_trajectory_heatmap/plot_one_trajectory_np.py
+++ learn_bot/learn_bot/latent/analyze/plot_trajectory_heatmap/plot_one_trajectory_np.py
@@ -5,7 +5,7 @@ from typing import Dict, Optional, List, Tuple
 
 import numpy as np
 import pandas as pd
-from PIL import Image
+from PIL import Image, ImageDraw
 from skimage.draw import line_aa, line
 
 from learn_bot.latent.analyze.compare_trajectories.plot_trajectories_from_comparison import concat_horizontal, \
@@ -36,6 +36,7 @@ class ImageBuffers:
 
 image_to_buffers: Dict[str, ImageBuffers] = {}
 spread_radius = 2
+fill_per_line=10
 
 
 def plot_one_trajectory_np(loaded_model: LoadedModel, id_df: pd.DataFrame, dataset: np.ndarray,
@@ -60,21 +61,26 @@ def plot_one_trajectory_np(loaded_model: LoadedModel, id_df: pd.DataFrame, datas
                 alive_round_np[:, loaded_model.model.nested_players_pos_columns_tensor[player_index, 0, 1]])
             canvas_pos_x_np = canvas_pos_np[0].astype(np.intc)
             canvas_pos_y_np = canvas_pos_np[1].astype(np.intc)
+            canvas_pos_xy = list(zip(list(canvas_pos_x_np), list(canvas_pos_y_np)))
 
             buffer = image_to_buffers[title_str].get_buffer(ct_team)
+            cur_player_d2_img = Image.new("L", d2_img.size, color=0)
+            cur_player_d2_drw = ImageDraw.Draw(cur_player_d2_img)
+            cur_player_d2_drw.line(xy=canvas_pos_xy, fill=fill_per_line, width=5)
+            buffer += np.asarray(cur_player_d2_img)
 
-            for i in range(len(canvas_pos_x_np) - 1):
-                #if canvas_pos_x_np[i] == 1318 and canvas_pos_y_np[i] == 1725:
-                #    print(f"{loaded_model.model.nested_players_pos_columns_tensor[i, player_index]}")
-                #buffer[(canvas_pos_x_np[i]-5):(canvas_pos_x_np[i]+5),
-                #       (canvas_pos_y_np[i]-5):(canvas_pos_y_np[i]+5)] += 1
-                rr, cc = line(canvas_pos_x_np[i], canvas_pos_y_np[i], canvas_pos_x_np[i+1], canvas_pos_y_np[i+1])
-                for j in range(len(rr)):
-                    buffer[(rr[j] - spread_radius):(rr[j] + spread_radius),
-                           (cc[j] - spread_radius):(cc[j] + spread_radius)] += 1
-                #spread_points_set = set(spread_points)
-                #for spread_point in spread_points_set:
-                #    buffer[spread_point[0], spread_point[1]] += 1
+            #for i in range(len(canvas_pos_x_np) - 1):
+            #    #if canvas_pos_x_np[i] == 1318 and canvas_pos_y_np[i] == 1725:
+            #    #    print(f"{loaded_model.model.nested_players_pos_columns_tensor[i, player_index]}")
+            #    #buffer[(canvas_pos_x_np[i]-5):(canvas_pos_x_np[i]+5),
+            #    #       (canvas_pos_y_np[i]-5):(canvas_pos_y_np[i]+5)] += 1
+            #    rr, cc = line(canvas_pos_x_np[i], canvas_pos_y_np[i], canvas_pos_x_np[i+1], canvas_pos_y_np[i+1])
+            #    for j in range(len(rr)):
+            #        buffer[(rr[j] - spread_radius):(rr[j] + spread_radius),
+            #               (cc[j] - spread_radius):(cc[j] + spread_radius)] += 1
+            #    #spread_points_set = set(spread_points)
+            #    #for spread_point in spread_points_set:
+            #    #    buffer[spread_point[0], spread_point[1]] += 1
 
 
 def plot_one_image_one_team(title: str, ct_team: bool, team_color: List, saturated_team_color: List,
@@ -99,7 +105,7 @@ def plot_one_image_one_team(title: str, ct_team: bool, team_color: List, saturat
              full_alpha_saturated_team_color[np.newaxis, :].repeat(np.sum(saturated_color_buffer_entries), axis=0)) + \
             ((1 - percent_saturated[:, np.newaxis].repeat(4, axis=1)) *
              full_alpha_team_color[np.newaxis, :].repeat(np.sum(saturated_color_buffer_entries), axis=0))
-    uint8_color_buffer = np.uint8(np.transpose(color_buffer, axes=[1, 0, 2]))
+    uint8_color_buffer = np.uint8(color_buffer, axes=[1, 0, 2])
     base_img.alpha_composite(Image.fromarray(uint8_color_buffer, 'RGBA'))
 
 
