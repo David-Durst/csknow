c16da1f1dc9befad32c77a29d97d358fb118d7cf
David Durst <davidbdurst@gmail.com>
2023-11-20 17:56:02 -0800

    removing unncessary pritning

--- s3_manager/process_prebaked_rounds.sh
+++ s3_manager/process_prebaked_rounds.sh
@@ -21,7 +21,6 @@ for demo in $1; do
     python disable.py rollout
     scp steam@54.219.195.192:csgo-ds/csgo/$demo $2_$i.dem
     python upload.py rollout $2_$i.dem
-    python list.py rollout
     cd ../demo_parser
     go run cmd/main.go -ro -dn _$2_$i
     cd ../analytics
