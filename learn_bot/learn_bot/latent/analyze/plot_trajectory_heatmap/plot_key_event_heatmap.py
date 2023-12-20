fixing scaling and rotation with heatmap

learn_bot/learn_bot/latent/analyze/plot_trajectory_heatmap/plot_key_event_heatmap.py
@@ -12,12 +12,22 @@ from learn_bot.latent.analyze.run_coverage import coverage_pickle_path
 fig_length = 10
 
 
-def plot_title_key_event_heatmap(title: str, event: str, ct_team: bool, x_pos: List[float], y_pos: List[float],
-                                 x_bins, y_bins, fig: plt.Figure, ax: plt.Axes, cmap, max_points: int):
-    ax.set_title(f"{title} {'CT' if ct_team else 'T'} {event} ({len(x_pos)} points)")
+def create_heatmap(title: str, ct_team: bool, title_to_team_to_heatmap: Dict[str, Dict],
+                   x_pos: List[float], y_pos: List[float], x_bins, y_bins):
     heatmap, _, _ = np.histogram2d(x_pos, y_pos, bins=[x_bins, y_bins])
+    heatmap = heatmap.T
+    if title not in title_to_team_to_heatmap:
+        title_to_team_to_heatmap[title] = {}
+    title_to_team_to_heatmap[title][ct_team] = heatmap
+
+
+def plot_heatmap(title: str, event: str, ct_team: bool, title_to_team_to_heatmap: Dict[str, Dict],
+                 x_pos: List[float], max_bin_value: int,
+                 x_bins, y_bins, fig: plt.Figure, ax: plt.Axes, cmap):
+    ax.set_title(f"{title} {'CT' if ct_team else 'T'} {event} ({len(x_pos)} points)")
     grid_x, grid_y = np.meshgrid(x_bins, y_bins)
-    heatmap_im = ax.pcolormesh(grid_x, grid_y, heatmap, vmin=1, vmax=max_points, cmap=cmap)
+    heatmap_im = ax.pcolormesh(grid_x, grid_y, title_to_team_to_heatmap[title][ct_team], vmin=1,
+                               vmax=max_bin_value, cmap=cmap)
     cbar = fig.colorbar(heatmap_im, ax=ax)
     cbar.ax.set_ylabel(f'Number of {event}', rotation=270, labelpad=15, fontsize=14)
 
@@ -30,7 +40,8 @@ def plot_key_event_heatmaps(title_to_team_to_key_event_pos: title_to_team_to_pos
         (coverage_heatmap, x_bins, y_bins) = pickle.load(infile)
 
     num_titles = len(title_to_team_to_key_event_pos.keys())
-    fig = plt.figure(figsize=(fig_length, fig_length * num_titles), constrained_layout=True)
+    # mul cols by 2 as need ct and t plots
+    fig = plt.figure(figsize=(fig_length * 2, fig_length * num_titles), constrained_layout=True)
     axs = fig.subplots(num_titles, 2, squeeze=False)
 
     cmap = plt.get_cmap('viridis').copy()
@@ -49,11 +60,21 @@ def plot_key_event_heatmaps(title_to_team_to_key_event_pos: title_to_team_to_pos
     elif trajectory_filter_options.only_shots:
         event = "Shots"
 
+    title_to_team_to_heatmap = {}
+    max_bin_value = 0
     for i, (title, team_to_key_event_pos) in enumerate(title_to_team_to_key_event_pos.items()):
-        plot_title_key_event_heatmap(title, event, True,
-                                     team_to_key_event_pos[True][0], team_to_key_event_pos[True][1],
-                                     x_bins, y_bins, fig, axs[i, 0], cmap, max_points)
-        plot_title_key_event_heatmap(title, event, False,
-                                     team_to_key_event_pos[False][0], team_to_key_event_pos[False][1],
-                                     x_bins, y_bins, fig, axs[i, 0], cmap, max_points)
-    plt.savefig(plots_path / (event + '_' + str(trajectory_filter_options) + '.png'))
+        create_heatmap(title, True, title_to_team_to_heatmap,
+                       team_to_key_event_pos[True][0], team_to_key_event_pos[True][1], x_bins, y_bins)
+        max_bin_value = max(max_bin_value, title_to_team_to_heatmap[title][True].max())
+        create_heatmap(title, False, title_to_team_to_heatmap,
+                       team_to_key_event_pos[False][0], team_to_key_event_pos[False][1], x_bins, y_bins)
+        max_bin_value = max(max_bin_value, title_to_team_to_heatmap[title][False].max())
+
+    for i, (title, team_to_key_event_pos) in enumerate(title_to_team_to_key_event_pos.items()):
+        plot_heatmap(title, event, True, title_to_team_to_heatmap,
+                     team_to_key_event_pos[True][0], max_bin_value,
+                     x_bins, y_bins, fig, axs[i, 0], cmap)
+        plot_heatmap(title, event, False, title_to_team_to_heatmap,
+                     team_to_key_event_pos[False][0], max_bin_value,
+                     x_bins, y_bins, fig, axs[i, 1], cmap)
+    plt.savefig(plots_path / (event + '.png'))