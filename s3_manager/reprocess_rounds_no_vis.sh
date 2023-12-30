349f085bec2cb1b8eeb157549f4c335562c54041
David Durst <davidbdurst@gmail.com>
2023-12-29 20:16:26 -0800

    adding reprocess rounds no vis

---
+++ s3_manager/reprocess_rounds_no_vis.sh
@@ -0,0 +1,24 @@
+script_dir="tmp"
+#https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     script_dir="$DIR"
+}
+
+# create tick data
+get_script_dir
+export IFS=","
+i=0
+cd ../analytics
+for demo in ${script_dir}/../demo_parser/hdf5/rollout_data_$1_*.hdf5; do
+    ./scripts/create_rollout_other_datasets.sh $1_$i
+    ((i+=1))
+done
