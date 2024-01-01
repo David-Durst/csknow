bd190ec9d76524f185e5ec21204a859658c44635
David Durst <davidbdurst@gmail.com>
2024-01-01 13:39:40 -0800

    actually making directories in deploy scripts

--- learn_bot/scripts/deploy_latent_models_specific.sh
+++ learn_bot/scripts/deploy_latent_models_specific.sh
@@ -1,2 +1,3 @@
 aws s3 cp s3://csknow/models/$1/ saved_models/latent_model/ --recursive
+mkdir -p models/latent_model/
 cp saved_models/latent_model/* models/latent_model/
--- learn_bot/scripts/deploy_uncertain_models_specific.sh
+++ learn_bot/scripts/deploy_uncertain_models_specific.sh
@@ -1,2 +1,3 @@
 aws s3 cp s3://csknow/models/$1/ saved_models/uncertain_model/ --recursive
+mkdir -p models/uncertain_model/
 cp saved_models/uncertain_model/* models/uncertain_model/
