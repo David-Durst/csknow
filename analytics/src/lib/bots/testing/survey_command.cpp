b294290e83af5bac634f747ea437eef8f11b2887
David Durst <davidbdurst@gmail.com>
2024-03-29 13:32:07 -0700

    adding second combat results, starting to add survey comamnd

---
+++ analytics/src/lib/bots/testing/survey_command.cpp
@@ -0,0 +1,14 @@
+//
+// Created by durst on 3/29/24.
+//
+
+#include "bots/testing/survey_command.h"
+
+namespace csknow::survey_command {
+    /*
+    NodeState InteractiveCommand::exec(const ServerState &state, TreeThinker &treeThinker) {
+
+    }
+     */
+
+}
