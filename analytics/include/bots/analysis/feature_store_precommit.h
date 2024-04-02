df39d167d5b31bae7ef01a524c8fda34510d34c3
David Durst <davidbdurst@gmail.com>
2024-04-01 18:18:09 -0700

    only one second of prior data

--- analytics/include/bots/analysis/feature_store_precommit.h
+++ analytics/include/bots/analysis/feature_store_precommit.h
@@ -22,8 +22,8 @@ namespace csknow::feature_store {
     using key_retake_events::max_enemies;
     constexpr double maxWorldDistance = 4000.;
     constexpr int prior_tick_spacing = 16;
-    constexpr int num_prior_ticks = 48;
-    constexpr int num_prior_ticks_inference = 24;
+    constexpr int num_prior_ticks = 8;
+    constexpr int num_prior_ticks_inference = 8;
     constexpr int num_future_ticks = 2;
 
     enum class DecreaseTimingOption {
