5fe30e5318736bce2e1c2b4f9b96a0fb23d00dfd
David Durst <davidbdurst@gmail.com>
2024-04-08 00:34:45 -0700

    adding profile on loaded model

--- learn_bot/learn_bot/latent/profiling.py
+++ learn_bot/learn_bot/latent/profiling.py
@@ -1,7 +1,12 @@
 import pathlib
+import sys
 import time
 import torch
 import random
+
+from torch._C._profiler import ProfilerActivity
+from torch.autograd.profiler import record_function
+from torch.profiler import profile
 
 aggregate_batch_time = 0.
 num_batch_calls = 0
@@ -29,3 +34,29 @@ def profile_latent_model(model_path: pathlib.Path, batch_size: int, batch: torch
         aggregate_per_row_time += (time.time() - start)
         num_per_row_calls += 1
         print("CPU per line: " + str(aggregate_per_row_time / (batch_size * num_per_row_calls)))
+
+
+def print_trainable_parameters(model):
+    print(sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+
+def profile_loaded_model(model_path):
+    torch.set_num_threads(1)
+    torch.set_num_interop_threads(1)
+    temperature_cpu = torch.Tensor([1.])
+    first_row = torch.zeros([1, 1144])
+    first_row_similarity = torch.zeros([1, 1])
+    cpu_model = torch.jit.load(model_path)
+    cpu_model(first_row, first_row_similarity, temperature_cpu)
+    cpu_model(first_row, first_row_similarity, temperature_cpu)
+    cpu_model(first_row, first_row_similarity, temperature_cpu)
+    with profile(activities=[ProfilerActivity.CPU]) as prof:
+        with record_function("model_inference"):
+            cpu_model(first_row, first_row_similarity, temperature_cpu)
+    print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=10))
+    print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+    #prof.export_chrome_trace('/tmp/profiler_output')
+
+
+if __name__ == '__main__':
+    profile_loaded_model(sys.args[1])
